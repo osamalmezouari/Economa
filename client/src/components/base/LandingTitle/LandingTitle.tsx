a81b5494dda18e4d91f896afdac3467e198612f2
Osama Lmezouari <Oussamalm147@gmail.com>
2024-11-22 20:38:13 +0100

    refactor: adjust Box maxWidth, margin, and add padding

--- client/src/components/base/LandingTitle/LandingTitle.tsx
+++ client/src/components/base/LandingTitle/LandingTitle.tsx
@@ -2,10 +2,10 @@ import { Box, Typography } from "@mui/material"
 import { SectionTitleProps } from "./interfaces"
 
 const LandingTitle = ({ title, subTitle }: SectionTitleProps) => {
-    const words = title.split(' '); // Split by spaces
+    const words = title.split(' '); 
     const lastWord = words.filter(word => word !== '').pop() || '';
     const updatedTitle = title.slice(0, title.lastIndexOf(lastWord)).trim();
-    return <Box maxWidth={'1400px'} margin={'60px auto'}>
+    return <Box maxWidth={'1200px'} className="p-2" margin={'30px auto'}>
         <Typography variant="h3" className="text-secondary-main capitalize">
             {updatedTitle}
             <span className="text-primary-main "> {lastWord}</span>
