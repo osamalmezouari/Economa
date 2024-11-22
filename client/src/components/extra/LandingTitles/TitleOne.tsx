f1b7386ec80ba7e900db5312cb4e7181a4a72cb8
Osama Lmezouari <Oussamalm147@gmail.com>
2024-11-22 20:40:14 +0100

    `refactor: replace SectionTitle with LandingTitle component`

--- client/src/components/extra/LandingTitles/TitleOne.tsx
+++ client/src/components/extra/LandingTitles/TitleOne.tsx
@@ -1,8 +1,8 @@
-import SectionTitle from "../../base/LandingTitle/LandingTitle"
+import LandingTitle from "../../base/LandingTitle/LandingTitle"
 
 const TitleOne = () => {
     return <>
-        <SectionTitle title="Day of the deal" subTitle="Don't wait. The time will never be just right." />
+        <LandingTitle title="Day of the deal" subTitle="Don't wait. The time will never be just right." />
     </>
 }
 export default TitleOne
