5063ae79b9a06e6226fe385fc2f8b5ead3bf8c8e
Osama Lmezouari <Oussamalm147@gmail.com>
2024-11-24 17:35:18 +0100

    🎨 feat: integrate PromoCard into OfferBanner component

---
+++ client/src/components/extra/offerBanner/offerBanner.tsx
@@ -0,0 +1,9 @@
+import PromoCard from '../../base/PromoCard/PromoCard';
+
+const OfferBanner = () => {
+  return (
+    <PromoCard image={'https://github.com/osamalmezouari/ecommerce/blob/master/client/public/assets/images/offerbanner.png?raw=true'} title={''} subtitle={''} discountText={''} buttonText={'none'} />
+  );
+};
+
+export default OfferBanner;
