2a6ea04d1a97e52dab190227f2129fba5136bd9d
Osama Lmezouari <Oussamalm147@gmail.com>
2024-11-24 17:46:42 +0100

    ✨ feat: add thunk for fetching new arrivals

--- client/src/features/products/productThunk.ts
+++ client/src/features/products/productThunk.ts
@@ -1,14 +1,26 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
+import { createAsyncThunk } from '@reduxjs/toolkit';
 import * as productsApi from '../../api/products';
 
 export const getproductsCards = createAsyncThunk(
-    '/products/getcards', async (_, { rejectWithValue }) => {
-        try {
-            const products = await productsApi.getProductsCards()
-            return products
-        }
-        catch (error) {
-            return rejectWithValue(error.message || 'Failed to fetch products Cards');
-        }
+  '/products/getcards',
+  async (_, { rejectWithValue }) => {
+    try {
+      const products = await productsApi.getProductsCards();
+      return products;
+    } catch (error) {
+      return rejectWithValue(error.message || 'Failed to fetch products Cards');
     }
-)
+  }
+);
+
+export const getProductsNewArrivals = createAsyncThunk(
+  '/products/getnewArrivals',
+  async (_, { rejectWithValue }) => {
+    try {
+      const products = await productsApi.getProductsNewArrivals();
+      return products;
+    } catch (error) {
+      return rejectWithValue(error.message || 'Failed to fetch products Cards');
+    }
+  }
+);
