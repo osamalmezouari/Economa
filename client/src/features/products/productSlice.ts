e38bda229e9b434c285b602cc2e53dad9cec8dac
Osama Lmezouari <Oussamalm147@gmail.com>
2024-11-24 17:36:51 +0100

    ✨ feat: add productsNewArrivals state management in productsSlice

--- client/src/features/products/productSlice.ts
+++ client/src/features/products/productSlice.ts
@@ -1,36 +1,67 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit"
-import { getproductsCards } from "./productThunk"
-import { ProductCardStateType, ProductCardType } from "../../interfaces/product"
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { getproductsCards, getProductsNewArrivals } from './productThunk';
+import {
+  ProductCardStateType,
+  ProductCardType,
+  ProductsNewArrivals,
+} from '../../interfaces/product';
 
 const initialState: ProductCardStateType = {
-    productsCard: {
-        data: [],
-        loading: false,
-        error: ""
-    }
-}
-
+  productsCard: {
+    data: [],
+    loading: false,
+    error: '',
+  },
+  productsNewArrivals: {
+    data: [],
+    loading: false,
+    error: '',
+  },
+};
 
 const productsSlice = createSlice({
-    name: "products",
-    initialState,
-    reducers: {},
-    extraReducers: (builder) => {
-        builder.addCase(getproductsCards.pending, (state) => {
-            state.productsCard.loading === true
-            state.productsCard.error === null
-        })
-            .addCase(getproductsCards.rejected, (state, action) => {
-                state.productsCard.loading = false
-                state.productsCard.error = action.payload as string
-            })
-            .addCase(getproductsCards.fulfilled, (state, action: PayloadAction<ProductCardType[]>) => {
-                state.productsCard.loading = false
-                state.productsCard.error = ''
-                state.productsCard.data = action.payload
-            })
-    }
-})
+  name: 'products',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      //products cards
+      .addCase(getproductsCards.pending, (state) => {
+        state.productsCard.loading === true;
+        state.productsCard.error === null;
+      })
+      .addCase(getproductsCards.rejected, (state, action) => {
+        state.productsCard.loading = false;
+        state.productsCard.error = action.payload as string;
+      })
+      .addCase(
+        getproductsCards.fulfilled,
+        (state, action: PayloadAction<ProductCardType[]>) => {
+          state.productsCard.loading = false;
+          state.productsCard.error = '';
+          state.productsCard.data = action.payload;
+        }
+      )
 
-export const productsReducer = productsSlice.reducer
-export default productsSlice.reducer
+      //new Arrivals
+      .addCase(getProductsNewArrivals.pending, (state) => {
+        state.productsNewArrivals.loading === true;
+        state.productsNewArrivals.error === null;
+      })
+      .addCase(getProductsNewArrivals.rejected, (state, action) => {
+        state.productsNewArrivals.loading = false;
+        state.productsNewArrivals.error = action.payload as string;
+      })
+      .addCase(
+        getProductsNewArrivals.fulfilled,
+        (state, action: PayloadAction<ProductsNewArrivals[]>) => {
+          state.productsNewArrivals.loading = false;
+          state.productsNewArrivals.error = '';
+          state.productsNewArrivals.data = action.payload;
+        }
+      );
+  },
+});
+
+export const productsReducer = productsSlice.reducer;
+export default productsSlice.reducer;
