5b75e8a74c4e9e64036f6a60abc991d28274f2b9
Osama Lmezouari <Oussamalm147@gmail.com>
2024-11-24 17:48:24 +0100

    ✨ feat: create NewArrivalsSection with TitleTwo and ProductnewArrivalsContainer components

---
+++ client/src/layouts/newArrivalsSection.tsx
@@ -0,0 +1,13 @@
+import TitleTwo from '../components/extra/LandingTitles/TitleTwo';
+import ProductnewArrivalsContainer from '../components/extra/productnewArrivalsContainer/productnewArrivalsContainer';
+
+const NewArrivalsSection = () => {
+  return (
+    <>
+      <TitleTwo />
+      <ProductnewArrivalsContainer />
+    </>
+  );
+};
+
+export default NewArrivalsSection;
