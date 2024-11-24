7fde13e839c9a663ab9a91723cbbe62d08c2cd41
Osama Lmezouari <Oussamalm147@gmail.com>
2024-11-24 17:33:52 +0100

    ✨ feat: enhance PromoCard with responsive design and conditional button rendering

--- client/src/components/base/PromoCard/PromoCard.tsx
+++ client/src/components/base/PromoCard/PromoCard.tsx
@@ -10,28 +10,20 @@ const PromoCard: React.FC<PromoCardProps> = ({
   buttonText,
 }) => {
   return (
+    <div className='px-6 sm:px-0'>
     <Box
-      sx={{
-        display: 'flex',
-        alignItems: 'center',
-        maxWidth: '1200px',
-        margin: 'auto',
-      }}
-    >
-      <Box
         component="div"
         style={{ backgroundImage: `url(${image})` }}
-        className="w-full h-[350px] rounded-xl relative bg-center flex justify-center items-center text-center sm:block sm:text-start"
+        className="bg-cover w-full max-w-[1150px] m-auto h-[320px] rounded relative bg-center flex justify-center items-center text-center sm:block sm:text-start"
       >
-        <Box component={'div'} className=" sm:absolute sm:right-[10%] sm:top-[25%]">
-          <Typography
-            variant="h2"
-            fontWeight="bold"
-            color="secondary"
-          >
+        <Box
+          component={'div'}
+          className=" sm:absolute sm:right-[10%] sm:top-[25%] text-end"
+        >
+          <Typography variant="h3" fontWeight="bold" color="secondary">
             {title}
           </Typography>
-          <Typography variant="h2" color="text.secondary" >
+          <Typography variant="h5" color="text.secondary">
             {subtitle}
           </Typography>
           <Typography
@@ -42,17 +34,18 @@ const PromoCard: React.FC<PromoCardProps> = ({
           >
             {discountText}
           </Typography>
-          <Button
+          {buttonText !== "none" && <Button
             variant="contained"
-            color='secondary'
-            className='hover:bg-primary-main bg-secondary-main h-8'
+            color="secondary"
+            className="hover:bg-primary-main bg-secondary-main h-8"
             sx={{ textTransform: 'none', fontSize: 16 }}
           >
             {buttonText}
-          </Button>
+          </Button>}
         </Box>
       </Box>
-    </Box>
+    </div>
+      
   );
 };
 
