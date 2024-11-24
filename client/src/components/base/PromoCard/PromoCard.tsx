9c0559d8c6ffbbb33efbac73ad9f0e041679b36b
Osama Lmezouari <Oussamalm147@gmail.com>
2024-11-24 13:29:04 +0100

    feat: refactor PromoCard layout with background image and improved styling

--- client/src/components/base/PromoCard/PromoCard.tsx
+++ client/src/components/base/PromoCard/PromoCard.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { Box, Button, Typography } from '@mui/material';
 import { PromoCardProps } from './interfaces';
 
-
-
 const PromoCard: React.FC<PromoCardProps> = ({
   image,
   title,
@@ -16,48 +14,43 @@ const PromoCard: React.FC<PromoCardProps> = ({
       sx={{
         display: 'flex',
         alignItems: 'center',
-        padding: 3,
-        borderRadius: 2,
-        backgroundColor: '#f9f9f9',
-        boxShadow: '0 4px 8px rgba(0,0,0,0.1)',
-        overflow: 'hidden',
+        maxWidth: '1200px',
+        margin: 'auto',
       }}
     >
-    <Box
-        component="img"
-        src={image}
-        alt={title}
-        sx={{
-          width: '50%',
-          height: '100%',
-          borderRadius: 2,
-          objectFit: 'cover',
-        }}
-      />
-
-      {/* Text and Button Section */}
-      <Box sx={{ paddingLeft: 3, flex: 1 }}>
-        <Typography variant="h5" fontWeight="bold" gutterBottom>
-          {title}
-        </Typography>
-        <Typography variant="body1" color="text.secondary" gutterBottom>
-          {subtitle}
-        </Typography>
-        <Typography
-          variant="body1"
-          color="success.main"
-          fontWeight="bold"
-          gutterBottom
-        >
-          {discountText}
-        </Typography>
-        <Button
-          variant="contained"
-          color="success"
-          sx={{ textTransform: 'none', fontSize: 16 }}
-        >
-          {buttonText}
-        </Button>
+      <Box
+        component="div"
+        style={{ backgroundImage: `url(${image})` }}
+        className="w-full h-[350px] rounded-xl relative bg-center flex justify-center items-center text-center sm:block sm:text-start"
+      >
+        <Box component={'div'} className=" sm:absolute sm:right-[10%] sm:top-[25%]">
+          <Typography
+            variant="h2"
+            fontWeight="bold"
+            color="secondary"
+          >
+            {title}
+          </Typography>
+          <Typography variant="h2" color="text.secondary" >
+            {subtitle}
+          </Typography>
+          <Typography
+            variant="body1"
+            color="success.main"
+            fontWeight="bold"
+            gutterBottom
+          >
+            {discountText}
+          </Typography>
+          <Button
+            variant="contained"
+            color='secondary'
+            className='hover:bg-primary-main bg-secondary-main h-8'
+            sx={{ textTransform: 'none', fontSize: 16 }}
+          >
+            {buttonText}
+          </Button>
+        </Box>
       </Box>
     </Box>
   );
