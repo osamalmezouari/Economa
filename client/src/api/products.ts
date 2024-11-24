d963fde58d653a03722975f44f8c11c6893837c3
Osama Lmezouari <Oussamalm147@gmail.com>
2024-11-24 17:31:11 +0100

    feat: add API service for fetching new arrivals products

--- client/src/api/products.ts
+++ client/src/api/products.ts
@@ -1,12 +1,24 @@
-import { ProductCardType } from "../interfaces/product";
-import { apiClient } from "./apiClient";
-
+import { ProductCardType, ProductsNewArrivals } from '../interfaces/product';
+import { apiClient } from './apiClient';
 
 export const getProductsCards = async (): Promise<ProductCardType[]> => {
-    try {
-        const response = await apiClient.get<ProductCardType[]>('products/cards')
-        return response.data
-    } catch (error) {
-        throw new Error('Failed to fetch products cards');
-    }
-}
+  try {
+    const response = await apiClient.get<ProductCardType[]>('products/cards');
+    return response.data;
+  } catch (error) {
+    throw new Error('Failed to fetch products cards');
+  }
+};
+
+export const getProductsNewArrivals = async (): Promise<
+  ProductsNewArrivals[]
+> => {
+  try {
+    const response = await apiClient.get<ProductsNewArrivals[]>(
+      'products/newArrivals'
+    );
+    return response.data;
+  } catch (error) {
+    throw new Error('Failed to fetch new arrivals products');
+  }
+};
