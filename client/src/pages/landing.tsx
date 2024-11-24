30d19adad48a27fe2b6e31257ec2b37e640b0250
Osama Lmezouari <Oussamalm147@gmail.com>
2024-11-24 17:49:08 +0100

    ✨ feat: update Landing component with NewArrivalsSection and OfferBanner

--- client/src/pages/landing.tsx
+++ client/src/pages/landing.tsx
@@ -1,5 +1,7 @@
+import OfferBanner from '../components/extra/offerBanner/offerBanner';
 import PromoCardWraper from '../components/extra/PromoCard/PromoCard';
 import Home from '../layouts/Home';
+import NewArrivalsSection from '../layouts/newArrivalsSection';
 import ProductsWithDiscount from '../layouts/ProductsWithDiscount';
 
 const Landing = () => {
@@ -8,6 +10,8 @@ const Landing = () => {
       <Home />
       <ProductsWithDiscount />
       <PromoCardWraper />
+      <NewArrivalsSection />
+      <OfferBanner />
     </>
   );
 };
