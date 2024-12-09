0caf2e7df5208de9bc904657aaffa711abeff7ff
Osama Lmezouari <Oussamalm147@gmail.com>
2024-12-10 00:12:24 +0100

    ✨ Add API client with axios and request interceptor for token

---
+++ client/src/utils/apiClient.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const api = 'http://localhost:3000/';
+export const apiClient = axios.create({
+  baseURL: api,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+apiClient.interceptors.request.use(
+  (config) => {
+    if (typeof window !== 'undefined') {
+      const token = localStorage.getItem('accesstoken');
+      if (token) {
+        console.log(token);
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
