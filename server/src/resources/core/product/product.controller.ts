9a73e224dbcabf4744ebab030fcb3ffba417d18b
Osama Lmezouari <Oussamalm147@gmail.com>
2024-12-10 00:19:27 +0100

    🔒 feat: Add authentication decorator to product routes

--- server/src/resources/core/product/product.controller.ts
+++ server/src/resources/core/product/product.controller.ts
@@ -10,22 +10,27 @@ import {
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { AUTH } from 'src/common/decorators/meta/authentication.decorator';
+import { AuthenticationType } from 'src/common/enums/authentication';
 
 @Controller('products')
 export class ProductController {
   constructor(private readonly productService: ProductService) { }
   
+  @AUTH(AuthenticationType.None)
   @Get()
   async findAll() {
     const product = await this.productService.findAll();
     return product;
   }
 
+  @AUTH(AuthenticationType.None)
   @Get('cards')
   async getAllProductCards() {
     return this.productService.getAllProductCards();
   }
-
+  
+  @AUTH(AuthenticationType.None)
   @Get('newArrivals')
   async getnewArrivals() {
     return this.productService.getnewArrivals();
