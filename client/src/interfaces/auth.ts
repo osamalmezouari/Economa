e4781c7da239a76909a43169eda2236a07075e54
Osama Lmezouari <Oussamalm147@gmail.com>
2024-12-10 00:10:15 +0100

    ✨ chore: Export LoginPayload and RegisterPayload interfaces for user authentication

---
+++ client/src/interfaces/auth.ts
@@ -0,0 +1,12 @@
+export interface LoginPayload {
+    email: string;
+    password: string;
+}
+export interface RegisterPayload {
+    name: string;
+    email: string;
+    password: string;
+    phoneNumber: string;
+    address: string;
+}
+
