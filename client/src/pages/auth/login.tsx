5c0682bf1dc9711fa4d8687d2c2cab67e0e26312
Osama Lmezouari <Oussamalm147@gmail.com>
2024-12-09 14:06:06 +0100

    style: Update grid item sizes in Login component
    Modified grid item sizes to xs={12}, sm={12}, md={12}, lg={12} to ensure consistent full-width layout on all screen sizes.

---
+++ client/src/pages/auth/login.tsx
@@ -0,0 +1,69 @@
+import { Box, Button, Grid, TextField, Typography } from '@mui/material';
+import Navbar from '../../components/extra/navbar/Navbar';
+import Footer from '../../components/extra/footer/footer';
+
+const Login = () => {
+  return (
+    <>
+      <Navbar />
+      <Box className="flex max-w-[1000px] gap-4 h-full my-20 m-auto items-center justify-center">
+        <Grid
+          container
+          spacing={1}
+          className="justify-center items-center p-4"
+        >
+          <Grid item xs={12} sm={12} md={12} lg={12}>
+            <Typography variant="h2" className="text-primary-main">
+              Login
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={12} md={12} lg={12}>
+            <TextField
+              variant="outlined"
+              fullWidth
+              label="Email"
+              placeholder="Email"
+              className="mb-4"
+              defaultValue={''}
+              required={true}
+            />
+          </Grid>
+          <Grid item  xs={12} sm={12} md={12} lg={12}>
+            <TextField
+              variant="outlined"
+              fullWidth
+              label="Password"
+              placeholder="Password"
+              type="password"
+              className="mb-4"
+              defaultValue={''}
+              required={true}
+            />
+          </Grid>
+          <Grid item xs={12} sm={12} md={12} lg={12}>
+            <p className="text-end font-light text-secondary-lighter text-[12px] hover:text-primary-main hover:cursor-pointer hover:underline">
+              Don't have an account? Register
+            </p>
+          </Grid>
+          <Grid item xs={12} sm={12} md={12} lg={12}>
+            <Button
+              variant="contained"
+              color="primary"
+              fullWidth
+              className="mt-4"
+            >
+              Submit
+            </Button>
+          </Grid>
+        </Grid>
+        <img
+          src="assets/images/Login_register.png"
+          className="rounded w-0 md:w-[500px]"
+        />
+      </Box>
+      <Footer />
+    </>
+  );
+};
+
+export default Login;
