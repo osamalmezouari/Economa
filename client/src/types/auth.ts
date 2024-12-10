7041d4b479ef730c5de7e468d430ea477ffefc9b
Osama Lmezouari <Oussamalm147@gmail.com>
2024-12-11 00:19:39 +0100

    🔐 feat : add auth interfaces for login and registration state management

---
+++ client/src/types/auth.ts
@@ -0,0 +1,26 @@
+import { ApiError } from './error';
+
+export interface LoginPayload {
+  email: string;
+  password: string;
+}
+export interface RegisterPayload {
+  name: string;
+  email: string;
+  password: string;
+  phoneNumber: string;
+  address: string;
+}
+
+export interface AuthState {
+  Login: {
+    loading: boolean;
+    error: ApiError | null | undefined | unknown;
+    data: LoginPayload[];
+  };
+  Register: {
+    loading: boolean;
+    error: ApiError | null | undefined | unknown;
+    data: RegisterPayload[];
+  };
+}
