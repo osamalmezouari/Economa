c72620dd70cf71754b2a8a7ea24dfb1c20ea8f8b
Osama Lmezouari <Oussamalm147@gmail.com>
2024-12-11 00:18:33 +0100

    ✨ Refactor Register component: integrate form submission, error handling, and success alerts

--- client/src/pages/auth/register.tsx
+++ client/src/pages/auth/register.tsx
@@ -1,18 +1,56 @@
-import { Box, Button, Grid, TextField, Typography } from '@mui/material';
+import {
+  Alert,
+  Box,
+  Button,
+  CircularProgress,
+  Grid,
+  TextField,
+  Typography,
+} from '@mui/material';
 import Navbar from '../../components/extra/navbar/Navbar';
 import Footer from '../../components/extra/footer/footer';
+import { RegisterPayload } from '../../types/auth';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from '../../app/store';
+import { Register } from '../../features/auth/authThunk';
+import { ApiError } from '../../types/error';
 
-const Register = () => {
+const RegisterComponent = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const { loading, error, data } = useSelector(
+    (state: RootState) => state.auth.Register
+  );
+  const HandleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const newRegistredUser: RegisterPayload = {
+      name: formData.get('name') as string,
+      email: formData.get('email') as string,
+      password: formData.get('password') as string,
+      phoneNumber: formData.get('phoneNumber') as string,
+      address: formData.get('address') as string,
+    };
+    await dispatch(Register(newRegistredUser));
+  };
+
   return (
     <>
       <Navbar />
-      <Box className="flex max-w-[1000px] gap-4 h-full my-20 m-auto items-center justify-center">
+      <Box
+        component="form"
+        className="flex max-w-[1000px] gap-4 h-full my-20 m-auto items-center justify-center"
+        onSubmit={(e) => HandleSubmit(e)}
+      >
+        <img
+          src="assets/images/Login_register.png"
+          className="rounded w-0 md:w-[580px] h-full"
+        />
         <Grid
           container
           spacing={1}
           className="justify-center items-center  p-4"
         >
-          <Grid item  xs={12} sm={12} md={12} lg={12}>
+          <Grid item xs={12} sm={12} md={12} lg={12}>
             <Typography variant="h2" className="text-primary-main">
               Register
             </Typography>
@@ -24,8 +62,10 @@ const Register = () => {
               label="Name"
               placeholder="Name"
               className="mb-4"
+              type="text"
               defaultValue={''}
               required={false}
+              name="name"
               sx={{
                 '& .MuiOutlinedInput-root': {
                   '&.Mui-focused fieldset': {
@@ -47,6 +87,8 @@ const Register = () => {
               className="mb-4"
               defaultValue={''}
               required={true}
+              type="email"
+              name="email"
               sx={{
                 '& .MuiOutlinedInput-root': {
                   '&.Mui-focused fieldset': {
@@ -69,6 +111,7 @@ const Register = () => {
               className="mb-4"
               defaultValue={''}
               required={true}
+              name="password"
               sx={{
                 '& .MuiOutlinedInput-root': {
                   '&.Mui-focused fieldset': {
@@ -86,10 +129,12 @@ const Register = () => {
               variant="outlined"
               fullWidth
               label="Phone Number"
-              placeholder="Phone Number"
+              placeholder="Ex : +212 600 000000 "
               className="mb-4"
               defaultValue={''}
               required={true}
+              type="text"
+              name="phoneNumber"
               sx={{
                 '& .MuiOutlinedInput-root': {
                   '&.Mui-focused fieldset': {
@@ -109,7 +154,9 @@ const Register = () => {
               label="Address"
               placeholder="Address"
               className="mb-4"
+              type="text"
               defaultValue={''}
+              name="address"
               sx={{
                 '& .MuiOutlinedInput-root': {
                   '&.Mui-focused fieldset': {
@@ -128,24 +175,37 @@ const Register = () => {
             </p>
           </Grid>
           <Grid item xs={12} sm={12} md={12} lg={12}>
+            {error instanceof ApiError && (
+              <Alert severity="error">{error.message}</Alert>
+            )}
+            {data.length > 0 && (
+              <Alert severity="success">
+                Registration successful! You will be redirected to the home
+                page.
+              </Alert>
+            )}
+          </Grid>
+          <Grid item xs={12} sm={12} md={12} lg={12}>
             <Button
+              type="submit"
               variant="contained"
               color="primary"
               fullWidth
               className="mt-4"
+              disabled={loading}
             >
-              Submit
+              {loading && data.length === 0 ? (
+                <CircularProgress size="30px" color="inherit" />
+              ) : (
+                'Register'
+              )}
             </Button>
           </Grid>
         </Grid>
-        <img
-          src="assets/images/Login_register.png"
-          className="rounded w-0 md:w-[500px]"
-        />
       </Box>
       <Footer />
     </>
   );
 };
 
-export default Register;
+export default RegisterComponent;
