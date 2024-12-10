9ea61131838d1286cfa4557e3199387028aabd52
Osama Lmezouari <Oussamalm147@gmail.com>
2024-12-11 00:23:50 +0100

    🔑fix update JWT environment variables for audience and issuer

--- server/src/config/jwt.config.ts
+++ server/src/config/jwt.config.ts
@@ -4,8 +4,8 @@ import * as process from 'node:process';
 export default registerAs('jwt', () => {
   return {
     secret: process.env.JWT_SECRET,
-    audience: process.env.JWT_AUDIENCE,
-    issuer: process.env.JWT_ISUER,
+    audience: process.env.JWT_TOKEN_AUDIENCE,
+    issuer: process.env.JWT_TOKEN_ISSUER,
     accessTokenTtl: parseInt(process.env.JWT_ACCESS_TOKEN_TTL ?? '3600', 10),
   };
 });
