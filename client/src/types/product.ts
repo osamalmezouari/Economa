8bfb670abd4eac275e72d66e9c34b8961e295c07
Osama Lmezouari <Oussamalm147@gmail.com>
2024-12-11 00:21:47 +0100

    🛍️ feat add product interfaces for cards, new arrivals, and state management

---
+++ client/src/types/product.ts
@@ -0,0 +1,34 @@
+import { ApiError } from './error';
+
+export interface ProductCardType {
+  id: string;
+  discount: number;
+  name: string;
+  categoryName: string;
+  description: string;
+  productAvgRaiting: number;
+  price: string;
+  priceWithDiscount: number;
+  unit: string;
+  imageLink: string;
+}
+
+export interface ProductsNewArrivals extends ProductCardType {}
+
+export interface ProductDialogProps extends ProductCardType {
+  open: boolean;
+  setopen: (open: boolean) => void;
+}
+
+export interface ProductCardStateType {
+  productsCard: {
+    data: ProductCardType[];
+    loading: boolean;
+    error: ApiError | null | undefined | unknown;
+  };
+  productsNewArrivals: {
+    data: ProductsNewArrivals[];
+    loading: boolean;
+    error: ApiError | null | undefined | unknown;
+  };
+}
