e62c6833367c29c5a4ea216881d06d19dc38513b
Osama Lmezouari <Oussamalm147@gmail.com>
2024-12-11 00:15:12 +0100

    🔧 Refactor: Replace category-related actions with category cards fetching

--- client/src/features/category/categoryThunk.ts
+++ client/src/features/category/categoryThunk.ts
@@ -1,51 +1,19 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import * as categoryApi from '../../api/category';
-import { CategoryType } from '../../interfaces/category';
 
-export const getCategories = createAsyncThunk(
-    'category/getCategories',
-    async (_, { rejectWithValue }) => {
-        try {
-            const categories = await categoryApi.getCategory();
-            return categories;
-        } catch (error) {
-            return rejectWithValue(error.message || 'Failed to fetch categories');
-        }
+export const getCategoryCards = createAsyncThunk(
+  'category/getCategoryCards',
+  async (_, { rejectWithValue }) => {
+    try {
+      const categories = await categoryApi.CategoryCards();
+      return categories;
+    } catch (error: any) {
+      if (error) {
+        return rejectWithValue(error);
+      }
+      return rejectWithValue({
+        message: error.message || 'Something went wrong.',
+      });
     }
-);
-
-export const createCategory = createAsyncThunk(
-    'category/createCategory',
-    async (categoryData: CategoryType, { rejectWithValue }) => {
-        try {
-            const newCategory = await categoryApi.createCategory(categoryData);
-            return newCategory;
-        } catch (error) {
-            return rejectWithValue(error.message || 'Failed to create category');
-        }
-    }
-);
-
-export const updateCategory = createAsyncThunk(
-    'category/updateCategory',
-    async ({ categoryData, id }: { categoryData: CategoryType; id: string }, { rejectWithValue }) => {
-        try {
-            const updatedCategory = await categoryApi.updateCategory(categoryData, id);
-            return updatedCategory;
-        } catch (error) {
-            return rejectWithValue(error.message || 'Failed to update category');
-        }
-    }
-);
-
-export const deleteCategory = createAsyncThunk(
-    'category/deleteCategory',
-    async (id: string, { rejectWithValue }) => {
-        try {
-            const deletedCategory = await categoryApi.deleteCategory(id);
-            return deletedCategory;
-        } catch (error) {
-            return rejectWithValue(error.message || 'Failed to delete category');
-        }
-    }
+  }
 );
