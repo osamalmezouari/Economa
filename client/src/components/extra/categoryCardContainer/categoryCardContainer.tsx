d76045d2abc618fbed4af98f4770f9d7d051a5b8
Osama Lmezouari <Oussamalm147@gmail.com>
2024-12-11 00:12:17 +0100

    🔧 Refactor: Update import and state structure for category data (getCategories → getCategoryCards)

--- client/src/components/extra/categoryCardContainer/categoryCardContainer.tsx
+++ client/src/components/extra/categoryCardContainer/categoryCardContainer.tsx
@@ -1,39 +1,46 @@
-import { Container } from "@mui/material";
-import Grid2 from "@mui/material/Grid2"; 
-import CategoryCard from "../../base/category-card/categoryCard";
-import { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { AppDispatch, RootState } from "../../../app/store";
-import { getCategories } from "../../../features/category/categoryThunk";
+import { Container } from '@mui/material';
+import Grid2 from '@mui/material/Grid2';
+import CategoryCard from '../../base/category-card/categoryCard';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { AppDispatch, RootState } from '../../../app/store';
+import { getCategoryCards } from '../../../features/category/categoryThunk';
 
 const CategoryCardContainer = () => {
-    const dispatch = useDispatch<AppDispatch>();
-    const { categories, loading } = useSelector((state: RootState) => state.category);
+  const dispatch = useDispatch<AppDispatch>();
+  const { data, loading } = useSelector(
+    (state: RootState) => state.category.CategoryCards
+  );
 
-    useEffect(() => {
-        dispatch(getCategories());
-    }, [dispatch]);
+  useEffect(() => {
+    dispatch(getCategoryCards());
+  }, [dispatch]);
 
-    return (
-        <Container style={{ maxWidth: "1200px", marginTop: "40px" }}>
-            {loading ? (
-                <p>Loading categories...</p>
-            ) : (
-                <Grid2 container spacing={4} sx={{ alignItems: 'center'}} component={'div'} className="justify-evenly" >
-                    {categories.map((category) => (
-                        <CategoryCard
-                            key={category.id}
-                            linkTo={`/category/${category.id}`}
-                            svgLink={category.svgLink || ''} 
-                            name={category.name}
-                            description={category.description}
-                            productsCount={category.productsCount || 0} 
-                        />
-                    ))}
-                </Grid2>
-            )}
-        </Container>
-    );
+  return (
+    <Container style={{ maxWidth: '1200px', marginTop: '40px' }}>
+      {loading ? (
+        <p>Loading categories...</p>
+      ) : (
+        <Grid2
+          container
+          spacing={4}
+          sx={{ alignItems: 'center' }}
+          component={'div'}
+          className="justify-evenly"
+        >
+          {data.map((category) => (
+            <CategoryCard
+              key={category.id}
+              linkTo={`/category/${category.id}`}
+              svgLink={category.svgLink || ''}
+              name={category.name}
+              productsCount={category.productsCount || 0}
+            />
+          ))}
+        </Grid2>
+      )}
+    </Container>
+  );
 };
 
 export default CategoryCardContainer;
