17a5b558c30cccab4a8f66970805b82337717916
Osama Lmezouari <Oussamalm147@gmail.com>
2024-12-11 00:09:44 +0100

    🔧 Add Auth Reducer to Root Reducer
        Included authReducer in the combineReducers function to handle authentication state.
        Updated root reducer to manage state for categories, products, and authentication.

--- client/src/app/rootReducer.ts
+++ client/src/app/rootReducer.ts
@@ -1,10 +1,11 @@
 import { combineReducers } from 'redux';
 import { categoryReducer } from '../features/category/categorySlice';
 import { productsReducer } from '../features/products/productSlice';
+import { authReducer } from '../features/auth/authSlice';
 
 const rootReducer = combineReducers({
     category: categoryReducer,
-    products: productsReducer
+    products: productsReducer,
+    auth : authReducer
 });
-
 export default rootReducer;
