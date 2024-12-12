9b9c13391e4c78e5e5e6afd8be687fcd7031d1e5
Osama Lmezouari <Oussamalm147@gmail.com>
2024-12-12 23:39:26 +0100

    add shoppingCartItemProps interface

---
+++ client/src/components/base/shoppingCartItem/interface.ts
@@ -0,0 +1,9 @@
+export interface shoppingCartItemProps {
+  id: string;
+  productName: string;
+  productPrice: number;
+  productunit: string;
+  productId: string;
+  svgLink: string;
+  quantity: number;
+}
