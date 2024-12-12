3965dfd91d23d14f86bb111a0c9e84af808114b6
Osama Lmezouari <Oussamalm147@gmail.com>
2024-12-12 23:43:12 +0100

    📝 add: Define types for shopping cart state and related actions

---
+++ client/src/types/shoppingCart.ts
@@ -0,0 +1,35 @@
+import { shoppingCartItemProps } from '../components/base/shoppingCartItem/interface';
+import { ApiError } from './error';
+
+export interface ShoopingCartType extends shoppingCartItemProps {}
+
+export interface shoppingCartStateType {
+  shoppingCartWithProducts: {
+    data: ShoopingCartType[];
+    loading: boolean;
+    error: ApiError | null | undefined | unknown;
+  };
+  createshoppingCart: {
+    data: createShoppingCart | {};
+    loading: boolean;
+    error: ApiError | null | undefined | unknown;
+  };
+  removefromshoppingCart: {
+    data: removefromshoppingCart | {};
+    loading: boolean;
+    error: ApiError | null | undefined | unknown;
+  };
+  open: boolean;
+  basePrice: number;
+  totalPrice: number;
+  vat: number;
+}
+
+export interface createShoppingCart {
+  productId: string;
+  quantity: number;
+}
+
+export interface removefromshoppingCart {
+  id: string;
+}
