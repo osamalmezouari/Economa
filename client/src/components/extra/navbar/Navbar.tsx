f338bc4c69203eb9e318fb18626f832a8277c044
Osama Lmezouari <Oussamalm147@gmail.com>
2024-12-12 23:40:25 +0100

    🔧 feat: Add cart display functionality and update navbar with Material-UI buttons

--- client/src/components/extra/navbar/Navbar.tsx
+++ client/src/components/extra/navbar/Navbar.tsx
@@ -9,9 +9,16 @@ import { IoPersonOutline } from 'react-icons/io5';
 import { GrFavorite } from 'react-icons/gr';
 import { TbShoppingBag } from 'react-icons/tb';
 import { logo } from '../../../mock/constants';
+import { useDispatch } from 'react-redux';
+import { AppDispatch } from '../../../app/store';
+import { Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import { setDisplayCart } from '../../../features/shoppingCart/shoppingCartSlice';
 
 const Navbar = () => {
   const [isOpen, setOpen] = useState(false);
+  const dispatch = useDispatch<AppDispatch>();
+  const navigate = useNavigate();
   const NavItems = {
     itemsCenter: [
       { icon: <FiHome />, link: '#', name: 'home' },
@@ -20,9 +27,46 @@ const Navbar = () => {
       { icon: <LuGitCompare />, link: '#', name: 'Compare' },
     ],
     ItemsRight: [
-      { icon: <IoPersonOutline fontSize={'25px'} />, name: 'Login', link: '#' },
-      { icon: <GrFavorite fontSize={'25px'} />, name: 'Wishlist', link: '#' },
-      { icon: <TbShoppingBag fontSize={'25px'} />, name: 'Cart', link: '#' },
+      {
+        icon: (
+          <Button
+            variant="text"
+            className="w-28 bg-secondary-darker ml-2 h-14 hover:border-2 "
+            startIcon={<IoPersonOutline />}
+          >
+            <p className="text-secondary-darker font-secondary">Login</p>
+          </Button>
+        ),
+        name: 'Login',
+        link: '#',
+      },
+      {
+        icon: (
+          <Button
+            variant="text"
+            className="w-28 bg-secondary-darker ml-2 h-14 hover:border-2 "
+            startIcon={<GrFavorite />}
+          >
+            <p className="text-secondary-darker font-secondary">Wishlist</p>
+          </Button>
+        ),
+        name: 'Wishlist',
+        link: '#',
+      },
+      {
+        icon: (
+          <Button
+            variant="text"
+            className="w-28 bg-secondary-darker ml-2 h-14 hover:border-2 "
+            startIcon={<TbShoppingBag />}
+            onClick={() => dispatch(setDisplayCart())}
+          >
+            <p className="text-secondary-darker font-secondary">cart</p>
+          </Button>
+        ),
+        name: 'Cart',
+        link: '#',
+      },
     ],
     logo: logo,
     isOpen,
