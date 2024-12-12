fb22a8e974fb0ca82896c36f099b569a79b7c912
Osama Lmezouari <Oussamalm147@gmail.com>
2024-12-12 23:41:37 +0100

    🔧 feat: Implement shopping cart slice with actions for CRUD operations

---
+++ client/src/features/shoppingCart/shoppingCartSlice.ts
@@ -0,0 +1,133 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import {
+  createshoppingCart,
+  getshoppingCart,
+  removefromshoppingCart,
+  updatequantity,
+} from './shoppingCartThunk';
+import {
+  createShoppingCart,
+  shoppingCartStateType,
+} from '../../types/shoppingCart';
+
+import { ShoopingCartType } from '../../types/shoppingCart';
+const initialState: shoppingCartStateType = {
+  shoppingCartWithProducts: {
+    data: [],
+    loading: false,
+    error: null,
+  },
+  createshoppingCart: {
+    data: {},
+    loading: false,
+    error: null,
+  },
+  removefromshoppingCart: {
+    data: {},
+    loading: false,
+    error: null,
+  },
+  open: true,
+  basePrice: 0,
+  totalPrice: 0,
+  vat: 0,
+};
+
+const shoppingCartSlice = createSlice({
+  name: 'shoppingCart',
+  initialState,
+  reducers: {
+    setTotalPrice: (state) => {
+      state.totalPrice = state.basePrice + state.vat;
+    },
+    setvat: (state) => {
+      state.vat = state.basePrice * 0.2;
+    },
+    setBasePrice: (state) => {
+      state.basePrice = state.shoppingCartWithProducts.data.reduce(
+        (total, item) => total + item.productPrice * item.quantity,
+        0
+      );
+    },
+    setDisplayCart: (state) => {
+      state.open = !state.open;
+    },
+    clearCreateError: (state) => {
+      state.createshoppingCart.error = null;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getshoppingCart.pending, (state) => {
+        state.shoppingCartWithProducts.loading = true;
+        state.shoppingCartWithProducts.error = null;
+      })
+      .addCase(getshoppingCart.fulfilled, (state, action: any) => {
+        state.shoppingCartWithProducts.loading = false;
+        state.shoppingCartWithProducts.error = null;
+        state.shoppingCartWithProducts.data = action.payload;
+        state.basePrice = action.payload.reduce(
+          (total: number, item: ShoopingCartType) =>
+            total + item.productPrice * item.quantity,
+          0
+        );
+        state.vat = state.basePrice * 0.2;
+        state.totalPrice = state.basePrice + state.vat;
+      })
+      .addCase(getshoppingCart.rejected, (state, action) => {
+        state.shoppingCartWithProducts.loading = false;
+        state.shoppingCartWithProducts.error = action.payload;
+      })
+      .addCase(updatequantity.fulfilled, (state, action: any) => {
+        const updatedItem = action.payload;
+        const itemIndex = state.shoppingCartWithProducts.data.findIndex(
+          (item: ShoopingCartType) => item.id === updatedItem.id
+        );
+
+        if (itemIndex !== -1) {
+          state.shoppingCartWithProducts.data[itemIndex].quantity =
+            updatedItem.quantity;
+        }
+        state.basePrice = state.shoppingCartWithProducts.data.reduce(
+          (total, item) => total + item.productPrice * item.quantity,
+          0
+        );
+        state.vat = state.basePrice * 0.2;
+        state.totalPrice = state.basePrice + state.vat;
+      })
+
+      .addCase(createshoppingCart.pending, (state) => {
+        state.createshoppingCart.loading = true;
+        state.createshoppingCart.error = null;
+      })
+      .addCase(
+        createshoppingCart.fulfilled,
+        (state, action: PayloadAction<createShoppingCart>) => {
+          state.createshoppingCart.loading = false;
+          state.createshoppingCart.data = action.payload;
+        }
+      )
+      .addCase(createshoppingCart.rejected, (state, action) => {
+        state.createshoppingCart.loading = false;
+        state.createshoppingCart.error = action.payload;
+      })
+
+
+      .addCase(removefromshoppingCart.rejected, (state, action) => {
+        state.createshoppingCart.loading = false;
+        state.createshoppingCart.error = action.payload;
+      })
+      .addCase(removefromshoppingCart.pending, (state) => {
+        state.createshoppingCart.loading = true;
+        state.createshoppingCart.error = null;
+      })
+      .addCase(removefromshoppingCart.fulfilled, (state, action) => {
+        state.createshoppingCart.loading = false;
+        state.createshoppingCart.data = action.payload;
+      });
+  },
+});
+
+export default shoppingCartSlice.reducer;
+export const shoppingCartReducer = shoppingCartSlice.reducer;
+export const { setDisplayCart, clearCreateError } = shoppingCartSlice.actions;
