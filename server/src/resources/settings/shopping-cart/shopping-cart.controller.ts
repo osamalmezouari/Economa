9793bcec78ec0b80608e08e33eb5d625fe42872a
Osama Lmezouari <Oussamalm147@gmail.com>
2024-12-12 23:46:58 +0100

    🔧 feat: Add authentication and activeUser decorator to ShoppingCartController

--- server/src/resources/settings/shopping-cart/shopping-cart.controller.ts
+++ server/src/resources/settings/shopping-cart/shopping-cart.controller.ts
@@ -10,22 +10,25 @@ import {
 import { ShoppingCartService } from './shopping-cart.service';
 import { CreateShoppingCartDto } from './dto/create-shopping-cart.dto';
 import { UpdateShoppingCartDto } from './dto/update-shopping-cart.dto';
+import { AUTH } from 'src/common/decorators/meta/authentication.decorator';
+import { AuthenticationType } from 'src/common/enums/authentication';
+import { activeUser } from 'src/common/decorators/params/activeUser.decorator';
 
 @Controller('shopping-cart')
 export class ShoppingCartController {
   constructor(private readonly shoppingCartService: ShoppingCartService) {}
 
-  @Post()
-  async create(@Body() createShoppingCartDto: CreateShoppingCartDto) {
-    const shoppingCart = await this.shoppingCartService.create(
-      createShoppingCartDto,
-    );
-    return shoppingCart;
-  }
-
   @Get()
   async findAll() {
     const shoppingCarts = await this.shoppingCartService.findAll();
+    return shoppingCarts;
+  }
+
+  @AUTH(AuthenticationType.bearer)
+  @Get('WithProducts') // shopping cart from a single user with some details of product like unit | price with discount | product name | product image
+  async findAllWithProducts(@activeUser('sub') userId: string) {
+    const shoppingCarts =
+      await this.shoppingCartService.findByUserIdWithProducts(userId);
     return shoppingCarts;
   }
 
@@ -35,20 +38,28 @@ export class ShoppingCartController {
     return shoppingCart;
   }
 
-  @Get('user/:userId')
-  async findByUserId(@Param('userId') userId: string) {
-    const shoppingCarts = await this.shoppingCartService.findByUserId(userId);
-    return shoppingCarts;
+  @Post()
+  async create(
+    @Body() createShoppingCartDto: CreateShoppingCartDto,
+    @activeUser('sub') userId: string,
+  ) {
+    const shoppingCart = await this.shoppingCartService.create(
+      userId,
+      createShoppingCartDto,
+    );
+    return shoppingCart;
   }
 
   @Patch(':id')
   async update(
     @Param('id') id: string,
-    @Body() updateShoppingCartDto: UpdateShoppingCartDto,
+    @activeUser('sub') userId: string,
+    @Body() body: { quantity: number }, // Corrected: body instead of quantity
   ) {
     const shoppingCart = await this.shoppingCartService.update(
       id,
-      updateShoppingCartDto,
+      userId,
+      body.quantity, // Access the quantity directly here
     );
     return shoppingCart;
   }
