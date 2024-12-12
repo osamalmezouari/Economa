10fa73ed6ed5b012680c01277443d163768b9cd7
Osama Lmezouari <Oussamalm147@gmail.com>
2024-12-12 23:37:20 +0100

    ➕ Add shoppingCartReducer to rootReducer
        Integrated shoppingCartReducer into the rootReducer to manage shopping cart state.

--- client/src/app/rootReducer.ts
+++ client/src/app/rootReducer.ts
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 import { categoryReducer } from '../features/category/categorySlice';
 import { productsReducer } from '../features/products/productSlice';
 import { authReducer } from '../features/auth/authSlice';
+import { shoppingCartReducer } from '../features/shoppingCart/shoppingCartSlice';
 
 const rootReducer = combineReducers({
-    category: categoryReducer,
-    products: productsReducer,
-    auth : authReducer
+  category: categoryReducer,
+  products: productsReducer,
+  auth: authReducer,
+  shoppingCart: shoppingCartReducer,
 });
 export default rootReducer;
