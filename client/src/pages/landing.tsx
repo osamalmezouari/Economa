56f3ecdc3fd10b726570b56c5b0a77bffa847ef0
Osama Lmezouari <Oussamalm147@gmail.com>
2024-12-12 23:42:35 +0100

    🛒 feat: Add ShoppingCartPreview component to Landing page

--- client/src/pages/landing.tsx
+++ client/src/pages/landing.tsx
@@ -2,6 +2,7 @@ import Footer from '../components/extra/footer/footer';
 import OfferBanner from '../components/extra/offerBanner/offerBanner';
 import PromoCardWraper from '../components/extra/PromoCard/PromoCard';
 import ServicesContainer from '../components/extra/servicesContainer/ServicesContainer';
+import ShoppingCartPreview from '../components/extra/shoppingCart/shoopingCart';
 import Home from '../layouts/Home';
 import NewArrivalsSection from '../layouts/newArrivalsSection';
 import ProductsWithDiscount from '../layouts/ProductsWithDiscount';
@@ -16,6 +17,7 @@ const Landing = () => {
       <OfferBanner />
       <ServicesContainer />
       <Footer />
+      
     </>
   );
 };
