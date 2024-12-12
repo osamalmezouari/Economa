cf2264e8f8ed736527a5e266c4a045ab97c3f6c0
Osama Lmezouari <Oussamalm147@gmail.com>
2024-12-12 23:44:33 +0100

    🔧 fix: Update error message and errorCode in SHOPPING_CART_NOT_FOUND_Exception

--- server/src/common/exceptions/SHOPPING_CART_NOT_FOUND.exception.ts
+++ server/src/common/exceptions/SHOPPING_CART_NOT_FOUND.exception.ts
@@ -5,8 +5,8 @@ export class SHOPPING_CART_NOT_FOUND_Exception extends HttpException {
     super(
       {
         statusCode: HttpStatus.NOT_FOUND,
-        message: `No user found with the provided id : ${Shopping_cartId}`,
-        errorCode: 'USER_NOT_FOUND',
+        message: `No SHOPPING_CART found with the provided id : ${Shopping_cartId}`,
+        errorCode: 'SHOPPING_CART_NOT_FOUND',
         data: { Shopping_cartId },
       },
       HttpStatus.NOT_FOUND,
