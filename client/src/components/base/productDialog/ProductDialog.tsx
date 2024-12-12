03a1169ccbdd3703797963f6505e03cd8519dcd8
Osama Lmezouari <Oussamalm147@gmail.com>
2024-12-12 23:39:07 +0100

    🛒 feat: Add product to cart with quantity adjustments and error handling

--- client/src/components/base/productDialog/ProductDialog.tsx
+++ client/src/components/base/productDialog/ProductDialog.tsx
@@ -11,11 +11,21 @@ import {
   InputAdornment,
   Button,
   Badge,
+  CircularProgress,
+  Alert,
 } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
 import { BiMinus, BiPlus } from 'react-icons/bi';
 import { ShoppingBagOutlined } from '@mui/icons-material';
 import { ProductDialogProps } from '../../../types/product';
+import {
+  createshoppingCart,
+  getshoppingCart,
+} from '../../../features/shoppingCart/shoppingCartThunk';
+import { AppDispatch, RootState } from '../../../app/store';
+import { useDispatch, useSelector } from 'react-redux';
+import { ApiError } from '../../../types/error';
+import { clearCreateError } from '../../../features/shoppingCart/shoppingCartSlice';
 
 export default function ProductDialog({
   open,
@@ -30,121 +40,172 @@ export default function ProductDialog({
   unit,
   imageLink,
 }: ProductDialogProps) {
+  const dispatch = useDispatch<AppDispatch>();
+  const quantity = useSelector((state: RootState) => {
+    const item = state.shoppingCart.shoppingCartWithProducts.data.find(
+      (item) => item.productId === id
+    );
+    return item ? item.quantity : 1;
+  });
+  const { loading, error } = useSelector(
+    (state: RootState) => state.shoppingCart.createshoppingCart
+  );
+  const [newquantity, setQuantity] = React.useState(quantity);
+  const incrementQuantity = () => {
+    setQuantity((prev: number) => prev + 1);
+  };
+  const decrementQuantity = () => {
+    setQuantity((prev: number) => (prev > 1 ? prev - 1 : 1));
+  };
+
+  const addToCart = async () => {
+    const cartItem = {
+      productId: id,
+      quantity: newquantity,
+    };
+    await dispatch(createshoppingCart(cartItem));
+    await dispatch(getshoppingCart());
+  };
+
   return (
-    <Dialog
-      className="h-full"
-      onClose={() => setopen(false)}
-      aria-labelledby="customized-dialog-title"
-      open={open}
-      maxWidth="lg"
-      fullWidth
-      sx={{
-        '& .MuiDialog-paper': {
-          maxWidth: '800px',
-          borderRadius: '8px',
-        },
-      }}
-    >
-      <DialogContent
+    <>
+      {' '}
+      <Dialog
         className="h-full"
+        onClose={async () => {
+          await dispatch(clearCreateError());
+          setopen(false);
+        }}
+        aria-labelledby="customized-dialog-title"
+        open={open}
+        maxWidth="lg"
+        fullWidth
         sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          height: '100%',
-          padding: '10px',
+          '& .MuiDialog-paper': {
+            maxWidth: '800px',
+            borderRadius: '8px',
+          },
         }}
       >
-        <IconButton
-          aria-label="close"
-          onClick={() => setopen(false)}
-          sx={(theme) => ({
-            position: 'absolute',
-            right: 8,
-            top: 8,
-            color: theme.palette.grey[500],
-          })}
+        <DialogContent
+          className="h-full"
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            height: '100%',
+            padding: '10px',
+          }}
         >
-          <CloseIcon />
-        </IconButton>
-        <Grid container spacing={2}>
-          <Grid item xs={12} md={5.5}>
-            <div
-              className="relative w-full h-full border-2 bg-green-50 bg-cover bg-center"
-              style={{
-                backgroundImage: `url(${imageLink})`,
-              }}
-            >
-              {discount > 0 && (
-                <div className="absolute top-1 right-2">
-                  <Badge className="bg-red-400 text-white rounded-sm px-2 uppercase text-[12px]">
-                    - {discount} %
+          <IconButton
+            aria-label="close"
+            onClick={async () => {
+              await dispatch(clearCreateError());
+              setopen(false);
+            }}
+            sx={(theme) => ({
+              position: 'absolute',
+              right: 8,
+              top: 8,
+              color: theme.palette.grey[500],
+            })}
+          >
+            <CloseIcon />
+          </IconButton>
+          <Grid container spacing={2}>
+            <Grid item xs={12} md={5.5}>
+              <div
+                className="relative w-full h-full border-2 bg-green-50 bg-cover bg-center"
+                style={{
+                  backgroundImage: `url(${imageLink})`,
+                }}
+              >
+                {discount > 0 && (
+                  <div className="absolute top-1 right-2">
+                    <Badge className="bg-red-400 text-white rounded-sm px-2 uppercase text-[12px]">
+                      - {discount} %
+                    </Badge>
+                  </div>
+                )}
+                <div className="absolute top-1 left-2">
+                  <Badge className="bg-primary-main text-white rounded-sm px-2 uppercase text-[12px]">
+                    1 {unit}
                   </Badge>
                 </div>
-              )}
-              <div className="absolute top-1 left-2">
-                <Badge className="bg-primary-main  text-white rounded-sm px-2 uppercase text-[12px]">
-                  1 {unit}
-                </Badge>
               </div>
-            </div>
-          </Grid>
-          <Grid item xs={12} md={6} className="p-4">
-            <p className="tracking-wide font-normal text-secondary-main text-2xl mt-4 cursor-pointer hover:text-primary-main transition-all duration-300">
-              {name}
-            </p>
-            <Rating
-              name="half-rating"
-              size="small"
-              defaultValue={productAvgRaiting}
-              precision={0.5}
-              className="py-2"
-            />
-            <p className="text-secondary-main py-4">{description}</p>
-            <Box className="flex gap-4 text-secondary-main pb-5">
-              {priceWithDiscount && priceWithDiscount > 0 && (
-                <p className="text-secondary-main font-extrabold text-2xl">
-                  ${priceWithDiscount}
-                </p>
-              )}
-              <p
-                className={`font-light text-secondary-main text-2xl ${
-                  discount > 0 ? 'line-through' : 'hidden'
-                }`}
-              >
-                ${price}
+            </Grid>
+            <Grid item xs={12} md={6} className="p-4">
+              <p className="tracking-wide font-normal text-secondary-main text-2xl mt-4 cursor-pointer hover:text-primary-main transition-all duration-300">
+                {name}
               </p>
-            </Box>
-
-            <DialogActions sx={{ padding: '5px !important' }}>
-              <TextField
-                className="w-16 text-secondary-main"
-                defaultValue={5}
-                variant="outlined"
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      <BiPlus className="hover:cursor-pointer hover:bg-primary-main hover:rounded-full hover:text-white duration-300 transition-all" />
-                    </InputAdornment>
-                  ),
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <BiMinus className="hover:cursor-pointer hover:bg-primary-main hover:rounded-full hover:text-white duration-300 transition-all" />
-                    </InputAdornment>
-                  ),
-                }}
+              <Rating
+                name="half-rating"
+                size="small"
+                defaultValue={productAvgRaiting}
+                precision={0.5}
+                className="py-2"
               />
-              <Button
-                className="h-[56px] hover:bg-primary-main"
-                startIcon={<ShoppingBagOutlined />}
-                color="secondary"
-                variant="contained"
-              >
-                Add to Cart
-              </Button>
-            </DialogActions>
+              <p className="text-secondary-main py-4">{description}</p>
+              <Box className="flex gap-4 text-secondary-main pb-5">
+                {priceWithDiscount && priceWithDiscount > 0 && (
+                  <p className="text-secondary-main font-extrabold text-2xl">
+                    ${priceWithDiscount}
+                  </p>
+                )}
+                <p
+                  className={`font-light text-secondary-main text-2xl ${
+                    discount > 0 ? 'line-through' : 'hidden'
+                  }`}
+                >
+                  ${price}
+                </p>
+              </Box>
+              {(error as ApiError) && (
+                <Alert severity="info">{(error as ApiError).message}</Alert>
+              )}
+
+              <DialogActions sx={{ padding: '5px !important' }}>
+                <TextField
+                  className="w-24 text-secondary-main"
+                  value={newquantity}
+                  variant="outlined"
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <BiPlus
+                          className="hover:cursor-pointer hover:bg-primary-main hover:rounded-full hover:text-white duration-300 transition-all"
+                          onClick={incrementQuantity}
+                        />
+                      </InputAdornment>
+                    ),
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <BiMinus
+                          className="hover:cursor-pointer hover:bg-primary-main hover:rounded-full hover:text-white duration-300 transition-all"
+                          onClick={decrementQuantity}
+                        />
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+                <Button
+                  className="h-[56px] hover:bg-primary-main w-[150px]"
+                  startIcon={<ShoppingBagOutlined />}
+                  color="secondary"
+                  variant="contained"
+                  onClick={addToCart}
+                  disabled={loading}
+                >
+                  {loading ? (
+                    <CircularProgress size={24} color="inherit" />
+                  ) : (
+                    'Add to Cart'
+                  )}
+                </Button>
+              </DialogActions>
+            </Grid>
           </Grid>
-        </Grid>
-      </DialogContent>
-    </Dialog>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 }
