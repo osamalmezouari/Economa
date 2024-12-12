d721410a42fbb46ecf917b452de97f496cfab682
Osama Lmezouari <Oussamalm147@gmail.com>
2024-12-12 23:36:53 +0100

    ✨ Add shopping cart API functions for CRUD operations
        Implemented shoppingCart, createshoppingCart, updatequantityapi, and removefromshoppingCart functions for interacting with the shopping cart API.
        Added error handling for API requests to improve reliability.

---
+++ client/src/api/shoopingCart.ts
@@ -0,0 +1,60 @@
+import { createShoppingCart, ShoopingCartType } from '../types/shoppingCart';
+import { apiClient } from '../utils/apiClient';
+
+export const shoppingCart = async (): Promise<ShoopingCartType[]> => {
+  try {
+    const response = await apiClient.get<ShoopingCartType[]>(
+      '/shopping-cart/WithProducts'
+    );
+    return response.data;
+  } catch (error: any) {
+    if (error.response) {
+      throw error.response.data;
+    }
+    throw new Error('Failed to Fetch shoppingCart');
+  }
+};
+
+export const createshoppingCart = async (
+  data: createShoppingCart
+): Promise<createShoppingCart> => {
+  try {
+    const response = await apiClient.post<createShoppingCart>(
+      '/shopping-cart',
+      data
+    );
+    return response.data;
+  } catch (error: any) {
+    if (error.response) {
+      throw error.response.data;
+    }
+    throw new Error('Failed to create shoppingCart');
+  }
+};
+
+export const updatequantityapi = async (id: string, quantity: number) => {
+  try {
+    const response = await apiClient.patch(`/shopping-cart/${id}`, {
+      quantity,
+    });
+    return response.data;
+  } catch (error: any) {
+    if (error.response) {
+      throw error.response.data;
+    }
+    throw new Error('Failed to update quantity');
+  }
+};
+
+
+export const removefromshoppingCart = async (id: string) => {
+  try {
+    const response = await apiClient.delete(`/shopping-cart/${id}`); 
+    return response.data;
+  } catch (error: any) {
+    if (error.response) {
+      throw error.response.data;
+    }
+    throw new Error('Failed to remove from shoppingCart');
+  }
+};
