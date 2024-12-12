db201796286fac943c9f8c44a7bc326b89f1f0b7
Osama Lmezouari <Oussamalm147@gmail.com>
2024-12-12 23:36:31 +0100

    ♻️ Refactor import order for improved readability
        Reordered imports to group and prioritize modules for better code organization.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
-import Landing from './pages/landing';
 import ThemeProvider from './theme';
 import { BrowserRouter } from 'react-router-dom';
+import Landing from './pages/landing';
 
 export default function App() {
   return (
