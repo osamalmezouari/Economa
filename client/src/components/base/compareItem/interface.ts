ea0eed07e991d32c8fc6c11ca01ab7db41cb4610
Osama Lmezouari <Oussamalm147@gmail.com>
2024-12-14 23:37:25 +0100

    ✨ **feat:** Added `CompareItemProps` interface for product comparison data structure - From: "No previous interface" - To: "Created interface to define the structure of `CompareItem` component props"

---
+++ client/src/components/base/compareItem/interface.ts
@@ -0,0 +1,12 @@
+export interface CompareItemProps {
+  svgLink: string;
+  productName: string;
+  productId: string;
+  categoryName: string;
+  rating: number;
+  reviewsCount: number;
+  price: number;
+  weight: string;
+  description: string;
+  stock: number;
+}
