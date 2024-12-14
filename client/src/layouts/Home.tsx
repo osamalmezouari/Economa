e88affc40f7396df2a99da0a037de96103504198
Osama Lmezouari <Oussamalm147@gmail.com>
2024-12-14 23:42:19 +0100

    ✨ **refactor:** Optimized import statement for clarity - From: "Multiple imports on separate lines" - To: "Grouped related imports for better readability"

--- client/src/layouts/Home.tsx
+++ client/src/layouts/Home.tsx
@@ -2,10 +2,12 @@ import Navbar from '../components/extra/navbar/Navbar';
 import HeroImage from '../components/base/hero-image/heroImage';
 import CategoryCardContainer from '../components/extra/categoryCardContainer/categoryCardContainer';
 import ShoppingCart from '../components/extra/shoppingCart/shoopingCart';
+import Wishlist from '../components/extra/wishlist/wishlist.tsx';
 const Home = () => {
   return (
     <>
       <ShoppingCart />
+      <Wishlist />
       <Navbar />
       <HeroImage />
       <CategoryCardContainer />
