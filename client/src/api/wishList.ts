80949565c0ef994b0db92a0f96029097630be5fe
Osama Lmezouari <Oussamalm147@gmail.com>
2024-12-14 23:35:49 +0100

    ✨ feat: Added Wishlist API functions for fetching, creating, and removing wishlist items
        From: "No previous implementation"
        To: "Implemented functions to handle wishlist operations (GetWishList, createWishList, removefromWishList)"

---
+++ client/src/api/wishList.ts
@@ -0,0 +1,42 @@
+import { WishlistType } from '../types/wishlist';
+import { apiClient } from '../utils/apiClient';
+
+export const GetWishList = async (): Promise<WishlistType[]> => {
+  try {
+    const response = await apiClient.get('/wishlist/withproducts');
+    return response.data;
+  } catch (error: any) {
+    if (error.response) {
+      throw error.response.data;
+    }
+    throw new Error('Failed to Fetch wishList');
+  }
+};
+
+export const createWishList = async (
+  productId: string
+): Promise<WishlistType> => {
+  try {
+    const response = await apiClient.post(`/wishlist`, {
+      productId,
+    });
+    return response.data;
+  } catch (error: any) {
+    if (error.response) {
+      throw error.response.data;
+    }
+    throw new Error('Failed to add to wishList');
+  }
+};
+
+export const removefromWishList = async (id: string): Promise<WishlistType> => {
+  try {
+    const response = await apiClient.delete(`/wishlist/${id}`);
+    return response.data;
+  } catch (error: any) {
+    if (error.response) {
+      throw error.response.data;
+    }
+    throw new Error('Failed to remove from wishList');
+  }
+};
