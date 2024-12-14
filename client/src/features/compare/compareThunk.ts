6048f2f7e030d7190b8f7320fb08709fe2ad1661
Osama Lmezouari <Oussamalm147@gmail.com>
2024-12-14 23:41:04 +0100

    ✨ feat: Added async thunk for fetching compared product details
        From: "No async action for fetching compared products"
        To: "Implemented async thunk for fetching compared product details from API"

---
+++ client/src/features/compare/compareThunk.ts
@@ -0,0 +1,19 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import * as compareApi from '../../api/compare';
+
+export const getComparedProductDetails = createAsyncThunk(
+  'get/products/ComparedProductDetails',
+  async (ids: string[], { rejectWithValue }) => {
+    try {
+      const comparedData = await compareApi.getComparedProductDetails(ids);
+      return comparedData;
+    } catch (error: any) {
+      if (error) {
+        return rejectWithValue(error);
+      }
+      return rejectWithValue({
+        message: error.message || 'Something went wrong.',
+      });
+    }
+  }
+);
