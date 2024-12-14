8d1671ffe8c8f00867a76c2f8e35c9b6ff54d197
Osama Lmezouari <Oussamalm147@gmail.com>
2024-12-14 23:48:55 +0100

    ✨ Remove userId from CreateWishlistDto as it's handled by the activeUser decorator

--- server/src/resources/settings/wishlist/dto/create-wishlist.dto.ts
+++ server/src/resources/settings/wishlist/dto/create-wishlist.dto.ts
@@ -3,9 +3,5 @@ import { IsNotEmpty, IsUUID } from 'class-validator';
 export class CreateWishlistDto {
   @IsUUID()
   @IsNotEmpty()
-  userId: string;
-
-  @IsUUID()
-  @IsNotEmpty()
   productId: string;
 }
