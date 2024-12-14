0bfd45069fdc6d1eec1ba004b15d129a5cb4c58e
Osama Lmezouari <Oussamalm147@gmail.com>
2024-12-14 23:17:58 +0100

    ✨ feat: Switched landing page to compare component
        From: <Landing />
        To: <Compare />

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,11 @@
 import ThemeProvider from './theme';
 import { BrowserRouter } from 'react-router-dom';
-import Landing from './pages/landing';
-
+import Compare from './components/extra/compare/compare';
 export default function App() {
   return (
     <ThemeProvider>
       <BrowserRouter>
-        <Landing />
+        <Compare />
       </BrowserRouter>
     </ThemeProvider>
   );
