b276be530f9e5dbba1bf6a3dee20605fb50e5e08
Osama Lmezouari <Oussamalm147@gmail.com>
2024-12-14 23:42:02 +0100

    ✨ **refactor:** Simplified error handling and added consistent message return - From: "Error handling with repeated checks for `error` in multiple thunks" - To: "Standardized error handling in each `createAsyncThunk` with a fallback error message"

---
+++ client/src/features/wishlist/wishlistThunk.ts
@@ -0,0 +1,53 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import * as wishListApi from '../../api/wishList';
+
+export const getWishlist = createAsyncThunk(
+  'wishlist/getWishlist',
+  async (_, { rejectWithValue }) => {
+    try {
+      const wishlist = await wishListApi.GetWishList();
+      return wishlist;
+    } catch (error: any) {
+      if (error) {
+        return rejectWithValue(error);
+      }
+      return rejectWithValue({
+        message: error.message || 'Something went wrong.',
+      });
+    }
+  }
+);
+
+export const createWishList = createAsyncThunk(
+  'wishlist/createWishList',
+  async (productId: string, { rejectWithValue }) => {
+    try {
+      const wishlist = await wishListApi.createWishList(productId);
+      return wishlist;
+    } catch (error: any) {
+      if (error) {
+        return rejectWithValue(error);
+      }
+      return rejectWithValue({
+        message: error.message || 'Something went wrong.',
+      });
+    }
+  }
+);
+
+export const removefromWishList = createAsyncThunk(
+  'wishlist/removefromWishList',
+  async (id: string, { rejectWithValue }) => {
+    try {
+      const wishlist = await wishListApi.removefromWishList(id);
+      return wishlist;
+    } catch (error: any) {
+      if (error) {
+        return rejectWithValue(error);
+      }
+      return rejectWithValue({
+        message: error.message || 'Something went wrong.',
+      });
+    }
+  }
+);
