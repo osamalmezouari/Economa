c410510405d7d6df59a78f375cb4dbdc7bc72d91
Osama Lmezouari <Oussamalm147@gmail.com>
2024-12-14 23:42:59 +0100

    ✨ **refactor:** Enhanced type structure for consistency and clarity - From: "Multiple types with nullable or unknown error fields" - To: "Refined and grouped types for wishlist and actions consistency"

---
+++ client/src/types/wishlist.ts
@@ -0,0 +1,24 @@
+import { WishlistItemProps } from '../components/base/wishlistItem/interfaces';
+import { ApiError } from './error';
+
+export interface WishlistType extends WishlistItemProps {}
+
+export interface WishlistState {
+  wishlist: {
+    data: WishlistType[];
+    loading: boolean;
+    error: ApiError | unknown | null | undefined;
+  };
+  createWishList: {
+    data: WishlistType |  {};
+    loading: boolean;
+    error: ApiError | unknown | null | undefined;
+  };
+  removefromWishList: {
+    data: WishlistType | {};
+    loading: boolean;
+    error: ApiError | unknown | null | undefined;
+  };
+  open: boolean
+
+}
