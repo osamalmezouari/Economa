a644d108e3a8c875053ac76758d8b7d2da865de2
Osama Lmezouari <Oussamalm147@gmail.com>
2024-12-15 22:42:11 +0100

    🔄 Update Compare component to Landing page in App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,12 @@
 import ThemeProvider from './theme';
 import { BrowserRouter } from 'react-router-dom';
+import Landing from './pages/landing';
 import Compare from './components/extra/compare/compare';
 export default function App() {
   return (
     <ThemeProvider>
       <BrowserRouter>
-        <Compare />
+        <Landing />
       </BrowserRouter>
     </ThemeProvider>
   );
