4630cbdc954b80884a47a627f635f3021ecf1dee
Osama Lmezouari <Oussamalm147@gmail.com>
2024-12-18 20:57:44 +0100

    refactor: comment out navigation logic in CategoryCardSingle component

--- client/src/components/base/category-card/categoryCard.tsx
+++ client/src/components/base/category-card/categoryCard.tsx
@@ -1,19 +1,18 @@
 import { Box } from "@mui/material";
 import Card from "@mui/material/Card";
 import { CategoryCardProps } from "../../../types/category";
-import { useNavigate } from "react-router-dom";
 
 const CategoryCardSingle = ({ svgLink, linkTo, productsCount, name }: CategoryCardProps) => {
     const calors: string[] = ['#e2fde2', '#fee9e8', '#fdf4ea', '#F9DEF2', 'fdf5eb']
     const randomColor = calors[Math.floor(Math.random() * calors.length - 1)];
-    const Navigate =  useNavigate()
+/*     const Navigate =  useNavigate() */
     return (
         <Box component={'div'} className={`p-3 rounded`} style={{ backgroundColor: `${randomColor}` }}>
             <Card
                 className="w-[140px]   bg-secondary-darker"
             >
                 <Box component={"div"} className="p-4 text-center grid justify-center gap-y-1">
-                   <Box component={'div'} className="w-12 m-auto h-12 bg-cover bg-center" style={{ backgroundImage: `url(${svgLink})` }} onClick={()=>Navigate(linkTo)}></Box>
+                   <Box component={'div'} className="w-12 m-auto h-12 bg-cover bg-center" style={{ backgroundImage: `url(${svgLink})` }} /* onClick={()=>Navigate(linkTo)} */></Box>
                     <Box component={"div"} className="font-secondary font-bold text-secondary-darker">
                         {name}
                     </Box>
