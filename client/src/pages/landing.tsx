c342d6e6ebef60fcf88c49c385723c5c28f81b40
Osama Lmezouari <Oussamalm147@gmail.com>
2024-12-18 20:58:39 +0100

    refactor: remove Footer and ShoppingCartPreview imports from Landing page for cleaner code

--- client/src/pages/landing.tsx
+++ client/src/pages/landing.tsx
@@ -1,8 +1,6 @@
-import Footer from '../components/extra/footer/footer';
 import OfferBanner from '../components/extra/offerBanner/offerBanner';
 import PromoCardWraper from '../components/extra/PromoCard/PromoCard';
 import ServicesContainer from '../components/extra/servicesContainer/ServicesContainer';
-import ShoppingCartPreview from '../components/extra/shoppingCart/shoopingCart';
 import Home from '../layouts/Home';
 import NewArrivalsSection from '../layouts/newArrivalsSection';
 import ProductsWithDiscount from '../layouts/ProductsWithDiscount';
@@ -15,9 +13,7 @@ const Landing = () => {
       <PromoCardWraper />
       <NewArrivalsSection />
       <OfferBanner />
-      <ServicesContainer />
-      <Footer />
-      
+      <ServicesContainer />      
     </>
   );
 };
