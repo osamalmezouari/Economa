21248ef641e6ff1803e85b40805a3ba9d78f1bef
Osama Lmezouari <Oussamalm147@gmail.com>
2024-12-18 20:57:59 +0100

    refactor: update DesktopNavBar component to use new icons and routing logic

--- client/src/components/base/navbars/desktop_nav_bar.tsx
+++ client/src/components/base/navbars/desktop_nav_bar.tsx
@@ -1,4 +1,3 @@
-import { Search } from '@mui/icons-material';
 import {
   Box,
   Button,
@@ -8,24 +7,41 @@ import {
   TextField,
 } from '@mui/material';
 import Grid from '@mui/material/Grid2';
-import { useNavigate } from 'react-router-dom';
-import { NavItemsProps } from './interfaces';
+import { BiSearch } from 'react-icons/bi';
+import { FiHome } from 'react-icons/fi';
+import { BiStoreAlt } from 'react-icons/bi';
+import { RiDiscountPercentLine } from 'react-icons/ri';
+import { LuGitCompare } from 'react-icons/lu';
+import { IoPersonOutline } from 'react-icons/io5';
+import { GrFavorite } from 'react-icons/gr';
+import { TbShoppingBag } from 'react-icons/tb';
+import { useDispatch } from 'react-redux';
+import { AppDispatch } from '../../../app/store';
+import { setDisplayCart } from '../../../features/shoppingCart/shoppingCartSlice';
+import { setDisplayWishlist } from '../../../features/wishlist/wishlistSlice';
+import { useRouter } from '@tanstack/react-router';
 
-const Desktop_nav_bar = ({ NavItems }: NavItemsProps) => {
-  const navigate = useNavigate();
+const Desktop_nav_bar = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const router = useRouter();
+
   return (
     <>
       <Stack className={'border-b-2 bg-gray'}>
         <Container style={{ maxWidth: '1200px' }}>
           <Grid container spacing={2} className={'px-8 items-center'}>
+            {/* Logo */}
             <Grid size={2}>
               <Box
                 component={'img'}
-                src={NavItems.logo}
-                className="w-[140px] h-[140px] scale-[1.5]"
+                src="/assets/images/logo.png"
+                className="w-[140px] h-[140px] scale-[1.5] cursor-pointer"
                 alt="logo"
+                onClick={() => router.navigate({ to: '/' })}
               />
             </Grid>
+
+            {/* Search Bar */}
             <Grid size={6}>
               <TextField
                 variant="outlined"
@@ -34,24 +50,59 @@ const Desktop_nav_bar = ({ NavItems }: NavItemsProps) => {
                 InputProps={{
                   endAdornment: (
                     <InputAdornment position="end">
-                      <Search />
+                      <BiSearch
+                        className={
+                          'hover:bg-primary-main rounded-full w-8 h-8 border-8 bg-secondary-main text-white transition-all duration-500 ease-in-out cursor-pointer hover:text-white border-secondary-main hover:border-primary-main '
+                        }
+                      />
                     </InputAdornment>
                   ),
                 }}
               />
             </Grid>
-            <Grid size={4} gap={'6'}>
-              {NavItems.ItemsRight.map((item) => {
-                return (
-                  <>
-                   {item.icon}
-                  </>
-                );
-              })}
+            <Grid size={4} className="flex justify-end gap-4">
+              {/* Login */}
+              <Button
+                variant="text"
+                className="w-28 bg-secondary-darker ml-2 h-14 hover:border-2"
+                startIcon={<IoPersonOutline />}
+                onClick={() =>
+                  router.navigate({
+                    to: '/login',
+                  })
+                }
+              >
+                <p className="text-secondary-darker font-secondary">Login</p>
+              </Button>
+              <Button
+                variant="text"
+                className="w-28 bg-secondary-darker ml-2 h-14 hover:border-2"
+                startIcon={<GrFavorite />}
+                onClick={() => {
+                  dispatch({ type: 'setDisplayCart', payload: false });
+                  dispatch(setDisplayWishlist());
+                }}
+              >
+                <p className="text-secondary-darker font-secondary">Wishlist</p>
+              </Button>
+
+              {/* Cart */}
+              <Button
+                variant="text"
+                className="w-28 bg-secondary-darker ml-2 h-14 hover:border-2"
+                startIcon={<TbShoppingBag />}
+                onClick={() => {
+                  dispatch({ type: 'setDisplayWishlist', payload: false });
+                  dispatch(setDisplayCart());
+                }}
+              >
+                <p className="text-secondary-darker font-secondary">Cart</p>
+              </Button>
             </Grid>
           </Grid>
         </Container>
       </Stack>
+
       <Grid
         container
         sx={{
@@ -64,20 +115,35 @@ const Desktop_nav_bar = ({ NavItems }: NavItemsProps) => {
           borderStyle: 'solid',
         }}
       >
-        {NavItems.itemsCenter.map((item) => {
-          return (
-            <>
-              {' '}
-              <Button
-                className="flex gap-2 w-28 bg-main-main ml-2 h-14"
-                startIcon={item.icon}
-                onClick={() => navigate(item.link)}
-              >
-                <p className="text-secondary-darker font-main">{item.name}</p>
-              </Button>
-            </>
-          );
-        })}
+        {' '}
+        <Button
+          className="flex gap-2 w-28 bg-main-main ml-2 h-14"
+          startIcon={<FiHome />}
+          onClick={() => router.navigate({ to: '/' })}
+        >
+          <p className="text-secondary-darker font-main">Home</p>
+        </Button>
+        <Button
+          className="flex gap-2 w-28 bg-main-main ml-2 h-14"
+          startIcon={<BiStoreAlt />}
+          /* onClick={() => router.navigate({ to: '/store' })} */
+        >
+          <p className="text-secondary-darker font-main">Store</p>
+        </Button>
+        <Button
+          className="flex gap-2 w-28 bg-main-main ml-2 h-14"
+          startIcon={<RiDiscountPercentLine />}
+          /* onClick={() => router.navigate({ to: '/offers' })} */
+        >
+          <p className="text-secondary-darker font-main">Offers</p>
+        </Button>
+        <Button
+          className="flex gap-2 w-28 bg-main-main ml-2 h-14"
+          startIcon={<LuGitCompare />}
+          onClick={() => router.navigate({ to: '/compare' })}
+        >
+          <p className="text-secondary-darker font-main">Compare</p>
+        </Button>
       </Grid>
     </>
   );
