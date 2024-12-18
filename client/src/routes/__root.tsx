112412b492e759997a77140dbb22dabf7ed8bfe0
Osama Lmezouari <Oussamalm147@gmail.com>
2024-12-18 20:58:54 +0100

    feat: add root route with Navbar and Footer components for consistent layout

---
+++ client/src/routes/__root.tsx
@@ -0,0 +1,33 @@
+import { Outlet, createRootRoute, createRouter } from '@tanstack/react-router';
+import Navbar from '../components/extra/navbar/Navbar';
+import Footer from '../components/extra/footer/footer';
+import { LoginRoute, RegisterRoute } from './auth';
+import { indexRoute } from './landing';
+import { compareRoute } from './compare';
+
+export const rootRoute = createRootRoute({
+  component: RootComponent,
+});
+
+function RootComponent() {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+}
+
+const routeTree = rootRoute.addChildren([
+  RegisterRoute,
+  LoginRoute,
+  indexRoute,
+  compareRoute,
+]);
+export const router = createRouter({ routeTree, defaultPreload: 'intent' });
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router;
+  }
+}
