f5cc04a13bea918438aede42903d2c0ed1a10509
Osama Lmezouari <Oussamalm147@gmail.com>
2024-12-18 20:57:54 +0100

    style: adjust HeroImage component layout for improved responsiveness

--- client/src/components/base/hero-image/heroImage.tsx
+++ client/src/components/base/hero-image/heroImage.tsx
@@ -3,7 +3,7 @@ import { Box, Button, Typography } from "@mui/material"
 import { Image_bg_hero } from "../../../mock/constants"
 const HeroImage = () => {
     return <>
-        <Box className={'rounded bg-cover mt-10 bg-center max-w-[1400px] h-[600px] m-auto grid grid-cols-2 grid-rows-3 p-4 sm:p-24'} component={'div'} style={{ backgroundImage: `url(${Image_bg_hero.src})` }} >
+        <Box className={'rounded bg-cover mt-10 bg-center max-w-[1300px] h-[600px] m-auto grid grid-cols-2  p-4 sm:px-24 sm:pt-4'} component={'div'} style={{ backgroundImage: `url(${Image_bg_hero.src})` }} >
             <Box className=" row-start-2 md:col-span-1 col-span-2 ">
                 <Typography variant="h4" className="-tracking-tighter font-secondary" color="primary" >Starting at {`10.00`}$</Typography>
                 <Typography variant="h1" className="-tracking-tighter text-secondary-main" >Explore fresh & juicy fruits</Typography>
