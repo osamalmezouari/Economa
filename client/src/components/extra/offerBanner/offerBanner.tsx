38f8ee8f84c8c5bea1d6926ee58323970db957cd
Osama Lmezouari <Oussamalm147@gmail.com>
2024-12-20 22:22:11 +0100

    feat: wrap PromoCard in Box component for improved layout and styling

--- client/src/components/extra/offerBanner/offerBanner.tsx
+++ client/src/components/extra/offerBanner/offerBanner.tsx
@@ -1,8 +1,12 @@
+import { Box } from '@mui/material';
 import PromoCard from '../../base/PromoCard/PromoCard';
 
 const OfferBanner = () => {
   return (
+    <Box className="w-[1200px] m-auto" component={'div'}>
     <PromoCard image={'https://github.com/osamalmezouari/ecommerce/blob/master/client/public/assets/images/offerbanner.png?raw=true'} title={''} subtitle={''} discountText={''} buttonText={'none'} />
+
+    </Box>
   );
 };
 
