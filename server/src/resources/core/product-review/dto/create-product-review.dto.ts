91226820f4b4a6ceb272c827ec9783504d07c927
Osama Lmezouari <Oussamalm147@gmail.com>
2024-12-26 20:32:28 +0100

    feat: add transformation for rating in CreateProductReviewDto

--- server/src/resources/core/product-review/dto/create-product-review.dto.ts
+++ server/src/resources/core/product-review/dto/create-product-review.dto.ts
@@ -1,17 +1,16 @@
 import { IsOptional, IsString, IsInt, Min, Max, IsUUID } from 'class-validator';
+import { Transform } from 'class-transformer';
 
 export class CreateProductReviewDto {
   @IsUUID()
   userId: string;
-
   @IsUUID()
   productId: string;
-
   @IsInt()
   @Min(1)
   @Max(5)
+  @Transform(({ value }) => parseInt(value))
   rating: number;
-
   @IsOptional()
   @IsString()
   reviewText?: string;
