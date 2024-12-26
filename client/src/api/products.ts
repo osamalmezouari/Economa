c18d4924a5a63f1a8ba0a6fe920a951242a9a6cf
Osama Lmezouari <Oussamalm147@gmail.com>
2024-12-26 20:31:19 +0100

    feat: add API function to fetch products for the store with pagination

--- client/src/api/products.ts
+++ client/src/api/products.ts
@@ -1,5 +1,21 @@
-import { ProductCardType, ProductsNewArrivals } from '../types/product';
+import {
+  ProductCardType,
+  ProductsNewArrivals,
+  ProductStoreType,
+} from '../types/product';
 import { apiClient } from '../utils/apiClient';
+
+export const getProductsStore = async (page : number): Promise<ProductStoreType[]> => {
+  try {
+    const response = await apiClient.get<ProductStoreType[]>(`products/store?page=${page}`);
+    return response.data;
+  } catch (error: any) {
+    if (error.response) {
+      throw error.response.data;
+    }
+    throw new Error('Failed to Fetch products');
+  }
+};
 
 export const getProductsCards = async (): Promise<ProductCardType[]> => {
   try {
