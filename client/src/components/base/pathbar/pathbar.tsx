eb90dc9eafdb9c71f7766e7ffb336e31b5d3ab43
Osama Lmezouari <Oussamalm147@gmail.com>
2024-12-26 20:31:25 +0100

    fix: update PathBar component styling for improved layout

--- client/src/components/base/pathbar/pathbar.tsx
+++ client/src/components/base/pathbar/pathbar.tsx
@@ -4,7 +4,7 @@ const PathBar = ({ path }: { path: string }) => {
     const pathName = path.split('>')[1]
 
     return (
-        <Box className="flex max-w-[1200px] m-auto justify-between items-center rounded border-[1px]  p-4  rounded-b-xl" component={'div'}  >
+        <Box className="flex max-w-[1200px] m-auto justify-between items-center rounded border-[1px]  p-4 rounded-t-none  rounded-b" component={'div'}  >
             <Typography variant="h4" color="primary.main">
                 {pathName}
             </Typography>
