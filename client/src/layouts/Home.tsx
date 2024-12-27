40030b88f1b2e8fe57afde9dd7eb07f2eab87485
Osama Lmezouari <Oussamalm147@gmail.com>
2024-12-27 21:42:37 +0100

    refactor: remove ShoppingCart and Wishlist components from Home layout

--- client/src/layouts/Home.tsx
+++ client/src/layouts/Home.tsx
@@ -1,4 +1,3 @@
-import Navbar from '../components/extra/navbar/Navbar';
 import HeroImage from '../components/base/hero-image/heroImage';
 import CategoryCardContainer from '../components/extra/categoryCardContainer/categoryCardContainer';
 import ShoppingCart from '../components/extra/shoppingCart/shoopingCart';
@@ -6,8 +5,7 @@ import Wishlist from '../components/extra/wishlist/wishlist.tsx';
 const Home = () => {
   return (
     <>
-      <ShoppingCart />
-      <Wishlist />
+      
       <HeroImage />
       <CategoryCardContainer />
     </>
