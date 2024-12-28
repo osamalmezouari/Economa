b9e594c3f50ae669521fb8019a8b2187a93e2ee6
Osama Lmezouari <Oussamalm147@gmail.com>
2024-12-28 23:03:02 +0100

    feat: enhance StoreFilter component with Redux integration for dynamic filtering

--- client/src/layouts/leftStoreFilter.tsx
+++ client/src/layouts/leftStoreFilter.tsx
@@ -1,82 +1,200 @@
 import {
-    FormControlLabel,
-    TextField,
-    Typography,
-    Box,
-    Checkbox,
-    Grid,
-    InputAdornment,
-    Divider,
-    FormGroup,
-    Slider,
+  FormControlLabel,
+  TextField,
+  Typography,
+  Box,
+  Checkbox,
+  Grid,
+  InputAdornment,
+  Divider,
+  FormGroup,
+  Slider,
 } from '@mui/material';
 import { BiSearch } from 'react-icons/bi';
+import { useDispatch, useSelector } from 'react-redux';
+import { setFilters } from '../features/products/productSlice';
 
 const StoreFilter = () => {
-    return <>
-        <Grid item lg={3} className=" rounded border "
-              sx={{ marginTop: '20px', marginBottom: '20px', padding: '10px' }}>
-            <Box>
-                <Typography variant="h6" color="secondary.main">
-                    Search
-                    <TextField
-                        sx={{ padding: '10px' }}
-                        variant="outlined"
-                        fullWidth
-                        placeholder="Search for a product"
-                        InputProps={{
-                            endAdornment: (
-                                <InputAdornment position="end">
-                                    <BiSearch
-                                        className={
-                                            'hover:bg-primary-main rounded-full w-8 h-8 border-8 bg-secondary-main text-white transition-all duration-500 ease-in-out cursor-pointer hover:text-white border-secondary-main hover:border-primary-main '
-                                        }
-                                    />
-                                </InputAdornment>
-                            ),
-                        }}
-                    />
-                </Typography>
-            </Box>
-            <Box>
-                <Typography variant="h6" color="secondary.main">
-                    Category
-                </Typography>
-                <Divider className="py-2" />
-                <FormGroup className="p-4">
-                    <FormControlLabel control={<Checkbox />} label="Label" />
-                    <FormControlLabel control={<Checkbox />} label="Required" />
-                    <FormControlLabel control={<Checkbox />} label="Disabled" />
-                </FormGroup>
-            </Box>
-            <Box>
-                <Typography variant="h6" color="secondary.main">
-                    Weight
-                </Typography>
-                <Divider className="py-2" />
-                <FormGroup className="p-4">
-                    <FormControlLabel control={<Checkbox />} label="Label" />
-                    <FormControlLabel control={<Checkbox />} label="Required" />
-                    <FormControlLabel control={<Checkbox />} label="Disabled" />
-                </FormGroup>
-            </Box>
-            <Box className="py-2">
-                <Typography variant="h6" className="pb-2" color="secondary.main">
-                    Price
-                </Typography>
-                <Box className="flex justify-center">
-                    <Slider
-                        sx={{ width: '80%', padding: '5px' }}
-                        getAriaLabel={() => 'Temperature range'}
-                        /*value={}
-                        onChange={} */
-                        valueLabelDisplay="auto"
-                        /*getAriaValueText={} */
-                    />
-                </Box>
+  const dispatch = useDispatch();
+  const filters = useSelector((state: any) => state.products.filters); // Adjust the state path
 
-            </Box>
-        </Grid>
-    </>;
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(setFilters({ key: 'search', value: event.target.value }));
+  };
+
+  const handleCategoryChange = (category: string) => {
+    dispatch(setFilters({ key: 'category', value: category }));
+  };
+
+  const handleWeightChange = (weight: string) => {
+    dispatch(setFilters({ key: 'weight', value: weight }));
+
+  };
+
+  const handlePriceChange = (event: any, newValue: number | number[]) => {
+    if (Array.isArray(newValue)) {
+      dispatch(setFilters({ key: 'Minprice', value: newValue[0] }));
+      dispatch(setFilters({ key: 'Maxprice', value: newValue[1] }));
+    }
+
+  };
+
+
+
+  return (
+    <Grid
+      item
+      lg={3}
+      className="rounded border"
+      sx={{ marginTop: '20px', marginBottom: '20px', padding: '10px' }}
+    >
+      <Box>
+        <Typography variant="h6" color="secondary.main">
+          Search
+          <TextField
+            sx={{ padding: '10px' }}
+            variant="outlined"
+            fullWidth
+            placeholder="Search for a product"
+            value={filters.search} // Set value from Redux store
+            onChange={handleSearchChange}
+
+          />
+        </Typography>
+      </Box>
+      <Box>
+        <Typography variant="h6" color="secondary.main">
+          Category
+        </Typography>
+        <Divider className="py-2" />
+        <FormGroup className="p-4">
+          <FormControlLabel
+            control={
+              <Checkbox
+                value={'Seafood'}
+                checked={filters.category === 'Seafood'}
+                onChange={() => handleCategoryChange('Seafood')}
+              />
+            }
+            label="Seafood"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox
+                value={'Vegetables'}
+                checked={filters.category === 'Vegetables'}
+                onChange={() => handleCategoryChange('Vegetables')}
+              />
+            }
+            label="Vegetables"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox
+                value={'Milk & Diary'}
+                checked={filters.category === 'Milk & Diary'}
+                onChange={() => handleCategoryChange('Milk & Diary')}
+              />
+            }
+            label="Milk & Diary"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox
+                value={'Bakery'}
+                checked={filters.category === 'Bakery'}
+                onChange={() => handleCategoryChange('Bakery')}
+              />
+            }
+            label="Bakery"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox
+                value={'Fruits'}
+                checked={filters.category === 'Fruits'}
+                onChange={() => handleCategoryChange('Fruits')}
+              />
+            }
+            label="Fruits"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox
+                value={'Fastfood'}
+                checked={filters.category === 'Fastfood'}
+                onChange={() => handleCategoryChange('Fastfood')}
+              />
+            }
+            label="Fastfood"
+          />
+        </FormGroup>
+      </Box>
+      <Box>
+        <Typography variant="h6" color="secondary.main">
+          Weight
+        </Typography>
+        <Divider className="py-2" />
+        <FormGroup className="p-4">
+          <FormControlLabel
+            control={
+              <Checkbox
+                value="kg"
+                checked={filters.weight === 'kg'}
+                onChange={() => handleWeightChange('kg')}
+              />
+            }
+            label="Kilogram"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox
+                value="L"
+                checked={filters.weight === 'L'}
+                onChange={() => handleWeightChange('L')}
+              />
+            }
+            label="Liter"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox
+                value="piece"
+                checked={filters.weight === 'piece'}
+                onChange={() => handleWeightChange('piece')}
+              />
+            }
+            label="Piece"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox
+                value="pack"
+                checked={filters.weight === 'pack'}
+                onChange={() => handleWeightChange('pack')}
+              />
+            }
+            label="Pack"
+          />
+        </FormGroup>
+      </Box>
+      <Box className="py-2">
+        <Typography variant="h6" className="pb-2" color="secondary.main">
+          Price
+        </Typography>
+        <Box className="flex justify-center">
+          <Slider
+            sx={{ width: '80%', padding: '5px' }}
+            value={[filters.Minprice, filters.Maxprice]} // Bind the price range values from Redux store
+            onChange={handlePriceChange}
+            valueLabelDisplay="auto"
+            min={0}
+            max={1000} // Adjust max value as needed
+          />
+        </Box>
+      </Box>
+    </Grid>
+  );
 };
+
 export default StoreFilter;
