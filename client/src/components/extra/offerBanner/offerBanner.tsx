3d1fadb96879e26ce9c1e64db4fb29e34b956550
Osama Lmezouari <Oussamalm147@gmail.com>
2024-12-29 23:24:20 +0100

    fix: remove unnecessary empty line in OfferBanner component

--- client/src/components/extra/offerBanner/offerBanner.tsx
+++ client/src/components/extra/offerBanner/offerBanner.tsx
@@ -5,7 +5,6 @@ const OfferBanner = () => {
   return (
     <Box className="w-[1200px] m-auto" component={'div'}>
     <PromoCard image={'https://github.com/osamalmezouari/ecommerce/blob/master/client/public/assets/images/offerbanner.png?raw=true'} title={''} subtitle={''} discountText={''} buttonText={'none'} />
-
     </Box>
   );
 };
