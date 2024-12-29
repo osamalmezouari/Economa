ff24aff4e7739727dd884cbd080e391124eac595
Osama Lmezouari <Oussamalm147@gmail.com>
2024-12-29 23:24:28 +0100

    fix: update ProductnewArrivalsContainer layout and improve productAvgRating property

--- client/src/components/extra/productnewArrivalsContainer/productnewArrivalsContainer.tsx
+++ client/src/components/extra/productnewArrivalsContainer/productnewArrivalsContainer.tsx
@@ -1,90 +1,3 @@
-/* import { Grid } from '@mui/material';
-import { useDispatch, useSelector } from 'react-redux';
-import { AppDispatch, RootState } from '../../../app/store';
-import { getProductsNewArrivals } from '../../../features/products/productThunk';
-import { useEffect } from 'react';
-import ProductCard from '../../base/ProductCard/ProductCard';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import Slider from 'react-slick';
-
-const ProductnewArrivalsContainer = () => {
-  const dispatch = useDispatch<AppDispatch>();
-  const { productsNewArrivals } = useSelector(
-    (state: RootState) => state.products
-  );
-  const { loading, data } = productsNewArrivals;
-
-  useEffect(() => {
-    dispatch(getProductsNewArrivals());
-  }, [dispatch]);
-
-  const sliderSettings = {
-    dots: false,
-    infinite: false,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  };
-
-  return (
-    <div style={{ margin: 'auto', maxWidth: '1200px', padding: '20px' }}>
-      {loading ? (
-        'Loading...'
-      ) : (
-        <Slider {...sliderSettings}>
-          {data?.map((product) => (
-            <div key={product.id}>
-              <Grid container spacing={2}>
-                <Grid item xs={12} gap={2}>
-                  <ProductCard
-                    id={product.id}
-                    discount={product.discount}
-                    name={product.name}
-                    categoryName={product.categoryName}
-                    description={product.description}
-                    productAvgRaiting={product.productAvgRaiting}
-                    price={product.price}
-                    priceWithDiscount={product.priceWithDiscount}
-                    unit={product.unit}
-                    imageLink={product.imageLink}
-                  />
-                </Grid>
-              </Grid>
-            </div>
-          ))}
-        </Slider>
-      )}
-    </div>
-  );
-};
-
-export default ProductnewArrivalsContainer;
- */
-
 import { Box, CircularProgress, Grid } from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from '../../../app/store';
@@ -110,7 +23,7 @@ const ProductnewArrivalsContainer = () => {
     dots: false,
     infinite: false,
     speed: 500,
-    slidesToShow: 4,
+    slidesToShow: 5,
     slidesToScroll: 4,
     responsive: [
       {
@@ -121,7 +34,7 @@ const ProductnewArrivalsContainer = () => {
         },
       },
       {
-        breakpoint: 600,
+        breakpoint: 800,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2,
@@ -138,7 +51,7 @@ const ProductnewArrivalsContainer = () => {
   };
 
   return (
-    <div style={{ margin: 'auto', maxWidth: '1200px', padding: '20px' }}>
+    <div style={{ margin: 'auto', maxWidth: '1350px', padding: '20px' }}>
       {loading ? (
         <Box className={'w-full flex justify-center items-center py-8'}>
           <CircularProgress className='' />
@@ -157,7 +70,7 @@ const ProductnewArrivalsContainer = () => {
                       name={product.name}
                       categoryName={product.categoryName}
                       description={product.description}
-                      productAvgRaiting={product.productAvgRaiting}
+                      productAvgRating={product.productAvgRating}
                       price={product.price}
                       priceWithDiscount={product.priceWithDiscount}
                       unit={product.unit}
@@ -181,7 +94,7 @@ const ProductnewArrivalsContainer = () => {
                       name={product.name}
                       categoryName={product.categoryName}
                       description={product.description}
-                      productAvgRaiting={product.productAvgRaiting}
+                      productAvgRating={product.productAvgRating}
                       price={product.price}
                       priceWithDiscount={product.priceWithDiscount}
                       unit={product.unit}
