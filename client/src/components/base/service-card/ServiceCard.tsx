2136bccf2ae6b36370fa6570556be39e8cf54673
Osama Lmezouari <Oussamalm147@gmail.com>
2024-12-29 23:24:07 +0100

    fix: enhance ServiceCard layout with improved padding and typography

--- client/src/components/base/service-card/ServiceCard.tsx
+++ client/src/components/base/service-card/ServiceCard.tsx
@@ -7,9 +7,9 @@ const ServiceCard = ({
     subtitle,
 }: ServiceCardProps) => {
 
-    return <Box className="w-[300px]  border-gray-200 border-2 rounded text-center">
-        {icon}
-        <p className="p-2 font-bold font-xl text-secondary-main">
+    return <Box className="w-[300px] h-max p-6 flex-col place-items-center border-gray-200 border-2 rounded text-center">
+        <>{icon}</>
+        <p className="p-2 font-bold tracking-wider  text-xl text-secondary-main">
             {title}
         </p>
         <p className="px-4 py-1 text-secondary-main font-light">{subtitle}</p>
