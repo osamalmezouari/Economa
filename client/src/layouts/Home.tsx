fc7c4720518cf7b5cdecc0341e2756d68fbea65a
Osama Lmezouari <Oussamalm147@gmail.com>
2024-12-29 23:24:41 +0100

    fix: remove unused imports in Home layout for cleaner code

--- client/src/layouts/Home.tsx
+++ client/src/layouts/Home.tsx
@@ -1,7 +1,5 @@
 import HeroImage from '../components/base/hero-image/heroImage';
 import CategoryCardContainer from '../components/extra/categoryCardContainer/categoryCardContainer';
-import ShoppingCart from '../components/extra/shoppingCart/shoopingCart';
-import Wishlist from '../components/extra/wishlist/wishlist.tsx';
 const Home = () => {
   return (
     <>
