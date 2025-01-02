1c2431c331d5cf4a3beff583d58785e8c5fad28e
Osama Lmezouari <Oussamalm147@gmail.com>
2025-01-02 21:14:36 +0100

    Refactor auth thunks to streamline API call returns.
    Simplified the return statements in the Register and Login async thunks by removing unnecessary intermediate variables. This improves readability without altering functionality.

--- client/src/features/auth/authThunk.ts
+++ client/src/features/auth/authThunk.ts
@@ -3,36 +3,33 @@ import { LoginPayload, RegisterPayload } from '../../types/auth';
 import * as authApi from '../../api/auth';
 
 export const Register = createAsyncThunk(
-    'auth/register',
-    async (data: RegisterPayload, { rejectWithValue }) => {
-      try {
-        const newRegister = await authApi.Register(data);
-        return newRegister;
-      } catch (error: any) {
-        if (error) {
-          return rejectWithValue(error);
-        }
-        return rejectWithValue({
-          message: error.message || 'Something went wrong.',
-        });
+  'auth/register',
+  async (data: RegisterPayload, { rejectWithValue }) => {
+    try {
+      return await authApi.Register(data);
+    } catch (error: any) {
+      if (error) {
+        return rejectWithValue(error);
       }
+      return rejectWithValue({
+        message: error.message || 'Something went wrong.',
+      });
     }
-  );
-  
+  }
+);
 
 export const Login = createAsyncThunk(
   'auth/login',
   async (data: LoginPayload, { rejectWithValue }) => {
     try {
-      const newLogin = authApi.Login(data);
-      return newLogin;
+      return await authApi.Login(data);
     } catch (error: any) {
-        if (error) {
-          return rejectWithValue(error);
-        }
-        return rejectWithValue({
-          message: error.message || 'Something went wrong.',
-        });
+      if (error) {
+        return rejectWithValue(error);
       }
+      return rejectWithValue({
+        message: error.message || 'Something went wrong.',
+      });
+    }
   }
 );
