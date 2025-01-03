2ea4e4f29f32c8383e53d7972a19cb6bc717ee42
Osama Lmezouari <Oussamalm147@gmail.com>
2025-01-03 23:59:48 +0100

    feat: update PromoCard component layout and styling

--- client/src/components/base/PromoCard/PromoCard.tsx
+++ client/src/components/base/PromoCard/PromoCard.tsx
@@ -10,17 +10,17 @@ const PromoCard: React.FC<PromoCardProps> = ({
   buttonText,
 }) => {
   return (
-    <div className='px-6 sm:px-0'>
-    <Box
+    <div className="px-6 sm:px-0">
+      <Box
         component="div"
         style={{ backgroundImage: `url(${image})` }}
-        className="bg-cover w-full m-auto h-[400px] rounded relative bg-center flex justify-center items-center text-center sm:block sm:text-start"
+        className="bg-cover w-full m-auto h-[300px] rounded relative bg-center flex justify-center items-center text-center sm:block sm:text-start"
       >
         <Box
           component={'div'}
           className=" sm:absolute sm:right-[10%] sm:top-[25%] text-end"
         >
-          <Typography variant="h2" fontWeight="bold" color="secondary">
+          <Typography variant="h3" fontWeight="bold" color="secondary">
             {title}
           </Typography>
           <Typography variant="h4" color="text.secondary">
@@ -34,18 +34,24 @@ const PromoCard: React.FC<PromoCardProps> = ({
           >
             {discountText}
           </Typography>
-          {buttonText !== "none" && <Button
-            variant="contained"
-            color="secondary"
-            className="hover:bg-primary-main bg-secondary-main h-8"
-            sx={{ textTransform: 'none', fontSize: 16 , borderRadius : '1px' , padding :"22px"}}
-          >
-            {buttonText}
-          </Button>}
+          {buttonText !== 'none' && (
+            <Button
+              variant="contained"
+              color="secondary"
+              className="hover:bg-primary-main bg-secondary-main h-8"
+              sx={{
+                textTransform: 'none',
+                fontSize: 16,
+                borderRadius: '1px',
+                padding: '22px',
+              }}
+            >
+              {buttonText}
+            </Button>
+          )}
         </Box>
       </Box>
     </div>
-      
   );
 };
 
