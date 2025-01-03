63ec982951c3fa45b5f1a7845a0ed3044a180158
Osama Lmezouari <Oussamalm147@gmail.com>
2025-01-04 00:00:11 +0100

    feat: update OfferBanner component to display multiple PromoCards with improved layout

--- client/src/components/extra/offerBanner/offerBanner.tsx
+++ client/src/components/extra/offerBanner/offerBanner.tsx
@@ -3,8 +3,25 @@ import PromoCard from '../../base/PromoCard/PromoCard';
 
 const OfferBanner = () => {
   return (
-    <Box className="w-[1200px] m-auto" component={'div'}>
-    <PromoCard image={'https://github.com/osamalmezouari/ecommerce/blob/master/client/public/assets/images/offerbanner.png?raw=true'} title={''} subtitle={''} discountText={''} buttonText={'none'} />
+    <Box className=" max-w-[1350px] mt-16 m-auto grid grid-cols-1 xl:grid-cols-2 gap-4">
+      <Box className="w-full">
+        <PromoCard
+          image={'/assets/images/storebanner1.jpg'}
+          title={'Fresh Fruits'}
+          subtitle={'Special flavor.'}
+          discountText={'Limited time: 10% off!'}
+          buttonText={'Shop now'}
+        />
+      </Box>
+      <Box className="w-full">
+        <PromoCard
+          image={'/assets/images/storebanner2.jpg'}
+          title={'Fastfood'}
+          subtitle={'Healthy meal.'}
+          discountText={'Limited time: 10% off!'}
+          buttonText={'Shop now'}
+        />
+      </Box>
     </Box>
   );
 };
