f9c767d8df37826d12163e103954027c175d1b14
Osama Lmezouari <Oussamalm147@gmail.com>
2025-01-04 23:24:41 +0100

    feat: implement product details fetching and display in ProductsDetails component

--- client/src/layouts/ProductsDetails.tsx
+++ client/src/layouts/ProductsDetails.tsx
@@ -9,7 +9,6 @@ import {
   TextField,
   Typography,
 } from '@mui/material';
-import { useParams } from '@tanstack/react-router';
 import { BiHeart, BiMinus, BiPlus } from 'react-icons/bi';
 import {
   createshoppingCart,
@@ -17,17 +16,14 @@ import {
 } from '../features/shoppingCart/shoppingCartThunk';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from '../app/store';
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   createWishList,
   getWishlist,
 } from '../features/wishlist/wishlistThunk';
+import { getProductsDetails } from '../features/products/productThunk';
 
-const ProductsDetails = ({
-  ProductId,
-}: {
-  ProductId: string;
-}) => {
+const ProductsDetails = ({ ProductId }: { ProductId: string }) => {
   const dispatch = useDispatch<AppDispatch>();
   const { loading: loadingWishlist } = useSelector(
     (state: RootState) => state.wishlist.createWishList
@@ -40,6 +36,14 @@ const ProductsDetails = ({
   });
   const { loading, error } = useSelector(
     (state: RootState) => state.shoppingCart.createshoppingCart
+  );
+
+  const productdetailsData = useSelector(
+    (state: RootState) => state.products.productsDetails.data.product
+  );
+
+  const { loading: productdetailsLoading } = useSelector(
+    (state: RootState) => state.products.productsDetails
   );
 
   const [newquantity, setQuantity] = React.useState(quantity);
@@ -58,126 +62,151 @@ const ProductsDetails = ({
     await dispatch(createshoppingCart(cartItem));
     await dispatch(getshoppingCart());
   };
+  useEffect(() => {
+    dispatch(getProductsDetails(ProductId));
+  }, [dispatch, ProductId]);
 
+  if (productdetailsLoading) {
+    return (
+      <div className="flex justify-center items-center h-[60vh] w-full">
+        <CircularProgress color="inherit" />
+      </div>
+    );
+  }
   return (
-    <Grid
-      container
-      spacing={2}
-      maxWidth="1200px"
-      margin="auto"
-      sx={{ marginTop: '50px', marginBottom: '50px' }}
-    >
-      <Grid item lg={5}>
-        <Box
-          component={'div'}
-          className="bg-cover bg-center h-[420px] rounded border-[1px]"
-          style={{
-            backgroundImage: `url(/assets/products/bakery/product1.jpg)`,
-          }}
-        ></Box>
-      </Grid>
-      <Grid item lg={6}>
-        <Typography
-          variant={'h4'}
-          className={'tracking-wider py-2'}
-          color={'secondary.main'}
-        >
-          Potato Chips - Sea food
-        </Typography>
-        <Box className={'flex item-center py-2 gap-4 '}>
-          <Rating name="read-only" value={4} readOnly size={'small'} />
-          <p className={'font-bold font-primary text-secondary-main '}>
-            ( 950 reviews )
-          </p>
-        </Box>
-        <Box className={'flex item-center py-4 gap-4 '}>
-          <p className="font-bold text-2xl text-secondary-dark">$600.00</p>
-          <p className="text-primary-main text-2xl font-bold">-78%</p>
-        </Box>
-        <Box className={'flex item-center justify-between py-2 gap-4 w-11/12 '}>
-          <p
-            className={
-              'font-bold font-primary line-through tracking-widest text-secondary-light text-2xl'
-            }
-          >
-            2000$
-          </p>
-          <p className="font-bold text-xl text-primary-main">In Stock</p>
-        </Box>
-        <p className="text-secondary-dark py-4 w-11/12">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1990.
-        </p>
-        <Box className="flex items-center gap-2 mb-4">
-          <p className="font-bold capitalize font-secondary-light cursor-text">
-            Weight :{' '}
-          </p>
-          <Button
-            color="primary"
-            variant="outlined"
-            className="bg-primary-main w-max cursor-text h-8"
-          >
-            250 g
-          </Button>
-        </Box>
-        <Box className="flex gap-2">
-          <TextField
-            className="w-24 text-secondary-main"
-            value={newquantity}
-            variant="outlined"
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <BiPlus
-                    className="hover:cursor-pointer hover:bg-primary-main hover:rounded-full hover:text-white duration-300 transition-all"
-                    onClick={incrementQuantity}
-                  />
-                </InputAdornment>
-              ),
-              endAdornment: (
-                <InputAdornment position="end">
-                  <BiMinus
-                    className="hover:cursor-pointer hover:bg-primary-main hover:rounded-full hover:text-white duration-300 transition-all"
-                    onClick={decrementQuantity}
-                  />
-                </InputAdornment>
-              ),
+    <>
+      <Grid
+        container
+        spacing={2}
+        maxWidth="1200px"
+        margin="auto"
+        sx={{ marginTop: '50px', marginBottom: '50px' }}
+      >
+        <Grid item lg={5}>
+          <Box
+            component={'div'}
+            className="bg-cover bg-center h-[420px] rounded border-[1px]"
+            style={{
+              backgroundImage: `url(${productdetailsData.imageLink})`,
             }}
-          />
-          <Button
-            className="h-[56px] hover:bg-primary-main w-[150px]"
-            startIcon={<ShoppingBagOutlined />}
-            color="secondary"
-            variant="contained"
-            onClick={addToCart}
-            disabled={loading}
+          ></Box>
+        </Grid>
+        <Grid item lg={6}>
+          <Typography
+            variant={'h4'}
+            className={'tracking-wider py-2'}
+            color={'secondary.main'}
           >
-            {loading ? (
-              <CircularProgress size={40} color="inherit" />
-            ) : (
-              'Add to Cart'
-            )}
-          </Button>
-          <Button
-            className="h-[56px] hover:bg-primary-main hover:text-white w-[50px]"
-            color="secondary"
-            variant="outlined"
-            disabled={loadingWishlist}
-            onClick={async () => {
-              await dispatch(createWishList(ProductId));
-              await dispatch(getWishlist());
-            }}
+            {productdetailsData.name} - {productdetailsData.categoryName}
+          </Typography>
+          <Box className={'flex item-center py-2 gap-4 '}>
+            <Rating name="read-only" value={4} readOnly size={'small'} />
+            <p className={'font-bold font-primary text-secondary-main '}>
+              ( {productdetailsData.reviewsCount} Reviews )
+            </p>
+          </Box>
+          <Box className={'flex item-center py-4 gap-4 '}>
+            <p className="font-bold text-2xl text-secondary-dark">
+              ${productdetailsData.priceWithDiscount}
+            </p>
+            <p className="text-primary-main text-2xl font-bold">
+              {productdetailsData.priceWithDiscount === productdetailsData.price
+                ? productdetailsData.discount
+                : ''}
+              %
+            </p>
+          </Box>
+          <Box
+            className={'flex item-center justify-between py-2 gap-4 w-11/12 '}
           >
-            {loadingWishlist ? (
-              <CircularProgress size={8} className="h-max" color="primary" />
-            ) : (
-              <BiHeart fontSize={24} />
-            )}
-          </Button>
-        </Box>
+            <p
+              className={
+                'font-bold font-primary line-through tracking-widest text-secondary-light text-2xl'
+              }
+            >
+              {productdetailsData.priceWithDiscount === productdetailsData.price
+                ? ''
+                : productdetailsData.price + '$'}
+            </p>
+            <p
+              className={`font-bold text-xl ${productdetailsData.inStock ? 'text-primary-main' : 'text-red-400'}`}
+            >
+              {productdetailsData.inStock ? 'In Stock' : 'Out Stock'}
+            </p>
+          </Box>
+          <p className="text-secondary-dark py-4 w-11/12">
+            {productdetailsData.description}
+          </p>
+          <Box className="flex items-center gap-2 mb-4">
+            <p className="font-bold capitalize font-secondary-light cursor-text">
+              Weight :
+            </p>
+            <Button
+              color="primary"
+              variant="outlined"
+              className="bg-primary-main w-max cursor-text h-8"
+            >
+              {productdetailsData.unit}
+            </Button>
+          </Box>
+          <Box className="flex gap-2">
+            <TextField
+              className="w-24 text-secondary-main"
+              value={newquantity}
+              variant="outlined"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <BiPlus
+                      className="hover:cursor-pointer hover:bg-primary-main hover:rounded-full hover:text-white duration-300 transition-all"
+                      onClick={incrementQuantity}
+                    />
+                  </InputAdornment>
+                ),
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <BiMinus
+                      className="hover:cursor-pointer hover:bg-primary-main hover:rounded-full hover:text-white duration-300 transition-all"
+                      onClick={decrementQuantity}
+                    />
+                  </InputAdornment>
+                ),
+              }}
+            />
+            <Button
+              className="h-[56px] hover:bg-primary-main w-[150px]"
+              startIcon={<ShoppingBagOutlined />}
+              color="secondary"
+              variant="contained"
+              onClick={addToCart}
+              disabled={loading}
+            >
+              {loading ? (
+                <CircularProgress size={40} color="inherit" />
+              ) : (
+                'Add to Cart'
+              )}
+            </Button>
+            <Button
+              className="h-[56px] hover:bg-primary-main hover:text-white w-[50px]"
+              color="secondary"
+              variant="outlined"
+              disabled={loadingWishlist}
+              onClick={async () => {
+                await dispatch(createWishList(ProductId));
+                await dispatch(getWishlist());
+              }}
+            >
+              {loadingWishlist ? (
+                <CircularProgress size={8} className="h-max" color="primary" />
+              ) : (
+                <BiHeart fontSize={24} />
+              )}
+            </Button>
+          </Box>
+        </Grid>
       </Grid>
-    </Grid>
+    </>
   );
 };
 export default ProductsDetails;
