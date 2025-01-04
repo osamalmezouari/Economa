2e3ad8af003f19d6b67fe31d3b61612cd5118700
Osama Lmezouari <Oussamalm147@gmail.com>
2025-01-04 23:24:46 +0100

    fix: update import path for TitleOne component in ProductsWithDiscount layout

--- client/src/layouts/ProductsWithDiscount.tsx
+++ client/src/layouts/ProductsWithDiscount.tsx
@@ -1,4 +1,4 @@
-import TitleOne from '../components/extra/LandingTitles/TitleOne';
+import TitleOne from '../components/extra/Titles/TitleOne';
 import ProductCardContainer from '../components/extra/ProductCardContainer/productCardContainer';
 
 const ProductsWithDiscount = () => {
