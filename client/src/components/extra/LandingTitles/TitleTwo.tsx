134ccbaa9f14e8566f3d18d4b5e7dca397afa444
Osama Lmezouari <Oussamalm147@gmail.com>
2025-01-04 23:23:51 +0100

    feat: remove TitleTwo component as part of landing titles refactor

--- client/src/components/extra/LandingTitles/TitleTwo.tsx
+++
@@ -1,8 +0,0 @@
-import LandingTitle from "../../base/LandingTitle/LandingTitle"
-
-const TitleTwo = () => {
-    return <>
-        <LandingTitle title="New Arrivals" subTitle="Shop online for new arrivals and get free shipping!" />
-    </>
-}
-export default TitleTwo
