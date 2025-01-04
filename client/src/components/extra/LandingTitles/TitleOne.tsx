f622dfba73e0e6df46897c4298cb30197d329a28
Osama Lmezouari <Oussamalm147@gmail.com>
2025-01-04 23:23:46 +0100

    feat: remove TitleOne component as part of landing titles refactor

--- client/src/components/extra/LandingTitles/TitleOne.tsx
+++
@@ -1,8 +0,0 @@
-import LandingTitle from "../../base/LandingTitle/LandingTitle"
-
-const TitleOne = () => {
-    return <>
-        <LandingTitle title="Day of the deal" subTitle="Don't wait. The time will never be just right." />
-    </>
-}
-export default TitleOne
