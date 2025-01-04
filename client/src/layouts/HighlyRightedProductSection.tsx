602a799065c338e662a0acd629075ee8f72b0ff4
Osama Lmezouari <Oussamalm147@gmail.com>
2025-01-04 23:24:31 +0100

    feat: add HighlyRightedProductSection component for displaying featured products

---
+++ client/src/layouts/HighlyRightedProductSection.tsx
@@ -0,0 +1,12 @@
+import HighlyRightedProductContainer from '../components/extra/HighlyRightedProductContainer/HighlyRightedProductContainer';
+import TitleThree from '../components/extra/Titles/TitleThree';
+
+const HighlyRightedProductSection = () => {
+  return (
+    <>
+      <TitleThree />
+      <HighlyRightedProductContainer />
+    </>
+  );
+};
+export default HighlyRightedProductSection;
