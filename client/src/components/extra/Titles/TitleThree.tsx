4c06c6e458604a73340ab59bb95dd7c1d4c67556
Osama Lmezouari <Oussamalm147@gmail.com>
2025-01-04 23:24:11 +0100

    feat: add TitleThree component for enhanced landing page titles

---
+++ client/src/components/extra/Titles/TitleThree.tsx
@@ -0,0 +1,8 @@
+import LandingTitle from "../../base/SectionsTitle/SectionTitle"
+
+const TitleThree = () => {
+    return <>
+        <LandingTitle title="Highly Righted Store Products" subTitle="Quality is everything" />
+    </>
+}
+export default TitleThree
