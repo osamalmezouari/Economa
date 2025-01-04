8de43dcc52998826d65e9e4214c3a09aabcc4436
Osama Lmezouari <Oussamalm147@gmail.com>
2025-01-04 23:24:05 +0100

    feat: add TitleOne component for landing page titles

---
+++ client/src/components/extra/Titles/TitleOne.tsx
@@ -0,0 +1,8 @@
+import LandingTitle from "../../base/SectionsTitle/SectionTitle"
+
+const TitleOne = () => {
+    return <>
+        <LandingTitle title="Day of the deal" subTitle="Don't wait. The time will never be just right." />
+    </>
+}
+export default TitleOne
