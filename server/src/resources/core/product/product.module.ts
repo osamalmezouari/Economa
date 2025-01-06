7bcc468e3e576f10fdea3d6f2c5369f1ed950e69
Osama Lmezouari <Oussamalm147@gmail.com>
2025-01-06 23:20:31 +0100

    feat: add ProductReviewModule to product module imports

--- server/src/resources/core/product/product.module.ts
+++ server/src/resources/core/product/product.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { ProductReviewService } from '../product-review/product-review.service';
+import { ProductReviewModule } from '../product-review/product-review.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, ProductReviewModule],
   controllers: [ProductController],
   providers: [ProductService],
 })
