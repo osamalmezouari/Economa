468f9161c27e196a56aa3a2af9cba1260aed0a5f
Osama Lmezouari <Oussamalm147@gmail.com>
2025-01-06 23:19:25 +0100

    feat: add loading state and conditional rendering for HighlyRightedProductSection

--- client/src/layouts/HighlyRightedProductSection.tsx
+++ client/src/layouts/HighlyRightedProductSection.tsx
@@ -1,11 +1,33 @@
+import { Box, CircularProgress } from '@mui/material';
 import HighlyRightedProductContainer from '../components/extra/HighlyRightedProductContainer/HighlyRightedProductContainer';
 import TitleThree from '../components/extra/Titles/TitleThree';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import { useSelector } from 'react-redux';
+import { RootState } from '../app/store';
 
 const HighlyRightedProductSection = () => {
+  const { loading, data } = useSelector(
+    (state: RootState) => state.products.productsDetails
+  );
+
+  if (loading) {
+    return (
+      <Box className="w-full flex justify-center items-center py-8">
+        <CircularProgress />
+      </Box>
+    );
+  }
   return (
     <>
-      <TitleThree />
-      <HighlyRightedProductContainer />
+      {data.HighlyRighted.length ? (
+        <>
+          <TitleThree />
+          <HighlyRightedProductContainer />
+        </>
+      ) : (
+        ''
+      )}
     </>
   );
 };
