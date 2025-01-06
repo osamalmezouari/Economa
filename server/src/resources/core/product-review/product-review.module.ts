02e807ce8736da579db04dced4635d1b9196ea50
Osama Lmezouari <Oussamalm147@gmail.com>
2025-01-06 23:20:41 +0100

    feat: export ProductReviewService from ProductReviewModule

--- server/src/resources/core/product-review/product-review.module.ts
+++ server/src/resources/core/product-review/product-review.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { ProductReviewService } from './product-review.service';
 import { ProductReviewController } from './product-review.controller';
 import { PrismaModule } from '../../../prisma/prisma.module';
+import { UserModule } from '../user/user.module';
 
 @Module({
   imports: [PrismaModule],
   controllers: [ProductReviewController],
   providers: [ProductReviewService],
+  exports: [ProductReviewService],
 })
 export class ProductReviewModule {}
