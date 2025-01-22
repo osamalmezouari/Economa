b5cae6c1db9a484b39921a6a75c6b6aa3bca4ef1
Osama Lmezouari <Oussamalm147@gmail.com>
2025-01-22 21:59:19 +0100

    feat: extend refill balance types to include payment options and balance state management

--- client/src/types/refillbalance.ts
+++ client/src/types/refillbalance.ts
@@ -1,3 +1,20 @@
-export interface RefillBalanceRequestStateType {
+import { ApiError } from "./apierror";
 
+export interface RefillBalanceRequest {
+  paymentType: 'cash' | 'bank-transfer';
+  file: File | null;
+  amount: number;
+}
+
+export interface BalanceType {
+  Balance: number;
+  name: string;
+}
+
+export interface balanceStateType {
+  refillBalanceRequest: {
+    loading: boolean;
+    error: ApiError | null | undefined | unknown;
+    data: RefillBalanceRequest;
+  };
 }
