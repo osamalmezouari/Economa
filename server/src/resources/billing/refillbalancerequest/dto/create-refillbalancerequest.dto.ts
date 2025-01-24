fcad71dce4efdc340d8d90acc817d8ce9f995495
Osama Lmezouari <Oussamalm147@gmail.com>
2025-01-24 22:21:08 +0100

    feat: enhance CreateRefillbalancerequestDto with validation decorators

--- server/src/resources/billing/refillbalancerequest/dto/create-refillbalancerequest.dto.ts
+++ server/src/resources/billing/refillbalancerequest/dto/create-refillbalancerequest.dto.ts
@@ -1,5 +1,11 @@
+import { Transform } from 'class-transformer';
+import { IsNumber, IsString } from 'class-validator';
+
 export class CreateRefillbalancerequestDto {
-    paymentType: 'cash' | 'bank-transfer';
-    file: Express.Multer.File | null;
-    amount: number;
+  @IsString()
+  paymentType: 'cash' | 'bank-transfer';
+  file: Express.Multer.File | null;
+  @IsNumber()
+  @Transform(({ value }) => parseFloat(value))
+  amount: number;
 }
