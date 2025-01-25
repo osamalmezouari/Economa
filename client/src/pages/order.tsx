bbd3f430a0ea48dcc33ad174d60d30e8d135a5f3
Osama Lmezouari <Oussamalm147@gmail.com>
2025-01-25 19:30:13 +0100

    feat: create Order page to integrate OrderValidationForm and OrderSummary components

---
+++ client/src/pages/order.tsx
@@ -0,0 +1,24 @@
+import { Grid } from '@mui/material';
+import OrderValidationForm from '../layouts/order/orderValidationForm';
+import OrderSummary from '../layouts/order/orderSummary';
+
+const Order = () => {
+  return (
+    <Grid
+      container
+      maxWidth={'lg'}
+      justifyContent={'center'}
+      marginX={'auto'}
+      marginY={8}
+      gap={4}
+    >
+      <Grid item lg={4}>
+        <OrderValidationForm />
+      </Grid>
+      <Grid item lg={3}>
+        <OrderSummary />
+      </Grid>
+    </Grid>
+  );
+};
+export default Order;
