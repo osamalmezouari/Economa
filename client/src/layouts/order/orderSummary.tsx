78b392d617d80fa978d84a3c7787c5e13235fc5c
Osama Lmezouari <Oussamalm147@gmail.com>
2025-01-26 23:16:32 +0100

    feat: update OrderSummary component to display dynamic pricing and improve layout

--- client/src/layouts/order/orderSummary.tsx
+++ client/src/layouts/order/orderSummary.tsx
@@ -1,19 +1,16 @@
-import {
-  Box,
-  Typography,
-  Button,
-  Divider,
-  TextField,
-  InputAdornment,
-} from '@mui/material';
-import EmptyBox from '../../components/base/empty-box/empty-box';
+import { Box, Typography, Button, Divider, TextField } from '@mui/material';
 import OrderItem from '../../components/extra/orderItem/orderitem';
-import { useDispatch } from 'react-redux';
-import { AppDispatch } from '../../app/store';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from '../../app/store';
 import { setDisplayCart } from '../../features/shoppingCart/shoppingCartSlice';
+import { useRouter } from '@tanstack/react-router';
 
 const OrderSummary = () => {
+  const Router = useRouter();
   const dispatch = useDispatch<AppDispatch>();
+  const { basePrice, vat, totalPrice } = useSelector(
+    (state: RootState) => state.shoppingCart
+  );
   return (
     <Box
       sx={{
@@ -45,17 +42,16 @@ const OrderSummary = () => {
         </Typography>
       </Box>
       <Divider />
-      <Box className={'flex items-center justify-center py-2'}>
-        {/* <EmptyBox /> */}
+      <Box className={' flex  flex-col gap-2 py-2'}>
         <OrderItem />
       </Box>
       <Box sx={{ display: 'flex', justifyContent: 'space-between', my: 1 }}>
-        <Typography variant="body2">Subtotal</Typography>
-        <Typography variant="body2">$0.00</Typography>
+        <Typography variant="body2">Base Price</Typography>
+        <Typography variant="body2">${basePrice.toFixed(2)}</Typography>
       </Box>
       <Box sx={{ display: 'flex', justifyContent: 'space-between', mb: 1 }}>
         <Typography variant="body2">Tax</Typography>
-        <Typography variant="body2">$0.00</Typography>
+        <Typography variant="body2">${vat.toFixed(2)}</Typography>
       </Box>
       <TextField
         variant="outlined"
@@ -64,7 +60,11 @@ const OrderSummary = () => {
         className="!mb-2"
         InputProps={{
           endAdornment: (
-            <Button size="small" className="text-[8px] w-full ">
+            <Button
+              size="small"
+              variant="outlined"
+              className="text-[8px] w-[160px] !rounded-[4px]"
+            >
               apply cupon
             </Button>
           ),
@@ -75,10 +75,15 @@ const OrderSummary = () => {
           Total
         </Typography>
         <Typography variant="body2" fontWeight="bold">
-          $0.00
+          ${totalPrice.toFixed(2)}
         </Typography>
       </Box>
-      <Button variant="contained" fullWidth sx={{ mt: 2 }}>
+      <Button
+        variant="contained"
+        fullWidth
+        sx={{ mt: 2 }}
+        onClick={() => Router.navigate({ to: '/Store' })}
+      >
         Back to Store
       </Button>
     </Box>
