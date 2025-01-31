65dbf71d3e5a8e403ac606554248b29384ea4338
Osama Lmezouari <Oussamalm147@gmail.com>
2025-01-31 02:43:22 +0100

    feat: simplify coupon verification function by passing data directly to API call

--- client/src/api/coupon.ts
+++ client/src/api/coupon.ts
@@ -1,13 +1,9 @@
 import { verfy_coupon_type } from '../types/coupon';
 import { apiClient } from '../utils/apiClient';
 
-export const verfy_coupon = async (
-  data: verfy_coupon_type
-): Promise<{ code: string; verfied: boolean }> => {
+export const verfy_coupon = async (data: verfy_coupon_type) => {
   try {
-    const response = await apiClient.post('/coupon/verify', {
-      code: data.code,
-    });
+    const response = await apiClient.post('/coupon/verify', data);
     return response.data;
   } catch (error: any) {
     if (error.response) {
