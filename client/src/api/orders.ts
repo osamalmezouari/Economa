920d93bfb41336fa2735dd28ddad8f160da7e950
Osama Lmezouari <Oussamalm147@gmail.com>
2025-01-31 02:43:27 +0100

    feat: implement place_and_pay_order API function for order processing

---
+++ client/src/api/orders.ts
@@ -0,0 +1,14 @@
+import { place_and_pay_order as place_and_pay_order_type } from '../types/order';
+import { apiClient } from '../utils/apiClient';
+
+export const place_and_pay_order = async (data: place_and_pay_order_type) => {
+  try {
+    const response = await apiClient.post('orders/placeandpay', data);
+    return response.data;
+  } catch (error: any) {
+    if (error.response) {
+      throw error.response.data;
+    }
+    throw new Error('Failed to place and Pay the Order');
+  }
+};
