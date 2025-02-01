35ce1d1ac58e20bc837f68dd7f41d845ca80e6a2
Osama Lmezouari <Oussamalm147@gmail.com>
2025-02-01 21:46:10 +0100

    fix: update parent route for Store component to use mainRoute

--- client/src/routes/storerout.tsx
+++ client/src/routes/storerout.tsx
@@ -1,9 +1,9 @@
 import { createRoute } from '@tanstack/react-router';
-import { rootRoute } from './__root';
+import { mainRoute } from './__root';
 import Store from './../pages/store';
 
 export const StoreRoute = createRoute({
-  getParentRoute: () => rootRoute,
+  getParentRoute: () => mainRoute,
   path: 'Store',
   component: Store,
 });
