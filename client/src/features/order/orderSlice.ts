df815e9810fa0ca73fc0e7c7ad24dddae6938a65
Osama Lmezouari <Oussamalm147@gmail.com>
2025-02-01 21:44:26 +0100

    feat: add orderSlice for managing order state and actions

---
+++ client/src/features/order/orderSlice.ts
@@ -0,0 +1,43 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { placeandpay } from './orderThunk';
+import { OrderStateType } from '../../types/order';
+
+const initialState: OrderStateType = {
+  placeandpay: {
+    loading: false,
+    error: null,
+    data: {
+      orderId: '',
+    },
+  },
+};
+
+const OrderSlice = createSlice({
+  name: 'Order',
+  initialState,
+  reducers: {
+    clearOrderId: (state) => {
+      state.placeandpay.data.orderId = '';
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(placeandpay.pending, (state) => {
+        state.placeandpay.loading = true;
+        state.placeandpay.error = null;
+      })
+      .addCase(placeandpay.fulfilled, (state, action) => {
+        state.placeandpay.data = action.payload;
+        state.placeandpay.error = null;
+        state.placeandpay.loading = false;
+      })
+      .addCase(placeandpay.rejected, (state, action) => {
+        state.placeandpay.error = action.payload;
+        state.placeandpay.loading = false;
+      });
+  },
+});
+
+export const { clearOrderId } = OrderSlice.actions;
+export default OrderSlice.reducer;
+export const OrderReducer = OrderSlice.reducer;
