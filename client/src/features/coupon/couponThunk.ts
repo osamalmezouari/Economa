a3e65f5bfd38d048104fee693ff6f79ba14ab190
Osama Lmezouari <Oussamalm147@gmail.com>
2025-02-01 21:44:22 +0100

    feat: update verfyCoupon thunk to use async/await and type for coupon data

--- client/src/features/coupon/couponThunk.ts
+++ client/src/features/coupon/couponThunk.ts
@@ -1,11 +1,12 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { verfy_coupon } from '../../api/coupon';
+import { verfy_coupon_type } from '../../types/coupon';
 
 export const verfyCoupon = createAsyncThunk(
   'verfy_coupon',
-  (data: { code: string }, { rejectWithValue }) => {
+  async (data : verfy_coupon_type, { rejectWithValue }) => {
     try {
-      const coupon = verfy_coupon(data);
+      const coupon = await verfy_coupon(data);
       return coupon;
     } catch (error: any) {
       if (error) {
