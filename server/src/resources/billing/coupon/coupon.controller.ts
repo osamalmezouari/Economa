2e5db2adf9445700df634fc15bb031004c6955e3
Osama Lmezouari <Oussamalm147@gmail.com>
2025-02-02 21:04:22 +0100

    feat: update verify method in CouponController to use verfy_coupon DTO for validation

--- server/src/resources/billing/coupon/coupon.controller.ts
+++ server/src/resources/billing/coupon/coupon.controller.ts
@@ -6,25 +6,23 @@ import {
   Patch,
   Param,
   Delete,
+  HttpCode,
+  HttpStatus,
 } from '@nestjs/common';
 import { CouponService } from './coupon.service';
 import { CreateCouponDto } from './dto/create-coupon.dto';
 import { UpdateCouponDto } from './dto/update-coupon.dto';
 import { activeUser } from 'src/common/decorators/params/activeUser.decorator';
+import { verfy_coupon } from './dto/verifyCoupon.dto';
 
 @Controller('coupon')
 export class CouponController {
   constructor(private readonly couponService: CouponService) {}
 
-  @Post('/verify')
-  async verify(
-    @Body() data: { code: string  },
-    @activeUser('sub') userId: string,
-  ) {
-    return await this.couponService.verify({
-      code: data.code,
-      userId,
-    });
+  @HttpCode(HttpStatus.OK)
+  @Post('verify')
+  async verify(@Body() data: verfy_coupon, @activeUser('sub') userId: string) {
+    return await this.couponService.verify(data, userId);
   }
 
   @Post()
