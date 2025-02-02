bb4b8a7016e5273f907567526609f982bec19b29
Osama Lmezouari <Oussamalm147@gmail.com>
2025-02-02 21:03:57 +0100

    feat: add BalanceController for managing balance-related requests

---
+++ server/src/resources/billing/balance/balance.controller.ts
@@ -0,0 +1,50 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  UseInterceptors,
+  UploadedFile,
+} from '@nestjs/common';
+import { BalanceService } from './balance.service';
+import { Express } from 'express';
+import { CreateRefillbalancerequestDto } from './dto/create-refillbalancerequest.dto';
+import { AnyFilesInterceptor, FileInterceptor } from '@nestjs/platform-express';
+import { activeUser } from 'src/common/decorators/params/activeUser.decorator';
+
+@Controller('balance')
+export class BalanceController {
+  constructor(private readonly balanceService: BalanceService) {}
+
+  @UseInterceptors(FileInterceptor('file'))
+  @Post('refillbalancerequest')
+  create(
+    @Body() createRefillbalancerequestDto: CreateRefillbalancerequestDto,
+    @UploadedFile() file: Express.Multer.File,
+    @activeUser('sub') userId: string,
+  ) {
+    console.log('file', file);
+    createRefillbalancerequestDto.file = file;
+    return this.balanceService.refilbalanceRequestcreate(
+      createRefillbalancerequestDto,
+      userId,
+    );
+  }
+
+  
+
+  @Get('CardInfo')
+  getCardInfo(@activeUser('sub') userId: string) {
+    return this.balanceService.getCardInfo(userId);
+  }
+  @Get()
+  findAll() {
+    return this.balanceService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.balanceService.findOne(+id);
+  }
+}
