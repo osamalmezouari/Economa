2896d536fe6ba0b62c1499598127b1f998338aae
Osama Lmezouari <Oussamalm147@gmail.com>
2025-02-02 21:03:01 +0100

    feat: add constants for cart keys, currency options, pagination, and user roles

--- client/src/mock/constants.ts
+++ client/src/mock/constants.ts
@@ -1,8 +1,40 @@
-import { Image_bg_hero_Type, logoTpye } from "./types";
+export const CART_KEY = 'isomorphic-cart';
+export const POS_CART_KEY = 'isomorphic-pos-cart';
+export const DUMMY_ID = 'FC6723757651DB74';
+export const CHECKOUT = 'isomorphic-checkout';
+export const CURRENCY_CODE = 'USD';
+export const LOCALE = 'en';
+export const CURRENCY_OPTIONS = {
+  formation: 'en-US',
+  fractions: 2,
+};
 
-export const logo: logoTpye = '/assets/icons/logo.png';
-export const Image_bg_hero : Image_bg_hero_Type= {
-    src: '/assets/images/hero-bg-2.jpg',
-    alt: ''
-}
+export const ROW_PER_PAGE_OPTIONS = [
+  {
+    value: 5,
+    name: '5',
+  },
+  {
+    value: 10,
+    name: '10',
+  },
+  {
+    value: 15,
+    name: '15',
+  },
+  {
+    value: 20,
+    name: '20',
+  },
+];
 
+export const ROLES = {
+  Administrator: 'Administrator',
+  Manager: 'Manager',
+  Sales: 'Sales',
+  Support: 'Support',
+  Developer: 'Developer',
+  HRD: 'HR Department',
+  RestrictedUser: 'Restricted User',
+  Customer: 'Customer',
+} as const;
