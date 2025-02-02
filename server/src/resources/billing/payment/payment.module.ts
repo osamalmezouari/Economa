44bd71a80ed41acdffc49353c475dc26cb99a91c
Osama Lmezouari <Oussamalm147@gmail.com>
2025-02-02 21:04:45 +0100

    feat: export PaymentService from PaymentModule for improved accessibility

--- server/src/resources/billing/payment/payment.module.ts
+++ server/src/resources/billing/payment/payment.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { PaymentService } from './payment.service';
 import { PaymentController } from './payment.controller';
 import { PrismaModule } from '../../../prisma/prisma.module';
+import { BalanceModule } from '../balance/balance.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule ],
   controllers: [PaymentController],
   providers: [PaymentService],
+  exports: [PaymentService]
 })
 export class PaymentModule {}
