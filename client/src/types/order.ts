44f6eea99e4d6cef8c20dd0b7859362ae87cdbda
Osama Lmezouari <Oussamalm147@gmail.com>
2025-02-02 21:01:58 +0100

    feat: add order types for place and pay functionality

---
+++ client/src/types/order.ts
@@ -0,0 +1,15 @@
+import { ApiError } from './apierror';
+
+export interface place_and_pay_order {
+  couponCode: string;
+}
+
+export interface OrderStateType {
+  placeandpay: {
+    data: {
+      orderId: string;
+    };
+    loading: boolean;
+    error: ApiError | null | unknown | undefined;
+  };
+}
