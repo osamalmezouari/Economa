59a3665b621b1e8218946e55a99bfaa6dcab82bf
Osama Lmezouari <Oussamalm147@gmail.com>
2025-02-02 21:05:22 +0100

    feat: add additional modules for enhanced order processing functionality

--- server/src/resources/core/orders/orders.module.ts
+++ server/src/resources/core/orders/orders.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
 import { PrismaModule } from '../../../prisma/prisma.module';
+import { CouponModule } from 'src/resources/billing/coupon/coupon.module';
+import { ShoppingCartModule } from 'src/resources/settings/shopping-cart/shopping-cart.module';
+import { BalanceModule } from 'src/resources/billing/balance/balance.module';
+import { PaymentModule } from 'src/resources/billing/payment/payment.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, CouponModule, ShoppingCartModule , BalanceModule , PaymentModule],
   controllers: [OrdersController],
   providers: [OrdersService],
 })
