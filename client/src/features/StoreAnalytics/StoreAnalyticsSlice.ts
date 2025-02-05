96885cfb9b0c3ddd6257f59361bf631e96f36866
Osama Lmezouari <Oussamalm147@gmail.com>
2025-02-05 22:54:42 +0100

    feat: enhance StoreAnalyticsSlice to include sales vs profit data handling

--- client/src/features/StoreAnalytics/StoreAnalyticsSlice.ts
+++ client/src/features/StoreAnalytics/StoreAnalyticsSlice.ts
@@ -1,32 +1,41 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { getCardsStats } from './StoreAnalyticsThunk';
+import { getCardsStats, getsalesXProfit } from './StoreAnalyticsThunk';
 import { storeAnalyticsState } from '../../types/storeAnalytics';
 
 const initialState: storeAnalyticsState = {
-  statCard: {
+  stateCards: {
     data: {
-      orderCountStats: {
-        totalOrdersMonth: 0,
-        ordersData: [],
+      orderStats: {
+        title: 'New Orders',
+        metric: 0,
         increased: false,
         decreased: false,
         percentage: 0,
+        chart: [],
       },
-      orderSalesStats: {
-        totalOrdersAmountMonth: 0,
-        ordersData: [],
+      SalesStats: {
+        title: 'Sales',
+        metric: 0,
         increased: false,
         decreased: false,
         percentage: 0,
+        chart: [],
       },
-      orderProfitStats: {
-        totalProfitMonth: 0,
-        ordersData: [],
+      ProfitStats: {
+        title: 'Profit',
+        metric: 0,
         increased: false,
         decreased: false,
         percentage: 0,
+        chart: [],
       },
     },
+
+    loading: false,
+    error: null,
+  },
+  SalesXProfit: {
+    data: [],
     loading: false,
     error: null,
   },
@@ -39,15 +48,27 @@ const StoreAnalyticsSlice = createSlice({
   extraReducers: (builder) => {
     builder
       .addCase(getCardsStats.pending, (state) => {
-        (state.statCard.loading = true), (state.statCard.error = null);
+        (state.stateCards.loading = true), (state.stateCards.error = null);
       })
       .addCase(getCardsStats.fulfilled, (state, action) => {
-        (state.statCard.loading = false),
-          (state.statCard.data = action.payload);
+        (state.stateCards.loading = false),
+          (state.stateCards.data = action.payload);
       })
       .addCase(getCardsStats.rejected, (state, action) => {
-        (state.statCard.loading = false),
-          (state.statCard.error = action.payload);
+        (state.stateCards.loading = false),
+          (state.stateCards.error = action.payload);
+      })
+
+      .addCase(getsalesXProfit.pending, (state) => {
+        (state.SalesXProfit.loading = true), (state.SalesXProfit.error = null);
+      })
+      .addCase(getsalesXProfit.fulfilled, (state, action) => {
+        (state.SalesXProfit.loading = false),
+          (state.SalesXProfit.data = action.payload);
+      })
+      .addCase(getsalesXProfit.rejected, (state, action) => {
+        (state.SalesXProfit.loading = false),
+          (state.SalesXProfit.error = action.payload);
       });
   },
 });
