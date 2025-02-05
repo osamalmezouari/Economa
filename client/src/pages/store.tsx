a7cd6c63a32cce360a1edf928c1281d2a4f29567
Osama Lmezouari <Oussamalm147@gmail.com>
2025-02-05 22:56:08 +0100

    refactor: update import paths for filters and improve component structure in Store page

--- client/src/pages/store.tsx
+++ client/src/pages/store.tsx
@@ -1,19 +1,21 @@
 import { Grid } from '@mui/material';
 import StoreTopSection from '../components/extra/StoreTopSection/StoreTopSection.tsx';
-import LeftStoreFilter from '../layouts/leftStoreFilter.tsx';
-import TopStoreFilter from '../layouts/topStoreFilter.tsx';
+import LeftStoreFilter from '../widgets/leftStoreFilter.tsx';
+import TopStoreFilter from '../widgets/topStoreFilter.tsx';
 import StoreProducts from '../components/extra/storeProducts/storeProducts.tsx';
 
 const Store = () => {
-    return <>
-        <StoreTopSection />
-        <Grid container gap={2} maxWidth={1200} sx={{ margin: 'auto' }}>
-            <LeftStoreFilter />
-            <Grid item lg={8.5} sx={{ marginTop: '20px', paddingTop: '0px' }}>
-                <TopStoreFilter />
-                <StoreProducts />
-            </Grid>
+  return (
+    <>
+      <StoreTopSection />
+      <Grid container gap={2} maxWidth={1200} sx={{ margin: 'auto' }}>
+        <LeftStoreFilter />
+        <Grid item lg={8.5} sx={{ marginTop: '20px', paddingTop: '0px' }}>
+          <TopStoreFilter />
+          <StoreProducts />
         </Grid>
-    </>;
+      </Grid>
+    </>
+  );
 };
 export default Store;
