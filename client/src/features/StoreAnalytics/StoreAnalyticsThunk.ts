db1dc223989675688fcf743d432e331137f83f1d
Osama Lmezouari <Oussamalm147@gmail.com>
2025-02-05 22:54:47 +0100

    feat: add async thunks for fetching card stats and sales vs profit data

---
+++ client/src/features/StoreAnalytics/StoreAnalyticsThunk.ts
@@ -0,0 +1,32 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { getCardStats, getSalesXProfit } from '../../api/StoreAnalytics';
+
+export const getCardsStats = createAsyncThunk(
+  'analytics/store/Stats-Cards',
+  async (_, { rejectWithValue }) => {
+    try {
+      const CardsStats = await getCardStats();
+      return CardsStats;
+    } catch (error: any) {
+      if (error.response) {
+        return rejectWithValue(error.response.data);
+      }
+      return rejectWithValue('Failed to fetch card stats');
+    }
+  }
+);
+
+export const getsalesXProfit = createAsyncThunk(
+  'analytics/store/SalesXProfit',
+  async (year: number | undefined, { rejectWithValue }) => {
+    try {
+      const SalesXProfit = await getSalesXProfit(year);
+      return SalesXProfit;
+    } catch (error: any) {
+      if (error.response) {
+        return rejectWithValue(error.response.data);
+      }
+      return rejectWithValue('Failed to fetch sales profit analytics');
+    }
+  }
+);
