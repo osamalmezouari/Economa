50aee18c64c44d6081a3e88d3a6e2daf25550abf
Osama Lmezouari <Oussamalm147@gmail.com>
2025-02-07 00:26:16 +0100

    feat: implement analytics service for card stats, sales, and profit data

---
+++ server/src/resources/core/analytics/analytics.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { OrdersService } from '../orders/orders.service';
+import { sortStoreAnalyticsStatsCardsByDate } from 'src/common/utils/sortStoreAnalyticsStatsCardsByDate';
+
+@Injectable()
+export class AnalyticsService {
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly orderService: OrdersService,
+  ) {}
+
+  async getCardStats() {
+    const orderStats = await this.orderService.getOrdersStatsData();
+    const SalesStats = await this.orderService.getOrdersSalesData();
+    const profitStats = await this.orderService.getProfitSalesData();
+    return {
+      orderStats: {
+        title: 'New Orders',
+        metric: orderStats.totalOrdersMonth,
+        increased: orderStats.increased,
+        decreased: orderStats.decreased,
+        percentage: Math.trunc(orderStats.percentage),
+        chart: sortStoreAnalyticsStatsCardsByDate(orderStats.ordersData),
+      },
+      SalesStats: {
+        title: 'Sales',
+        metric: Math.trunc(SalesStats.totalOrdersAmountMonth),
+        increased: SalesStats.increased,
+        decreased: SalesStats.decreased,
+        percentage: Math.trunc(SalesStats.percentage),
+        chart: sortStoreAnalyticsStatsCardsByDate(SalesStats.ordersData),
+      },
+      ProfitStats: {
+        title: 'Profit',
+        metric: Math.trunc(profitStats.totalProfitMonth),
+        increased: profitStats.increased,
+        decreased: profitStats.decreased,
+        percentage: Math.trunc(profitStats.percentage),
+        chart: sortStoreAnalyticsStatsCardsByDate(profitStats.ordersData),
+      },
+    };
+  }
+
+  async SalesXProfit(year: number) {
+    const data = await this.orderService.getYearlySalesXProfit(year);
+    return data;
+  }
+
+  async CostXProfitLastWeek() {
+    const data = await this.orderService.getLastWeekCostXProfit();
+    return data;
+  }
+}
