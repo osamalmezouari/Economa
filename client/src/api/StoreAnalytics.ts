5d50c16a7aa241236a4c21b72883f8189356464e
Osama Lmezouari <Oussamalm147@gmail.com>
2025-02-06 14:34:04 +0100

    feat: add function to fetch sales vs profit data for the last week

--- client/src/api/StoreAnalytics.ts
+++ client/src/api/StoreAnalytics.ts
@@ -23,3 +23,15 @@ export const getSalesXProfit = async (year? : number) => {
     throw new Error('Failed to fetch analytics');
   }
 };
+
+export const getSalesXProfitLastWeek = async () => {
+  try {
+    const response = await apiClient.get(`/analytics/store/SalesXProfitLastWeek`);
+    return response.data;
+  } catch (error: any) {
+    if (error.response) {
+      throw error.response.data;
+    }
+    throw new Error('Failed to fetch analytics');
+  }
+};
