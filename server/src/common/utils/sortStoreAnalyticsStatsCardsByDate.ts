b8216be49bbcf39993914b5079110efb10d8b60e
Osama Lmezouari <Oussamalm147@gmail.com>
2025-02-08 17:30:49 +0100

    refactor: remove commented-out code in sortStoreAnalyticsStatsCardsByDate utility

--- server/src/common/utils/sortStoreAnalyticsStatsCardsByDate.ts
+++ server/src/common/utils/sortStoreAnalyticsStatsCardsByDate.ts
@@ -10,17 +10,3 @@ export const sortStoreAnalyticsStatsCardsByDate = (
     (a, b) => new Date(a.day).getTime() - new Date(b.day).getTime(),
   );
 };
-
-/* export const sortStoreAnalyticsStatsCardsByDate = (
-  data: {
-    day: string;
-    totalProfit?: number;
-    totalAmount?: number;
-    orders?: number;
-  }[],
-) => {
-  return data.sort(
-    (a, b) => new Date(b.day).getTime() - new Date(a.day).getTime(),
-  );
-};
- */
