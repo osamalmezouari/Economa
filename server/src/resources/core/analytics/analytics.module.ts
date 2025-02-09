070eaae38009f36507babf6a4ed25a6eed0ad736
Osama Lmezouari <Oussamalm147@gmail.com>
2025-02-09 22:39:15 +0100

    feat: add ProductModule to analytics module imports

--- server/src/resources/core/analytics/analytics.module.ts
+++ server/src/resources/core/analytics/analytics.module.ts
@@ -3,9 +3,10 @@ import { AnalyticsService } from './analytics.service';
 import { AnalyticsController } from './analytics.controller';
 import { OrdersModule } from '../orders/orders.module';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { ProductModule } from '../product/product.module';
 
 @Module({
-  imports: [OrdersModule, PrismaModule],
+  imports: [OrdersModule, PrismaModule , ProductModule],
   controllers: [AnalyticsController],
   providers: [AnalyticsService],
 })
