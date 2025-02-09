215e3ddd9a2e037f6f8a725ccc1491a0fddce667
Osama Lmezouari <Oussamalm147@gmail.com>
2025-02-09 22:37:31 +0100

    refactor: remove UpdateProductReviewDto as it is no longer needed

--- server/src/resources/core/product-review/dto/update-product-review.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateProductReviewDto } from './create-product-review.dto';
-
-export class UpdateProductReviewDto extends PartialType(
-  CreateProductReviewDto,
-) {}
