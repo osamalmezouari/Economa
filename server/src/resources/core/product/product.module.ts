63f245f4031419b50e9a19af9f8bef3e13f8347c
Osama Lmezouari <Oussamalm147@gmail.com>
2025-02-09 22:39:24 +0100

    feat: restructure ProductModule to include dedicated services for product reviews and stock

--- server/src/resources/core/product/product.module.ts
+++ server/src/resources/core/product/product.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
-import { ProductService } from './product.service';
+import { ProductService } from './services/product.service';
 import { ProductController } from './product.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
-import { ProductReviewService } from '../product-review/product-review.service';
-import { ProductReviewModule } from '../product-review/product-review.module';
+import { ProductReviewService } from './services/product-review.service';
+import { ProductStockService } from './services/product-stock.service';
 
 @Module({
-  imports: [PrismaModule, ProductReviewModule],
+  imports: [PrismaModule],
   controllers: [ProductController],
-  providers: [ProductService],
+  providers: [ProductService, ProductReviewService, ProductStockService],
+  exports: [ProductStockService],
 })
 export class ProductModule {}
