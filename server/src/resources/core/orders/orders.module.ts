c081e4ed45e699069005eb0f909ae7caaa6fc245
Osama Lmezouari <Oussamalm147@gmail.com>
2025-02-10 17:49:16 +0100

    chore: update import path for OrdersService and add OrderItemService to OrdersModule

--- server/src/resources/core/orders/orders.module.ts
+++ server/src/resources/core/orders/orders.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
-import { OrdersService } from './orders.service';
+import { OrdersService } from './services/orders.service';
 import { OrdersController } from './orders.controller';
 import { PrismaModule } from '../../../prisma/prisma.module';
 import { CouponModule } from 'src/resources/billing/coupon/coupon.module';
 import { ShoppingCartModule } from 'src/resources/settings/shopping-cart/shopping-cart.module';
 import { BalanceModule } from 'src/resources/billing/balance/balance.module';
 import { PaymentModule } from 'src/resources/billing/payment/payment.module';
+import { OrderItemService } from './services/order-item.service';
 
 @Module({
   imports: [
@@ -16,7 +17,7 @@ import { PaymentModule } from 'src/resources/billing/payment/payment.module';
     PaymentModule,
   ],
   controllers: [OrdersController],
-  providers: [OrdersService],
+  providers: [OrdersService , OrderItemService],
   exports: [OrdersService],
 })
 export class OrdersModule {}
