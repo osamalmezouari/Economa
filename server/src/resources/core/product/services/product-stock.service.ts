b3953fb746ed9fcd2654643354f444e221ccd8f9
Osama Lmezouari <Oussamalm147@gmail.com>
2025-02-10 01:34:08 +0100

    feat: enhance low stock product retrieval with pagination and category details

--- server/src/resources/core/product/services/product-stock.service.ts
+++ server/src/resources/core/product/services/product-stock.service.ts
@@ -13,39 +13,58 @@ export class ProductStockService {
     const pageSize = 10;
     const { page, productName } = filter;
 
+    // Get the low stock products
     const lowStockProducts = await this.prisma.product.findMany({
       where: {
         stock: { lt: 50 },
-        name: { contains: productName },
+        name: { contains: productName || '' }, // Handling undefined productName
       },
       skip: (page - 1) * pageSize,
       take: pageSize,
-
       include: {
         Units: true,
         reviews: true,
         gallery: true,
+        category: true,
       },
     });
 
-    return lowStockProducts.map((product) => {
-      const avgRating = this.productreviewService.getProductAvgRating(
-        product.id,
-      );
-      return {
-        id: product.id,
-        name: product.name,
-        unit: product.Units.name,
-        stock: product.stock,
-        price: product.price,
-        rating: product.reviews.length,
-        avgRating: avgRating,
-        discount: product.discount ?? 0,
-        productImage: product.gallery[0].imageUrl,
-        productPageCount: pageSize,
-      };
+    // Get the total number of low stock products (needed for pagination)
+    const totalProductCount = await this.prisma.product.count({
+      where: {
+        stock: { lt: 50 },
+        name: { contains: productName || '' }, // Handle undefined productName
+      },
     });
+
+    // Calculate total pages
+    const productPageCount = Math.ceil(totalProductCount / pageSize);
+
+    const products = await Promise.all(
+      lowStockProducts.map(async (product) => {
+        const avgRating = await this.productreviewService.getProductAvgRating(
+          product.id,
+        );
+
+        return {
+          id: product.id,
+          name: product.name,
+          unit: product.Units.name,
+          stock: product.stock,
+          costprice: product.price,
+          reviews: product.reviews?.length || 0,
+          rating: avgRating,
+          productImage: product.gallery?.[0]?.imageUrl || '',
+          category: product.category.name,
+        };
+      }),
+    );
+    return {
+      products: products,
+      productPageCount,
+    };
   }
+
   async findStockForProduct(productId: string) {
     const result = await this.prisma.stockTransaction.groupBy({
       by: ['transactionType'],
