9d72d96a3aadd5bc675cefa4d463b90f6f767669
Osama Lmezouari <Oussamalm147@gmail.com>
2025-02-13 00:38:19 +0100

    chore: update CompareStateType to replace ApiError with string for error handling

--- client/src/types/compare.ts
+++ client/src/types/compare.ts
@@ -1,10 +1,9 @@
 import { CompareItemProps } from '../components/base/compareItem/interface';
-import { ApiError } from './apierror';
 
 export interface CompareType extends CompareItemProps {}
 export interface CompareStateType {
   compareItemsIds: string[];
   data: CompareType[];
   loading: boolean;
-  error: ApiError | null | undefined | unknown | string;
+  error: string;
 }
