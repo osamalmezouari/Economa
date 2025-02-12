c41848e8990ba84d2e264219d99191691e80d26c
Osama Lmezouari <Oussamalm147@gmail.com>
2025-02-12 21:41:21 +0100

    chore: improve error handling in place_and_pay_order function

--- client/src/api/orders.ts
+++ client/src/api/orders.ts
@@ -7,7 +7,7 @@ export const place_and_pay_order = async (data: place_and_pay_order_type) => {
     return response.data;
   } catch (error: any) {
     if (error.response) {
-      throw error.response.data;
+      throw error.response.data.message;
     }
     throw new Error('Failed to place and Pay the Order');
   }
