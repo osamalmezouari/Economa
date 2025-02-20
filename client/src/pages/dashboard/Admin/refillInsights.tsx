ebe282e5b420e364c28e9076abd5301f66208a46
Osama Lmezouari <Oussamalm147@gmail.com>
2025-02-20 23:04:33 +0100

    feat: create RefillInsights component to display refill statistics

---
+++ client/src/pages/dashboard/Admin/refillInsights.tsx
@@ -0,0 +1,11 @@
+import { Grid2 as Grid } from '@mui/material';
+import RefillStatCard from '../../../components/dashboards/AdminDashboard/refillStatCard';
+
+const RefillInsights = () => {
+  <Grid container className={'mt-16'}>
+    <Grid size={12} className={'px-4'}>
+      <RefillStatCard />
+    </Grid>
+  </Grid>;
+};
+export default RefillInsights;
