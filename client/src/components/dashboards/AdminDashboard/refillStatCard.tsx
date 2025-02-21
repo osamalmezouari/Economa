88a294073780dc1eabc8b2ffe55c6b99bd88546f
Osama Lmezouari <Oussamalm147@gmail.com>
2025-02-21 18:00:34 +0100

    feat: enhance RefillStatCard with new icons and improved layout

--- client/src/components/dashboards/AdminDashboard/refillStatCard.tsx
+++ client/src/components/dashboards/AdminDashboard/refillStatCard.tsx
@@ -1,81 +1,129 @@
-import { PiArrowDownRight, PiArrowUpRight } from 'react-icons/pi';
-import cn from '../../../utils/class-names';
+import {
+  PiArrowDownRight,
+  PiArrowUpRight,
+  PiCalendarCheck,
+  PiCheckCircle,
+  PiClock,
+  PiPhoneSlash,
+} from 'react-icons/pi';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from '../../../app/store';
 import { getRefillInsightsCardsStats } from '../../../features/RefillInsights/refillInsightsThunk';
+import { Card, CardContent, Typography, Grid, Grid2 } from '@mui/material';
+import cn from '../../../utils/class-names';
 
 const RefillStatCard = () => {
   const dispatch = useDispatch<AppDispatch>();
   const { data, loading } = useSelector(
     (state: RootState) => state.RefillInsights.RefillReqStatsCard
   );
+
   useEffect(() => {
     dispatch(getRefillInsightsCardsStats());
   }, [dispatch]);
 
+  const getIcon = (title: string) => {
+    switch (title) {
+      case 'Total Requests':
+        return <PiCalendarCheck className="w-[30px] h-auto" />;
+      case 'Pendding Requests':
+        return <PiClock className="w-[30px] h-auto" />;
+      case 'Approved Requests':
+        return <PiCheckCircle className="w-[30px] h-auto" />;
+      case 'Rejected Requests':
+        return <PiPhoneSlash className="w-[30px] h-auto" />;
+      default:
+        return null;
+    }
+  };
+
   return (
-    <>
+    <Grid container className="mt-4" gap={1}>
       {!loading && data.length > 0
-        ? data.map((stat) => {
-            return (
-              <div
+        ? data.map((stat, index) => (
+            <Grid2 size={2.9} key={stat.title}>
+              <Card
                 className={cn(
-                  'group w-full rounded-[14px] border border-gray-300 px-6 py-7 @container first:bg-[#2B7F75]'
+                  'group w-full rounded-[14px] border border-gray-300 p-2 shadow-none',
+                  index === 0 && 'bg-[#2B7F75] text-white'
                 )}
               >
-                <div className="mb-4 flex items-center gap-5">
-                  <span
-                    className={cn(
-                      'flex rounded-[14px] bg-[#2B7F75] p-2.5 text-gray-0 group-first:bg-gray-0 group-first:text-[#2B7F75] dark:text-gray-900 dark:group-first:bg-gray-900'
-                    )}
-                  >
-                    {/* <Icon className="h-auto w-[30px]" /> */}
-                  </span>
-                  <div className="space-y-1.5">
-                    <p className="font-medium text-gray-500 group-first:text-gray-100 dark:group-first:text-gray-800">
-                      {stat.title}
-                    </p>
-                    <p className="text-lg font-bold text-gray-900 group-first:text-gray-0 dark:text-gray-700 dark:group-first:text-gray-900 2xl:text-[20px] 3xl:text-3xl">
-                      {stat.metric}
-                    </p>
-                  </div>
-                </div>
-                <div className="flex items-center gap-1.5">
-                  <div
-                    className={cn(
-                      'flex items-center gap-1',
-                      stat.increased ? 'text-green-dark' : 'text-red-dark'
-                    )}
-                  >
+                <CardContent className="p-4">
+                  <div className="mb-4 flex items-center gap-5">
                     <span
                       className={cn(
-                        'flex rounded-full px-2.5 py-1.5 group-first:bg-gray-0 dark:group-first:bg-gray-900 dark:group-first:text-green-700',
-                        stat.increased
-                          ? 'bg-green-lighter/70 dark:bg-green-dark/30'
-                          : 'bg-red-lighter/70 dark:bg-red-dark/30'
+                        'flex rounded-[14px] p-2.5',
+                        index === 0
+                          ? 'bg-white text-[#2B7F75]'
+                          : 'bg-primary-main text-white'
                       )}
                     >
-                      {stat.increased ? (
-                        <PiArrowUpRight className="h-auto w-4" />
-                      ) : (
-                        <PiArrowDownRight className="h-auto w-4" />
-                      )}
+                      {getIcon(stat.title)}
                     </span>
-                    <span className="font-semibold leading-none group-first:text-gray-0 dark:group-first:text-gray-900">
-                      {stat.increased ? '+' : '-'}
-                      {stat.percentage}%
-                    </span>
+                    <div className="space-y-1.5">
+                      <Typography
+                        variant="body2"
+                        className={cn(
+                          'font-medium text-gray-500',
+                          index === 0 && 'text-gray-100'
+                        )}
+                      >
+                        {stat.title}
+                      </Typography>
+                      <Typography
+                        variant="h6"
+                        className={cn(
+                          'font-bold text-gray-900',
+                          index === 0 && 'text-gray-0'
+                        )}
+                      >
+                        {stat.metric}
+                      </Typography>
+                    </div>
                   </div>
-                  <span className="truncate leading-none text-gray-500 group-first:text-gray-100 dark:group-first:text-gray-800">
-                    {stat.increased ? 'Increased' : 'Decreased'}&nbsp;last month
-                  </span>
-                </div>
-              </div>
-            );
-          })
+                  <div className="flex items-center gap-1.5">
+                    <div
+                      className={cn(
+                        'flex items-center gap-1',
+                        stat.increased ? 'text-green-dark' : 'text-red-dark'
+                      )}
+                    >
+                      <span
+                        className={cn(
+                          'flex rounded-full px-2.5 py-1.5',
+                          index === 0 ? 'bg-white text-primary-main' : '',
+                          stat.increased && index != 0
+                            ? 'bg-green-200 text-green-600 '
+                            : '',
+                          stat.decreased && index != 0
+                            ? 'bg-red-200 text-red-600'
+                            : ''
+                        )}
+                      >
+                        {stat.increased && index != 0 ? (
+                          <PiArrowUpRight className="h-auto w-4" />
+                        ) : (
+                          <PiArrowDownRight className="h-auto w-4" />
+                        )}
+                      </span>
+                      <Typography variant="body2" className="font-semibold">
+                        {stat.percentage}%
+                      </Typography>
+                    </div>
+                    <Typography
+                      variant="caption"
+                      className={`${index === 0 ? 'text-white' : 'text-secondary-main'}`}
+                    >
+                      {stat.increased ? 'Increased' : 'Decreased'} last month
+                    </Typography>
+                  </div>
+                </CardContent>
+              </Card>
+            </Grid2>
+          ))
         : ''}
-    </>
+    </Grid>
   );
 };
 
