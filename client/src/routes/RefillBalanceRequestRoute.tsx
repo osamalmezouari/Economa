113b60a446f406af4ffa65181fd0531302b86ade
Osama Lmezouari <Oussamalm147@gmail.com>
2025-02-21 18:01:37 +0100

    fix: remove Refill Balance Request route for cleaner codebase

--- client/src/routes/RefillBalanceRequestRoute.tsx
+++
@@ -1,9 +0,0 @@
-import { createRoute } from '@tanstack/react-router';
-import RefillBalanceRequestPage from '../pages/refillBalanceRequestPage';
-import { mainRoute } from './__root';
-
-export const RefillBalanceRequestRoute = createRoute({
-  getParentRoute: () => mainRoute,
-  path: 'Refill_Balance',
-  component: RefillBalanceRequestPage,
-});
