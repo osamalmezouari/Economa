f0fbf72b2a4fac6f16061ee01b5254c30c89c551
Osama Lmezouari <Oussamalm147@gmail.com>
2025-02-21 18:01:01 +0100

    fix: remove landing route for cleaner codebase

--- client/src/routes/landing.tsx
+++
@@ -1,9 +0,0 @@
-import { createRoute } from '@tanstack/react-router';
-import { mainRoute } from './__root';
-import Landing from '../pages/landing';
-
-export const indexRoute = createRoute({
-  getParentRoute: () => mainRoute,
-  path: '/',
-  component: Landing,
-});
