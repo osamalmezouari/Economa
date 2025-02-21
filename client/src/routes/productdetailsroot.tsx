f43b8f3a6298f701dd583be4f7d29682b58cfce0
Osama Lmezouari <Oussamalm147@gmail.com>
2025-02-21 18:01:26 +0100

    fix: remove product details route for cleaner codebase

--- client/src/routes/productdetailsroot.tsx
+++
@@ -1,9 +0,0 @@
-import { createRoute } from '@tanstack/react-router';
-import { mainRoute } from './__root.tsx';
-import ProductDetailsPage from '../pages/productDetailsPage.tsx';
-
-export const productdetailsroot = createRoute({
-  getParentRoute: () => mainRoute,
-  path: 'Store/$ProductId',
-  component: ProductDetailsPage,
-});
