8ab971d23f4529a0ee7847a6b90699b3035bb25a
Osama Lmezouari <Oussamalm147@gmail.com>
2025-02-21 18:01:41 +0100

    fix: remove Refill Insights route for cleaner codebase

--- client/src/routes/refillInsights.tsx
+++
@@ -1,9 +0,0 @@
-import { createRoute } from '@tanstack/react-router';
-import { adminRoute } from './__root';
-import RefillInsights from '../pages/dashboard/Admin/refillInsights';
-
-export const RefillInsightsRoute = createRoute({
-  getParentRoute: () => adminRoute,
-  path: '/RefillInsights',
-  component: RefillInsights,
-});
