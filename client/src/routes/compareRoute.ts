7b82f7a570847ed64d5a8a2311d7dd2eaf2b54ab
Osama Lmezouari <Oussamalm147@gmail.com>
2025-02-21 18:00:58 +0100

    feat: add compare route for comparison functionality

---
+++ client/src/routes/compareRoute.ts
@@ -0,0 +1,9 @@
+import { createRoute } from '@tanstack/react-router';
+import { mainRoute } from './__root';
+import Compare from '../components/extra/compare/compare';
+
+export const compareRoute = createRoute({
+  getParentRoute: () => mainRoute,
+  path: 'compare',
+  component: Compare,
+});
