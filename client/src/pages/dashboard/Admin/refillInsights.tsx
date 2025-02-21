7f15ee1611713d4caa506bc2905bde07a99c8909
Osama Lmezouari <Oussamalm147@gmail.com>
2025-02-21 18:00:41 +0100

    fix: add missing return statement in RefillInsights component

--- client/src/pages/dashboard/Admin/refillInsights.tsx
+++ client/src/pages/dashboard/Admin/refillInsights.tsx
@@ -2,10 +2,12 @@ import { Grid2 as Grid } from '@mui/material';
 import RefillStatCard from '../../../components/dashboards/AdminDashboard/refillStatCard';
 
 const RefillInsights = () => {
-  <Grid container className={'mt-16'}>
-    <Grid size={12} className={'px-4'}>
-      <RefillStatCard />
+  return (
+    <Grid container className={'mt-16'}>
+      <Grid size={12} className={'px-2'}>
+        <RefillStatCard />
+      </Grid>
     </Grid>
-  </Grid>;
+  );
 };
 export default RefillInsights;
