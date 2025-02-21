792900effbe52b770067569aab8eadd497420b23
Osama Lmezouari <Oussamalm147@gmail.com>
2025-02-21 18:01:18 +0100

    fix: remove placeOrder route for cleaner codebase

--- client/src/routes/placeOrder.tsx
+++
@@ -1,9 +0,0 @@
-import { createRoute } from '@tanstack/react-router';
-import { mainRoute } from './__root';
-import Order from '../pages/order';
-
-export const OrderRoute = createRoute({
-  getParentRoute: () => mainRoute,
-  path: '/placeOrder',
-  component: Order,
-});
