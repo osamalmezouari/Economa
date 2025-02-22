c5e575ee8e23b69e7d53ef381b78367317064e18
Osama Lmezouari <Oussamalm147@gmail.com>
2025-02-22 23:57:56 +0100

    feat: integrate RefillDaily component into RefillInsights dashboard

--- client/src/pages/dashboard/Admin/refillInsights.tsx
+++ client/src/pages/dashboard/Admin/refillInsights.tsx
@@ -1,6 +1,7 @@
 import { Grid2 as Grid } from '@mui/material';
 import RefillStatCard from '../../../components/dashboards/AdminDashboard/refillStatCard';
 import RefillYearlyChart from '../../../components/dashboards/AdminDashboard/refillYearlychart';
+import RefillDaily from '../../../components/dashboards/AdminDashboard/RefillDaily';
 
 const RefillInsights = () => {
   return (
@@ -10,6 +11,9 @@ const RefillInsights = () => {
       </Grid>
       <Grid size={12} className={'px-2 mt-6'}>
         <RefillYearlyChart />
+      </Grid>
+      <Grid size={12} className={'px-2 mt-6'}>
+        <RefillDaily />
       </Grid>
     </Grid>
   );
