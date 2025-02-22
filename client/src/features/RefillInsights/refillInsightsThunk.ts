baf69c8a181c3321e34fada089cc061699edccb7
Osama Lmezouari <Oussamalm147@gmail.com>
2025-02-22 20:26:45 +0100

    feat: add getRefillInsightsYearlyChart thunk for fetching yearly refill data

--- client/src/features/RefillInsights/refillInsightsThunk.ts
+++ client/src/features/RefillInsights/refillInsightsThunk.ts
@@ -1,17 +1,35 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import { getRefillStatsCard } from "../../api/refillinsights";
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import {
+  getRefillStatsCard,
+  getRefillYearlyChart,
+} from '../../api/refillinsights';
 
 export const getRefillInsightsCardsStats = createAsyncThunk(
-    'analytics/RefillInsights/StatsCards',
-    async (_, { rejectWithValue }) => {
-      try {
-        const CardsStats = await getRefillStatsCard();
-        return CardsStats;
-      } catch (error: any) {
-        if (error.response) {
-          return rejectWithValue(error.response.data);
-        }
-        return rejectWithValue('Failed to fetch refill stats');
+  'analytics/RefillInsights/StatsCards',
+  async (_, { rejectWithValue }) => {
+    try {
+      const CardsStats = await getRefillStatsCard();
+      return CardsStats;
+    } catch (error: any) {
+      if (error.response) {
+        return rejectWithValue(error.response.data);
       }
+      return rejectWithValue('Failed to fetch refill stats');
     }
-  );
+  }
+);
+
+export const getRefillInsightsYearlyChart = createAsyncThunk(
+  'analytics/RefillInsights/YearlyChart',
+  async (year: string, { rejectWithValue }) => {
+    try {
+      const YearlyChart = await getRefillYearlyChart(year);
+      return YearlyChart;
+    } catch (error: any) {
+      if (error.response) {
+        return rejectWithValue(error.response.data);
+      }
+      return rejectWithValue('Failed to fetch refill chart');
+    }
+  }
+);
