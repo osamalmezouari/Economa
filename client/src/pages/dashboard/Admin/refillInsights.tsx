ec3bc06f1e1f20b903a216d96d6edbb410acb312
Osama Lmezouari <Oussamalm147@gmail.com>
2025-02-22 20:26:48 +0100

    feat: add RefillYearlyChart component to RefillInsights for yearly data visualization

--- client/src/pages/dashboard/Admin/refillInsights.tsx
+++ client/src/pages/dashboard/Admin/refillInsights.tsx
@@ -1,11 +1,15 @@
 import { Grid2 as Grid } from '@mui/material';
 import RefillStatCard from '../../../components/dashboards/AdminDashboard/refillStatCard';
+import RefillYearlyChart from '../../../components/dashboards/AdminDashboard/refillYearlychart';
 
 const RefillInsights = () => {
   return (
     <Grid container className={'mt-16'}>
       <Grid size={12} className={'px-2'}>
         <RefillStatCard />
+      </Grid>
+      <Grid size={12} className={'px-2 mt-6'}>
+        <RefillYearlyChart />
       </Grid>
     </Grid>
   );
