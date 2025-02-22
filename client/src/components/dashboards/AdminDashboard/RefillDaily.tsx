d16c7a4c3a4f0800972d3d575bb6c1af06214b82
Osama Lmezouari <Oussamalm147@gmail.com>
2025-02-22 21:29:28 +0100

    feat: add RefillDaily component for daily requests overview

---
+++ client/src/components/dashboards/AdminDashboard/RefillDaily.tsx
@@ -0,0 +1,8 @@
+import { Card, CardHeader } from '@mui/material';
+
+const RefillDaily = () => {
+  <Card>
+    <CardHeader title="Daily Requests Overview" />
+  </Card>;
+};
+export default RefillDaily;
