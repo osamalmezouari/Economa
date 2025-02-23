6e1a42600d363502e725a8fa6f6232a07b792495
Osama Lmezouari <Oussamalm147@gmail.com>
2025-02-23 22:59:29 +0000

    feat: integrate UsersTransfers component into RefillInsights dashboard

--- client/src/pages/dashboard/Admin/refillInsights.tsx
+++ client/src/pages/dashboard/Admin/refillInsights.tsx
@@ -2,6 +2,7 @@ import { Grid2 as Grid } from '@mui/material';
 import RefillStatCard from '../../../components/dashboards/AdminDashboard/refillStatCard';
 import RefillYearlyChart from '../../../components/dashboards/AdminDashboard/refillYearlychart';
 import RefillDaily from '../../../components/dashboards/AdminDashboard/RefillDaily';
+import UsersTransfers from '../../../components/dashboards/AdminDashboard/usertransfers';
 
 const RefillInsights = () => {
   return (
@@ -14,6 +15,9 @@ const RefillInsights = () => {
       </Grid>
       <Grid size={12} className={'px-2 mt-6'}>
         <RefillDaily />
+      </Grid>
+      <Grid size={12} className={'px-2 mt-6'}>
+        <UsersTransfers />
       </Grid>
     </Grid>
   );
