d18cfb8cb3d5c8aef97526644866deb4195d0462
Osama Lmezouari <Oussamalm147@gmail.com>
2025-03-03 13:30:52 +0000

    feat: add ReviewCard component for displaying user reviews in the admin panel

---
+++ client/src/components/base/reviewCard.tsx
@@ -0,0 +1,57 @@
+import { Avatar, Grid, Rating } from '@mui/material';
+import Review from '../../../types/review';
+
+const ReviewCard = ({
+  id,
+  user,
+  productId,
+  rating,
+  reviewText,
+  createdAt,
+}: Review) => {
+  const formatDateTime = (dateString: string) => {
+    const date = new Date(dateString);
+    return date.toLocaleDateString('En-MA', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    });
+  };
+
+  return (
+    <Grid
+      container
+      gap={2}
+      className="review-card flex rounded  p-4 border-[1px] my-2 border-solid border-gray-200 max-w-[1150px] mx-auto"
+    >
+      <Grid item lg={1} className="flex items-center justify-center">
+        <Avatar
+          src={user.name || ''}
+          alt={user.name || user.name.trim().charAt(0).toUpperCase()}
+          sx={{
+            width: 70,
+            height: 70,
+          }}
+        />
+      </Grid>
+      <Grid item lg={10}>
+        <Grid item lg={12}>
+          <p>{user.name || 'Anonymous'}</p>
+        </Grid>
+        <Grid item lg={12} className="flex items-center gap-2">
+          <Rating size="small" value={rating || 0} disabled />
+          <p className="text-secondary-light py-2 text-[12px]">
+            {createdAt ? formatDateTime(createdAt) : ''}
+          </p>
+        </Grid>
+        <Grid item lg={12}>
+          <p className="text-secondary-main text-[14px] w-11/12">
+            {reviewText || 'No review text provided.'}
+          </p>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default ReviewCard;
