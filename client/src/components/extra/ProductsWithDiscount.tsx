be50d909de7e6f58cfe6aa52e0433dc8152625fa
Osama Lmezouari <Oussamalm147@gmail.com>
2025-03-04 23:15:31 +0000

    feat: add ProductsWithDiscount component to display discounted products with a title

---
+++ client/src/components/extra/ProductsWithDiscount.tsx
@@ -0,0 +1,13 @@
+import ProductCardContainer from "./productCardContainer";
+import TitleOne from "./Titles/TitleOne";
+
+
+const ProductsWithDiscount = () => {
+  return (
+    <>
+      <TitleOne />
+      <ProductCardContainer />
+    </>
+  );
+};
+export default ProductsWithDiscount;
