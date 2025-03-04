548b3e926c8a624c76673666b50db01312ed6b0a
Osama Lmezouari <Oussamalm147@gmail.com>
2025-03-04 23:14:56 +0000

    feat: add NewArrivalsSection component to display new arrivals with title

---
+++ client/src/components/extra/newArrivalsSection.tsx
@@ -0,0 +1,14 @@
+import ProductnewArrivalsContainer from "./productnewArrivalsContainer";
+import TitleTwo from "./Titles/TitleTwo";
+
+
+const NewArrivalsSection = () => {
+  return (
+    <>
+      <TitleTwo />
+      <ProductnewArrivalsContainer />
+    </>
+  );
+};
+
+export default NewArrivalsSection;
