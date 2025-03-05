eb6d6b0beffd55a1e939b354a6c3ac929d0451cf
Osama Lmezouari <Oussamalm147@gmail.com>
2025-03-05 23:28:19 +0000

    feat: add CategoriesnamesandIds state and thunk for fetching category names and IDs

--- client/src/features/category/categorySlice.ts
+++ client/src/features/category/categorySlice.ts
@@ -1,9 +1,18 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { CategoryStateType, CategoryType } from '../../types/category';
-import { getCategoryCards } from './categoryThunk';
+import {
+  CategoriesnamesandIds,
+  CategoryStateType,
+  CategoryType,
+} from '../../types/category';
+import { getCategoriesNamesandIds, getCategoryCards } from './categoryThunk';
 
 const initialState: CategoryStateType = {
   CategoryCards: {
+    data: [],
+    loading: false,
+    error: '',
+  },
+  CategoriesnamesandIds: {
     data: [],
     loading: false,
     error: '',
@@ -31,6 +40,23 @@ const categorySlice = createSlice({
       .addCase(getCategoryCards.rejected, (state, action) => {
         state.CategoryCards.loading = false;
         state.CategoryCards.error = action.payload as string;
+      })
+
+      .addCase(getCategoriesNamesandIds.pending, (state) => {
+        state.CategoriesnamesandIds.loading = true;
+        state.CategoriesnamesandIds.error = '';
+      })
+      .addCase(
+        getCategoriesNamesandIds.fulfilled,
+        (state, action: PayloadAction<CategoriesnamesandIds[]>) => {
+          state.CategoriesnamesandIds.loading = false;
+          state.CategoriesnamesandIds.error = '';
+          state.CategoriesnamesandIds.data = action.payload;
+        }
+      )
+      .addCase(getCategoriesNamesandIds.rejected, (state, action) => {
+        state.CategoriesnamesandIds.loading = false;
+        state.CategoriesnamesandIds.error = action.payload as string;
       });
   },
 });
