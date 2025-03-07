568dfc649fd52a91037b930da5de4d5275e7d663
Osama Lmezouari <Oussamalm147@gmail.com>
2025-03-07 00:01:51 +0000

    feat: add thunks for managing products including create, update, and fetch by ID

--- client/src/features/products/productThunk.ts
+++ client/src/features/products/productThunk.ts
@@ -2,7 +2,11 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import * as productsApi from '../../api/products';
 import { AddReview } from '../../types/review';
 import { addreview } from '../../api/productReview';
-import { StoreFilters } from '../../types/product';
+import {
+  ManageProductsFilters,
+  ManageProductsResponse,
+  StoreFilters,
+} from '../../types/product';
 
 export const getProductsStore = createAsyncThunk(
   '/products/store',
@@ -78,6 +82,77 @@ export const addReview = createAsyncThunk(
     try {
       const review = await addreview(reviewobj);
       return review;
+    } catch (error: any) {
+      if (error) {
+        return rejectWithValue(error);
+      }
+      return rejectWithValue({
+        message: error.message || 'Something went wrong',
+      });
+    }
+  }
+);
+
+export const getManageProductsTable = createAsyncThunk<
+  ManageProductsResponse,
+  ManageProductsFilters
+>('/products/manageProductsTable', async (filters, { rejectWithValue }) => {
+  try {
+    const response = await productsApi.getManageProductsTable(filters);
+    return response;
+  } catch (error: any) {
+    if (error) {
+      return rejectWithValue(error);
+    }
+    return rejectWithValue({
+      message: error.message || 'Something went wrong',
+    });
+  }
+});
+
+export const CreateProduct = createAsyncThunk(
+  '/products/create',
+  async (data: any, { rejectWithValue }) => {
+    try {
+      const product = await productsApi.CreateProduct(data);
+      return product;
+    } catch (error: any) {
+      if (error) {
+        return rejectWithValue(error);
+      }
+      return rejectWithValue({
+        message: error.message || 'Something went wrong',
+      });
+    }
+  }
+);
+
+export const UpdateProduct = createAsyncThunk(
+  '/products/update',
+  async (
+    { formData, productId }: { formData: FormData; productId: string },
+    { rejectWithValue }
+  ) => {
+    try {
+      const product = await productsApi.UpdateProduct(productId, formData);
+      return product;
+    } catch (error: any) {
+      if (error) {
+        return rejectWithValue(error);
+      }
+      return rejectWithValue({
+        message: error.message || 'Something went wrong',
+      });
+    }
+  }
+);
+
+export const getProductById = createAsyncThunk(
+  '/products/byId',
+  async (productId: string, { rejectWithValue }) => {
+    try {
+      const product = await productsApi.getproductById(productId);
+      return product;
     } catch (error: any) {
       if (error) {
         return rejectWithValue(error);
