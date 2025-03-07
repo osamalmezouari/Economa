c030b3671fe6071b976f2951e0f4852f5aac538e
Osama Lmezouari <Oussamalm147@gmail.com>
2025-03-07 00:03:22 +0000

    feat: reorganize imports and add AddProductDialog component to AdminRoute

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -4,24 +4,26 @@ import {
   createRouter,
   Outlet,
 } from '@tanstack/react-router';
-import Navbar from '../components/extra/navbar/Navbar';
-import Footer from '../components/extra/footer/footer';
+import Footer from '../components/extra/footer.tsx';
 import { LoginRoute, RegisterRoute } from './authRoute.ts';
 import { indexRoute } from './landingRoute.ts';
 import { compareRoute } from './compareRoute.ts';
 import { StoreRoute } from './storeRoute.ts';
-import Wishlist from '../layouts/wishlist.tsx';
-import ShoppingCart from '../layouts/shoopingCart.tsx';
-import GlobalAlert from '../components/base/GlobalAlerts/globalAlert';
+import Wishlist from '../components/extra/wishlist.tsx';
+import ShoppingCart from '../components/extra/shoopingCart.tsx';
 import { productdetailsRoute } from './productdetailsRoute.ts';
 import { RefillBalanceRequestRoute } from './RefillBalanceRequestRoute.ts';
 import { OrderRoute } from './placeOrderRoute.ts';
-import Header from '../components/dashboards/shared/header.tsx';
 import { OverviewRoute } from './overviewRoute.ts';
-import Sidebar from '../components/dashboards/shared/sidebar/sideBar.tsx';
+import Sidebar from '../components/admin/extra/sidebar/sideBar.tsx';
 import { Box } from '@mui/material';
 import { StoreAnalyticsRoute } from './storeAnalytcsRoute.ts';
 import { RefillInsightsRoute } from './refillInsightsRoute.ts';
+import GlobalAlert from '../components/base/globalAlert.tsx';
+import Navbar from '../components/extra/Navbar.tsx';
+import { ManageProductsRoute } from './manageProductsRoote.ts';
+import Header from '../components/admin/extra/header.tsx';
+import AddProductDialog from '../components/admin/extra/addproduct.tsx';
 
 // Define the root route
 export const rootRoute = createRootRoute({
@@ -82,6 +84,7 @@ function AdminRoute() {
         </Box>
         <Box p={2} flexGrow={1}>
           <Outlet />
+          <AddProductDialog />
         </Box>
       </Box>
     </Box>
@@ -103,8 +106,8 @@ const routeTree = rootRoute.addChildren([
   OrderRoute,
   OverviewRoute,
   RefillInsightsRoute,
-
   StoreAnalyticsRoute,
+  ManageProductsRoute,
 ]);
 
 // Create the router
