9cfa115c8c535929481d140ea26155793aaaf51f
Osama Lmezouari <Oussamalm147@gmail.com>
2025-03-08 18:05:39 +0000

    feat: update CreateProductDto to include file upload support and enhance validation

--- server/src/resources/core/product/dto/create-product.dto.ts
+++ server/src/resources/core/product/dto/create-product.dto.ts
@@ -1,21 +1,33 @@
-import { IsInt, IsNumber, IsString, IsUUID, MaxLength } from 'class-validator';
+import { Transform } from 'class-transformer';
+import {
+  IsInt,
+  IsNumber,
+  IsOptional,
+  IsString,
+  IsUUID,
+  MaxLength,
+} from 'class-validator';
 
 export class CreateProductDto {
   @IsString()
   name: string;
   @IsString()
-  @MaxLength(150)
+  @MaxLength(200)
   description: string;
   @IsNumber()
+  @Transform(({ value }) => parseInt(value))
   discount?: number;
+  @Transform(({ value }) => parseInt(value))
   @IsNumber()
   price: number;
   @IsNumber()
+  @Transform(({ value }) => parseInt(value))
   cost_price: number;
-  @IsInt()
-  stock: number;
   @IsUUID()
   categoryId: string;
+  @IsOptional()
+  stock: number;
   @IsString()
-  unitId: string;
+  unitname: string;
+  file: Express.Multer.File;
 }
