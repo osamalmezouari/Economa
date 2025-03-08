3c3f617520af99f6644ac4eaf3c45d59c7d1ffc9
Osama Lmezouari <Oussamalm147@gmail.com>
2025-03-08 18:05:57 +0000

    feat: enhance ProductService with image handling and pagination for product management

--- server/src/resources/core/product/services/product.service.ts
+++ server/src/resources/core/product/services/product.service.ts
@@ -2,26 +2,151 @@ import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from '../dto/create-product.dto';
 import { UpdateProductDto } from '../dto/update-product.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { v4 as uuid } from 'uuid';
 import { PRODUCT_NOT_FOUND_Exception } from 'src/common/exceptions/PRODUCT_NOT_FOUND.exception';
 import { StoreFiltersDto } from 'src/resources/core/product/dto/storeFilters.dto';
-import { contains } from 'class-validator';
 import { ProductReviewService } from './product-review.service';
-import { CreateProductReviewDto } from '../dto/create-product-review.dto';
+import { ManageProductsTableDto } from '../dto/manageProductsTable.dto';
+import { Unsupported_FILE_Exception } from 'src/common/exceptions/UNSPORTED_FILE.exception';
+import * as fs from 'fs';
+import * as path from 'path';
+import { v4 as uuid } from 'uuid';
+import { GalleryService } from 'src/resources/media/gallery/gallery.service';
 @Injectable()
 export class ProductService {
   constructor(
     private readonly prisma: PrismaService,
     private readonly reviewService: ProductReviewService,
+    private readonly galleryService: GalleryService,
   ) {}
   async create(createProductDto: CreateProductDto) {
+    // Find the product category by its ID
+    const productCategoryname = await this.prisma.category.findUnique({
+      where: {
+        id: createProductDto.categoryId,
+      },
+      select: {
+        name: true,
+      },
+    });
+
+    // Check if the unit already exists
+    const findProductUnit = await this.prisma.units.findFirst({
+      where: {
+        name: createProductDto.unitname,
+      },
+    });
+
+    let unitId: string;
+
+    // If the unit exists, use the existing unitId
+    if (findProductUnit) {
+      unitId = findProductUnit.id;
+    } else {
+      // If the unit doesn't exist, create a new unit and get the unitId
+      const newUnit = await this.prisma.units.create({
+        data: {
+          id: uuid(),
+          name: createProductDto.unitname,
+        },
+      });
+      unitId = newUnit.id;
+    }
+
+    const unitname = await this.prisma.units
+      .findFirst({
+        where: {
+          id: unitId,
+        },
+        select: {
+          name: true,
+        },
+      })
+      .then((data) => data.name);
+
+    // Now you can use the unitId to create a new product or do further logic
     const product = await this.prisma.product.create({
       data: {
         id: uuid(),
-        ...createProductDto,
+        categoryId: createProductDto.categoryId,
+        unitId,
+        name: createProductDto.name,
+        description: createProductDto.description,
+        discount: createProductDto.discount,
+        price: createProductDto.price,
+        cost_price: createProductDto.cost_price,
+        stock: 0,
       },
     });
+    const path = this.storeProductImage(
+      product.id,
+      createProductDto.file,
+      `${productCategoryname.name}`,
+    );
+    await this.galleryService.StoreProductImage(path, product.id);
     return product;
+  }
+
+  async update(productId: string, updateProductDto: UpdateProductDto) {
+    // Find the existing product
+    const existingProduct = await this.prisma.product.findUnique({
+      where: { id: productId },
+      include: { Units: true, category: true },
+    });
+
+    // Find the product category by its ID
+    const productCategoryname = await this.prisma.category.findUnique({
+      where: { id: updateProductDto.categoryId },
+      select: { name: true },
+    });
+
+    // Check if the unit exists or create a new one
+    let unitId = existingProduct.unitId;
+    if (updateProductDto.unitname !== existingProduct.Units.name) {
+      const existingUnit = await this.prisma.units.findFirst({
+        where: { name: updateProductDto.unitname },
+      });
+
+      if (existingUnit) {
+        unitId = existingUnit.id;
+      } else {
+        const newUnit = await this.prisma.units.create({
+          data: { id: uuid(), name: updateProductDto.unitname },
+        });
+        unitId = newUnit.id;
+      }
+    }
+
+    // Update product details
+    const updatedProduct = await this.prisma.product.update({
+      where: { id: productId },
+      data: {
+        categoryId: updateProductDto.categoryId,
+        unitId,
+        name: updateProductDto.name,
+        description: updateProductDto.description,
+        discount: updateProductDto.discount,
+        price: updateProductDto.price,
+        cost_price: updateProductDto.cost_price,
+      },
+    });
+
+    // Handle file update if a new file is provided
+    if (updateProductDto.file) {
+      const unitname = await this.prisma.units.findFirst({
+        where: { id: unitId },
+        select: { name: true },
+      });
+
+      const imagePath = this.storeProductImage(
+        updatedProduct.id,
+        updateProductDto.file,
+        `${productCategoryname.name}`,
+      );
+
+      await this.galleryService.StoreProductImage(imagePath, updatedProduct.id);
+    }
+
+    return updatedProduct;
   }
 
   async findAll() {
@@ -30,8 +155,35 @@ export class ProductService {
   }
 
   async findOne(id: string) {
-    const product = await this.prisma.product.findUnique({ where: { id } });
-    if (product) return product;
+    const product = await this.prisma.product.findUnique({
+      where: { id },
+      include: {
+        Units: {
+          select: {
+            name: true,
+          
+          },
+        },
+        category: {
+          select: {
+            id: true,
+          },
+        },
+      },
+    });
+    if (product)
+      return {
+        id: product.id,
+        name: product.name,
+        description: product.description,
+        price: product.price,
+        cost_price: product.cost_price,
+        discount: product.discount,
+        unitname: product.Units.name,
+        categoryId: product.category.id,
+        stock : product.stock
+      };
+
     if (!product) throw new PRODUCT_NOT_FOUND_Exception(id);
   }
 
@@ -469,18 +621,152 @@ export class ProductService {
     return productDetails;
   }
 
-  async update(id: string, updateProductDto: UpdateProductDto) {
-    await this.findOne(id);
-    const product = await this.prisma.product.update({
-      where: { id },
-      data: updateProductDto,
-    });
-    return product;
-  }
+  ManageProductsTable = async ({
+    page = 1,
+    search = '',
+    category,
+    min_price,
+    max_price,
+    min_stock,
+    max_stock,
+  }: ManageProductsTableDto) => {
+    const itemsPerPage = 10;
+    search = search.replace(/%20/g, ' ');
+    const filters: any = {
+      name: {
+        contains: search,
+      },
+    };
 
-  async remove(id: string) {
-    await this.findOne(id);
-    const product = await this.prisma.product.delete({ where: { id } });
-    return product;
+    if (category && category !== '') {
+      // Only add the category filter if it's a valid string
+      filters.category = {
+        name: category,
+      };
+    }
+
+    if (min_price !== undefined || max_price !== undefined) {
+      filters.price = {};
+      if (min_price !== undefined) filters.price.gte = min_price;
+      if (max_price !== undefined) filters.price.lte = max_price;
+    }
+
+    if (min_stock !== undefined || max_stock !== undefined) {
+      filters.stock = {};
+      if (min_stock === undefined || min_stock === 0) {
+        filters.stock.gte = 0;
+      } else {
+        filters.stock.gte = min_stock;
+      }
+      if (max_stock !== undefined) filters.stock.lte = max_stock;
+    }
+
+    const totalProducts = await this.prisma.product.count({
+      where: filters,
+    });
+    page = Math.max(1, page);
+    const totalPages = Math.ceil(totalProducts / itemsPerPage);
+
+    const products = await this.prisma.product.findMany({
+      take: itemsPerPage,
+      skip: (page - 1) * itemsPerPage,
+      where: filters,
+      include: {
+        category: {
+          select: {
+            name: true,
+          },
+        },
+        Units: {
+          select: {
+            name: true,
+          },
+        },
+        gallery: {
+          select: {
+            imageUrl: true,
+          },
+        },
+      },
+    });
+
+    const productsWithAvgRating = await Promise.all(
+      products.map(async (product) => {
+        const cappedAvgRating = await this.reviewService.getProductAvgRating(
+          product.id,
+        );
+        return {
+          id: product.id,
+          name: product.name,
+          price: product.price,
+          description: product.description,
+          productAvgRating: cappedAvgRating || 0,
+          costprice: product.cost_price,
+          stock: product.stock,
+          priceWithDiscount: parseFloat(
+            (product.price - (product.price * product.discount) / 100).toFixed(
+              2,
+            ),
+          ),
+          categoryName: product.category.name,
+          unit: product.Units.name,
+          imageLink: product.gallery[0]?.imageUrl || null,
+        };
+      }),
+    );
+
+    return {
+      products: productsWithAvgRating,
+      totalProducts,
+      productspageCount: totalPages,
+    };
+  };
+
+  private storeProductImage(
+    productId: string,
+    file: Express.Multer.File,
+    storepath: string,
+  ): string {
+    const documentsRoot = path.join(
+      'D:/Oussama/PROJECTS/Economa/client/public/assets/products',
+      storepath,
+    );
+    const fileExtension = path.extname(file.originalname).toLowerCase();
+
+    if (!['.png', '.jpg', '.jpeg'].includes(fileExtension)) {
+      throw new Unsupported_FILE_Exception(fileExtension);
+    }
+
+    const fileName = `${productId}${fileExtension}`;
+    const filePath = path.join(documentsRoot, fileName);
+
+    try {
+      // Ensure the directory exists
+      if (!fs.existsSync(documentsRoot)) {
+        fs.mkdirSync(documentsRoot, { recursive: true });
+      }
+
+      // Check if an existing image for the product exists
+      const existingFiles = fs
+        .readdirSync(documentsRoot)
+        .filter((f) => f.startsWith(productId));
+
+      if (existingFiles.length > 0) {
+        // Delete existing files before writing a new one (handles update case)
+        existingFiles.forEach((existingFile) => {
+          fs.unlinkSync(path.join(documentsRoot, existingFile));
+        });
+      }
+
+      // Write the new file
+      fs.writeFileSync(filePath, file.buffer);
+
+      // Return the stored image path (relative to public folder)
+      return filePath
+        .replace(/\\/g, '/')
+        .replace('D:/Oussama/PROJECTS/Economa/client/public', '');
+    } catch (error) {
+      throw new Error(`Error while storing file: ${error.message}`);
+    }
   }
 }
