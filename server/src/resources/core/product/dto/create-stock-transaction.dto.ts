9fb2eab6e0aa0c66cb7ea6e870e35fa70ec0c646
Osama Lmezouari <Oussamalm147@gmail.com>
2025-03-09 19:40:29 +0000

    feat: update CreateStockTransactionDto to make unitCost optional and add transformation for quantity and unitCost

--- server/src/resources/core/product/dto/create-stock-transaction.dto.ts
+++ server/src/resources/core/product/dto/create-stock-transaction.dto.ts
@@ -1,13 +1,19 @@
 import { TransactionType } from '@prisma/client';
-import { IsNumber, IsString, IsUUID } from 'class-validator';
+import { Transform } from 'class-transformer';
+import { IsNumber, IsOptional, IsString, IsUUID } from 'class-validator';
 
 export class CreateStockTransactionDto {
   @IsUUID()
   productId: string;
   @IsString()
   transactionType: TransactionType;
+
   @IsNumber()
+  @Transform(({ value }) => parseInt(value))
   quantity: number;
+
   @IsNumber()
-  unitCost: number;
+  @IsOptional()
+  @Transform(({ value }) => parseInt(value))
+  unitCost?: number;
 }
