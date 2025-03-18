333f86cd6c867f847a4808613511ea868d52137f
Osama Lmezouari <Oussamalm147@gmail.com>
2025-03-18 16:49:52 +0000

    feat: implement image upload and storage for category creation and update

--- server/src/resources/core/category/category.service.ts
+++ server/src/resources/core/category/category.service.ts
@@ -4,14 +4,25 @@ import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
 import { v4 as uuid } from 'uuid';
 import { CATEGORY_NOT_FOUND_Exception } from '../../../common/exceptions/CATEGORY_NOT_FOUND.exception';
-
+import * as fs from 'fs';
+import * as path from 'path';
+import { Unsupported_FILE_Exception } from 'src/common/exceptions/UNSPORTED_FILE.exception';
 @Injectable()
 export class CategoryService {
   constructor(private readonly prisma: PrismaService) {}
 
   async create(createCategoryDto: CreateCategoryDto) {
+    const imagepath = this.storeCategoryImage(
+      createCategoryDto.name,
+      createCategoryDto.file,
+    );
     const category = await this.prisma.category.create({
-      data: { id: uuid(), ...createCategoryDto },
+      data: {
+        id: uuid(),
+        description: createCategoryDto.description,
+        name: createCategoryDto.name,
+        svgLink: imagepath,
+      },
     });
     return category;
   }
@@ -54,11 +65,26 @@ export class CategoryService {
     return category;
   }
 
-  async update(id: string, updateCategoryDto: UpdateCategoryDto) {
-    await this.findOne(id);
+  async update(categoryId: string, updateCategoryDto: UpdateCategoryDto) {
+    const targetcategory = await this.findOne(categoryId);
+    if (updateCategoryDto.file) {
+      const imagepath = this.storeCategoryImage(
+        targetcategory.name,
+        updateCategoryDto.file,
+      );
+      const category = await this.prisma.category.update({
+        where: {
+          id: categoryId,
+        },
+        data: { ...updateCategoryDto, svgLink: imagepath },
+      });
+      return category;
+    }
     const category = await this.prisma.category.update({
-      where: { id },
-      data: updateCategoryDto,
+      where: {
+        id: categoryId,
+      },
+      data: { ...updateCategoryDto },
     });
     return category;
   }
@@ -67,5 +93,39 @@ export class CategoryService {
     await this.findOne(id);
     const category = await this.prisma.category.delete({ where: { id } });
     return category;
+  }
+
+  private storeCategoryImage(
+    categoryName: string,
+    file: Express.Multer.File,
+  ): string {
+    const documentsRoot = path.join(
+      'D:/Oussama/PROJECTS/Economa/client/public/assets/categories',
+    );
+    const fileExtension = path.extname(file.originalname).toLowerCase();
+    if (!['.png', '.jpg', '.jpeg'].includes(fileExtension)) {
+      throw new Unsupported_FILE_Exception(fileExtension);
+    }
+    const fileName = `${categoryName}${fileExtension}`;
+    const filePath = path.join(documentsRoot, fileName);
+    try {
+      if (!fs.existsSync(documentsRoot)) {
+        fs.mkdirSync(documentsRoot, { recursive: true });
+      }
+      const existingFiles = fs
+        .readdirSync(documentsRoot)
+        .filter((f) => f.startsWith(categoryName));
+      if (existingFiles.length > 0) {
+        existingFiles.forEach((existingFile) => {
+          fs.unlinkSync(path.join(documentsRoot, existingFile));
+        });
+      }
+      fs.writeFileSync(filePath, file.buffer);
+      return filePath
+        .replace(/\\/g, '/')
+        .replace('D:/Oussama/PROJECTS/Economa/client/public', '');
+    } catch (error) {
+      throw new Error(`Error while storing file: ${error.message}`);
+    }
   }
 }
