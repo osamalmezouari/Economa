bbc13ca272cdf899f7d954d29adb65df30da8bd2
Osama Lmezouari <Oussamalm147@gmail.com>
2025-03-19 23:35:11 +0000

    feat: extend category types with description and create/update functionality

--- client/src/types/category.ts
+++ client/src/types/category.ts
@@ -2,6 +2,7 @@ export interface CategoryType {
   id: string;
   name: string;
   svgLink: string;
+  description?: string;
   productsCount?: number;
 }
 export interface CategoryCardProps {
@@ -11,13 +12,34 @@ export interface CategoryCardProps {
   productsCount: number;
 }
 
-export interface CategoriesnamesandIds {
-  id: string;
+export interface CreateCategory {
   name: string;
+  description: string;
+  file: any;
 }
+
+export interface CreateCategory {
+  name: string;
+  description: string;
+  file: File|any;
+}
+export interface UpdateCategory {
+  name: string;
+  description: string;
+  file: File|any;
+}
+
 export interface CategoryStateType {
   CategoryCards: {
     data: CategoryType[];
+    loading: boolean;
+    error: string;
+  };
+  Categories: {
+    data: {
+      categories: CategoryType[];
+      pageCount: number;
+    };
     loading: boolean;
     error: string;
   };
@@ -26,4 +48,17 @@ export interface CategoryStateType {
     loading: boolean;
     error: string;
   };
+  createCategory: {
+    data: CategoryType;
+    loading: boolean;
+    error: string;
+  };
+  updateCategory: {
+    data: CategoryType;
+    loading: boolean;
+    error: string;
+  };
+  categorytoEdit: String;
+  isUpdateCategoryOpen: boolean;
+  isCreateCategoryOpen: boolean;
 }
