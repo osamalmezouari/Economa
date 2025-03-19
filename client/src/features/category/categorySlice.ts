53e2ae3c8d72d83e47acefdf93d1edf58cebe6be
Osama Lmezouari <Oussamalm147@gmail.com>
2025-03-19 23:34:45 +0000

    feat: enhance category slice with create and update functionality

--- client/src/features/category/categorySlice.ts
+++ client/src/features/category/categorySlice.ts
@@ -4,7 +4,13 @@ import {
   CategoryStateType,
   CategoryType,
 } from '../../types/category';
-import { getCategoriesNamesandIds, getCategoryCards } from './categoryThunk';
+import {
+  AddCategory,
+  getCategoriesNamesandIds,
+  getCategoryCards,
+  getCategoryList,
+  updateCategory,
+} from './categoryThunk';
 
 const initialState: CategoryStateType = {
   CategoryCards: {
@@ -17,12 +23,61 @@ const initialState: CategoryStateType = {
     loading: false,
     error: '',
   },
+  Categories: {
+    data: {
+      categories: [],
+      pageCount: 0,
+    },
+    loading: false,
+    error: '',
+  },
+  createCategory: {
+    data: {
+      id: '',
+      name: '',
+      svgLink: '',
+      description: '',
+      productsCount: 0,
+    },
+    loading: false,
+    error: '',
+  },
+  updateCategory: {
+    data: {
+      id: '',
+      name: '',
+      svgLink: '',
+      description: '',
+      productsCount: 0,
+    },
+    loading: false,
+    error: '',
+  },
+  categorytoEdit: '',
+  isUpdateCategoryOpen: false,
+  isCreateCategoryOpen: false,
 };
 
 const categorySlice = createSlice({
   name: 'category',
   initialState,
-  reducers: {},
+  reducers: {
+    setCategoryToEdit: (state, action: PayloadAction<string>) => {
+      state.categorytoEdit = action.payload;
+    },
+    OpenCreateCategoryDailog: (state) => {
+      state.isCreateCategoryOpen = true;
+    },
+    CloseCreateCategoryDailog: (state) => {
+      state.isCreateCategoryOpen = false;
+    },
+    OpenUpdateCategoryDailog: (state) => {
+      state.isUpdateCategoryOpen = true;
+    },
+    CloseUpdateCategoryDailog: (state) => {
+      state.isUpdateCategoryOpen = false;
+    },
+  },
   extraReducers: (builder) => {
     builder
       .addCase(getCategoryCards.pending, (state) => {
@@ -57,9 +112,58 @@ const categorySlice = createSlice({
       .addCase(getCategoriesNamesandIds.rejected, (state, action) => {
         state.CategoriesnamesandIds.loading = false;
         state.CategoriesnamesandIds.error = action.payload as string;
+      })
+
+      .addCase(getCategoryList.pending, (state) => {
+        state.Categories.loading = true;
+        state.Categories.error = '';
+      })
+      .addCase(getCategoryList.fulfilled, (state, action) => {
+        state.Categories.loading = false;
+        state.Categories.error = '';
+        state.Categories.data = action.payload;
+      })
+      .addCase(getCategoryList.rejected, (state, action) => {
+        state.Categories.loading = false;
+        state.Categories.error = action.payload as string;
+      })
+
+      .addCase(AddCategory.pending, (state) => {
+        state.createCategory.loading = true;
+        state.createCategory.error = '';
+      })
+      .addCase(AddCategory.fulfilled, (state, action) => {
+        state.createCategory.loading = false;
+        state.createCategory.error = '';
+        state.createCategory.data = action.payload;
+      })
+      .addCase(AddCategory.rejected, (state, action) => {
+        state.createCategory.loading = false;
+        state.createCategory.error = action.payload as string;
+      })
+
+      .addCase(updateCategory.pending, (state) => {
+        state.updateCategory.loading = true;
+        state.updateCategory.error = '';
+      })
+      .addCase(updateCategory.fulfilled, (state, action) => {
+        state.updateCategory.loading = false;
+        state.updateCategory.error = '';
+        state.updateCategory.data = action.payload;
+      })
+      .addCase(updateCategory.rejected, (state, action) => {
+        state.updateCategory.loading = false;
+        state.updateCategory.error = action.payload as string;
       });
   },
 });
 
 export const categoryReducer = categorySlice.reducer;
 export default categorySlice.reducer;
+export const {
+  CloseCreateCategoryDailog,
+  OpenCreateCategoryDailog,
+  CloseUpdateCategoryDailog,
+  OpenUpdateCategoryDailog,
+  setCategoryToEdit,
+} = categorySlice.actions;
