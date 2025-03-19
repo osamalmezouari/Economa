bd96afbbb222a8798c133ae8822f1ebdb3312e4a
Osama Lmezouari <Oussamalm147@gmail.com>
2025-03-19 23:34:49 +0000

    feat: add async thunks for fetching, adding, and updating categories

--- client/src/features/category/categoryThunk.ts
+++ client/src/features/category/categoryThunk.ts
@@ -1,6 +1,23 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import * as categoryApi from '../../api/category';
 
+export const getCategoryList = createAsyncThunk(
+  'category/getCategoryList',
+  async (page: number, { rejectWithValue }) => {
+    try {
+      const categories = await categoryApi.CategoryList(page);
+      return categories;
+    } catch (error: any) {
+      if (error) {
+        return rejectWithValue(error);
+      }
+      return rejectWithValue({
+        message: error.message || 'Something went wrong.',
+      });
+    }
+  }
+);
+
 export const getCategoryCards = createAsyncThunk(
   'category/getCategoryCards',
   async (_, { rejectWithValue }) => {
@@ -24,6 +41,38 @@ export const getCategoriesNamesandIds = createAsyncThunk(
     try {
       const categories = await categoryApi.getCategoriesNamesandIds();
       return categories;
+    } catch (error: any) {
+      if (error) {
+        return rejectWithValue(error);
+      }
+      return rejectWithValue({
+        message: error.message || 'Something went wrong.',
+      });
+    }
+  }
+);
+
+export const AddCategory = createAsyncThunk(
+  'category/AddCategory',
+  (category: FormData, { rejectWithValue }) => {
+    try {
+      return categoryApi.AddCategory(category);
+    } catch (error: any) {
+      if (error) {
+        return rejectWithValue(error);
+      }
+      return rejectWithValue({
+        message: error.message || 'Something went wrong.',
+      });
+    }
+  }
+);
+
+export const updateCategory = createAsyncThunk(
+  'category/updateCategory',
+  (category: FormData, { rejectWithValue }) => {
+    try {
+      return categoryApi.updateCategory(category);
     } catch (error: any) {
       if (error) {
         return rejectWithValue(error);
