2dcb0a3cdf7dd755a3d238c74d7f2193619421fe
Osama Lmezouari <Oussamalm147@gmail.com>
2025-03-19 23:34:11 +0000

    feat: add category fetching and updating functions to API

--- client/src/api/category.ts
+++ client/src/api/category.ts
@@ -1,6 +1,20 @@
 import { apiClient } from '../utils/apiClient';
 import { CategoriesnamesandIds, CategoryType } from '../types/category';
 
+export const CategoryList = async (page: number): Promise<CategoryType[]> => {
+  try {
+    const response = await apiClient.get<CategoryType[]>(
+      `/category?page=${page}`
+    );
+    return response.data;
+  } catch (error: any) {
+    if (error.response) {
+      throw error.response.data.message;
+    }
+    throw new Error('Failed to Fetch Category');
+  }
+};
+
 export const CategoryCards = async (): Promise<CategoryType[]> => {
   try {
     const response = await apiClient.get<CategoryType[]>('/category/cards');
@@ -26,5 +40,33 @@ export const getCategoriesNamesandIds = async (): Promise<
       throw error.response.data.message;
     }
     throw new Error('Failed to Fetch Category');
+  }
+};
+
+export const AddCategory = async (
+  category: FormData
+): Promise<CategoryType> => {
+  try {
+    const response = await apiClient.post('/category', category);
+    return response.data;
+  } catch (error: any) {
+    if (error.response) {
+      throw error.response.data.message;
+    }
+    throw new Error('Failed to Add Category');
+  }
+};
+
+export const updateCategory = async (
+  category: FormData
+): Promise<CategoryType> => {
+  try {
+    const response = await apiClient.patch('/category', category);
+    return response.data;
+  } catch (error: any) {
+    if (error.response) {
+      throw error.response.data.message;
+    }
+    throw new Error('Failed to Add Category');
   }
 };
