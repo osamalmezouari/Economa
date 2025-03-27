a31951b86509a4c5d4817cdd2eedcf59a3ae9a15
Osama Lmezouari <Oussamalm147@gmail.com>
2025-03-27 23:29:55 +0000

    feat: implement ManageRefills component for handling refill requests and CSV export

---
+++ client/src/pages/Admin/manageRefills.tsx
@@ -0,0 +1,38 @@
+import { Box, Button } from '@mui/material';
+import PageHeader from '../../components/admin/extra/pageheader/PageHeader';
+import { Download } from '@mui/icons-material';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../app/store';
+import exportToCSV from '../../utils/exportcsv';
+import ManageRefillsTable from '../../components/admin/extra/manageRefillsTable';
+
+const ManageRefills = () => {
+  const Refills = useSelector(
+    (state: RootState) => state.balance.refillsList.data.refills
+  );
+  return (
+    <Box className={'p-4 mt-16'}>
+      <PageHeader
+        title="Refills Requests"
+        breadcrumb={[
+          { name: 'Dashboard', href: '/Economa/admin/dashboard' },
+          { name: 'products' },
+          {
+            name: 'Manage Refills',
+          },
+        ]}
+      >
+        <Button
+          variant="outlined"
+          className="px-4 py-2 mr-2"
+          startIcon={<Download />}
+          onClick={() => exportToCSV(Refills, `TransactionsCSV`)}
+        >
+          Export
+        </Button>
+      </PageHeader>
+      <ManageRefillsTable />
+    </Box>
+  );
+};
+export default ManageRefills;
