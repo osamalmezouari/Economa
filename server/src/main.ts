d333b0e9c64fc4016b74438b1c4c5f9b244e3334
Osama Lmezouari <Oussamalm147@gmail.com>
2025-03-29 23:37:35 +0000

    feat: add static file serving for receipts and enable CORS in main application

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConsoleLogger, ValidationPipe } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
 import { GLOBAL_EXCEPTION_Filter } from './common/filters/GLOBAL_EXCEPTION.filter';
 import { LoggerInterceptor } from './common/interceptors/logger.interceptor';
-
+import * as express from 'express'; // Import express
+import * as path from 'path'; // Import path for path manipulation
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
@@ -11,6 +12,18 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  app.use(
+    '/receipts',
+    express.static(path.join(__dirname, '..', 'public', 'receipts')),
+  );
+  console.log(
+    'Serving static files from:',
+    path.join(__dirname, '..', 'public', 'receipts'),
+  );
+
+  app.enableCors({
+    origin: '*', // Allow all domains for testing purposes, you can restrict this later
+  });
   app.useGlobalFilters(new GLOBAL_EXCEPTION_Filter());
   app.useGlobalInterceptors(new LoggerInterceptor());
   app.enableCors();
