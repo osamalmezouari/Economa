bb5c3a58d67c5c5c42d78864c1cf72f26471629d
Osama Lmezouari <Oussamalm147@gmail.com>
2025-04-18 20:39:08 +0100

    feat: refactor getOrdersHistory thunk to use ordersApi for fetching order history

--- client/src/features/order/orderThunk.ts
+++ client/src/features/order/orderThunk.ts
@@ -1,6 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { place_and_pay_order as place_and_pay_order_type } from '../../types/order';
 import { place_and_pay_order } from '../../api/orders';
+import * as ordersApi from '../../api/orders';
 
 export const placeandpay = createAsyncThunk(
   'order/placeandpay',
@@ -8,6 +9,23 @@ export const placeandpay = createAsyncThunk(
     try {
       const Order = await place_and_pay_order(data);
       return Order;
+    } catch (error: any) {
+      if (error) {
+        return rejectWithValue(error);
+      }
+      return rejectWithValue({
+        message: error.message || 'Something went wrong.',
+      });
+    }
+  }
+);
+
+export const getOrdersHistory = createAsyncThunk(
+  'order/getOrdersHistory',
+  async (data: { email: string; page: number }, { rejectWithValue }) => {
+    try {
+      const Orders = await ordersApi.getOrdersHistory(data.page, data.email);
+      return Orders;
     } catch (error: any) {
       if (error) {
         return rejectWithValue(error);
