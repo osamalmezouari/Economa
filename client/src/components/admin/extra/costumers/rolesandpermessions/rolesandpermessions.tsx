d7f1ee4c6cc810e813ac0e07afd79b475247178c
Osama Lmezouari <Oussamalm147@gmail.com>
2025-04-18 20:38:34 +0100

    fix: add optional chaining to roles mapping in RolesAndPermissions component

--- client/src/components/admin/extra/costumers/rolesandpermessions/rolesandpermessions.tsx
+++ client/src/components/admin/extra/costumers/rolesandpermessions/rolesandpermessions.tsx
@@ -68,7 +68,7 @@ const RolesAndPermissions = () => {
         </Box>
       ) : (
         <Grid container spacing={2}>
-          {roles.map((role) => (
+          {roles?.map((role) => (
             <Grid item xs={12} sm={6} md={4} key={role.id}>
               <RoleCard
                 roleName={role.name}
