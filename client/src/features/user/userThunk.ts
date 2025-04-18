0c49784300f96e82b1e50246ba95be24bbd9eb08
Osama Lmezouari <Oussamalm147@gmail.com>
2025-04-18 20:39:22 +0100

    feat: add user management thunks for creating, updating, and fetching users

--- client/src/features/user/userThunk.ts
+++ client/src/features/user/userThunk.ts
@@ -17,3 +17,71 @@ export const getShoortedUserInfo = createAsyncThunk(
     }
   }
 );
+
+export const getUsersList = createAsyncThunk(
+  '/user/list',
+  async (data: { search: string; page: number }, { rejectWithValue }) => {
+    try {
+      const users = await userApi.getUsersList(data.page, data.search);
+      return users;
+    } catch (error: any) {
+      if (error) {
+        rejectWithValue(error);
+      }
+      return rejectWithValue({
+        message: 'failed to fetch user list',
+      });
+    }
+  }
+);
+
+export const adminCreate = createAsyncThunk(
+  '/user/adminCreate',
+  async (userData: FormData, { rejectWithValue }) => {
+    try {
+      const user = await userApi.adminCreate(userData);
+      return user;
+    } catch (error: any) {
+      if (error) {
+        return rejectWithValue(error);
+      }
+      return rejectWithValue({
+        message: 'Failed to create user',
+      });
+    }
+  }
+);
+
+export const adminUpdate = createAsyncThunk(
+  '/user/adminUpdate',
+  async (data: { id: string; userData: FormData }, { rejectWithValue }) => {
+    try {
+      const user = await userApi.adminUpdate(data.id, data.userData);
+      return user;
+    } catch (error: any) {
+      if (error) {
+        return rejectWithValue(error);
+      }
+      return rejectWithValue({
+        message: 'Failed to update user',
+      });
+    }
+  }
+);
+
+export const getUserById = createAsyncThunk(
+  '/user/byId',
+  async (id: string, { rejectWithValue }) => {
+    try {
+      const user = await userApi.getUserById(id);
+      return user;
+    } catch (error: any) {
+      if (error) {
+        return rejectWithValue(error);
+      }
+      return rejectWithValue({
+        message: 'Failed to fetch user',
+      });
+    }
+  }
+);
