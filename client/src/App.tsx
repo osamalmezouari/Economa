38a7f83aea051efc3f1873623e1eb45bdfb58141
Osama Lmezouari <Oussamalm147@gmail.com>
2025-04-22 22:00:42 +0100

    fix: add a blank line for improved readability in App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { RouterProvider } from '@tanstack/react-router';
 import { router } from './routes/__root';
 
 export default function App() {
+
   return (
     <ThemeProvider>
       <RouterProvider router={router} />
