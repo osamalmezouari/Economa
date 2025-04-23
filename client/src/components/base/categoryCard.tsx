3c4be8bc502121c40e5f96447644bdb8430f1940
Osama Lmezouari <Oussamalm147@gmail.com>
2025-04-23 23:42:39 +0100

    refactor: simplify CategoryCardSingle component structure and remove unused color logic

--- client/src/components/base/categoryCard.tsx
+++ client/src/components/base/categoryCard.tsx
@@ -1,46 +1,32 @@
 import { Box, Typography } from '@mui/material';
 import Card from '@mui/material/Card';
-import { CategoryCardProps } from '../../../types/category';
+import { CategoryCardProps } from '../../types/category';
 
 const CategoryCardSingle = ({
   svgLink,
   productsCount,
   name,
 }: CategoryCardProps) => {
-  const calors: string[] = [
-    '#e2fde2',
-    '#fee9e8',
-    '#fdf4ea',
-    '#F9DEF2',
-    'fdf5eb',
-  ];
-  const randomColor = calors[Math.floor(Math.random() * calors.length)];
   return (
-    <Box
-      component={'div'}
-      className={`p-3 rounded`}
-      style={{ backgroundColor: `${randomColor}` }}
-    >
-      <Card className="w-[160px]">
+    <Box component={'div'} className={``}>
+      <Box className="w-[160px] border-[1px]">
         <Box
           component={'div'}
           className="p-4 text-center grid justify-center gap-y-1"
         >
           <Box
-            component={'div'}
-            className="w-12 m-auto h-12 bg-cover bg-center"
-            style={{
-              backgroundImage: `url(${svgLink})`,
-            }} /* onClick={()=>Navigate(linkTo)} */
+            component={'img'}
+            className="w-28 m-auto h-28 bg-cover bg-center hover:opacity-75 cursor-pointer"
+            src={svgLink}
           ></Box>
-          <Typography className="!font-main font-bold text-secondary-darker">
+          <Typography className="text-[12px] font-main font-bold text-secondary-darker">
             {name}
           </Typography>
           <p className="text-[12px] text-secondary-main">
             {productsCount} Products
           </p>
         </Box>
-      </Card>
+      </Box>
     </Box>
   );
 };
