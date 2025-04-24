bb36c67b62a7acb9d2711887a3a0c2e44f76dd45
Osama Lmezouari <Oussamalm147@gmail.com>
2025-04-24 23:03:46 +0100

    feat: enhance CategoryCardContainer with slider functionality and HeroTitle component

--- client/src/components/extra/categoryCardContainer.tsx
+++ client/src/components/extra/categoryCardContainer.tsx
@@ -1,10 +1,13 @@
-import { Box, CircularProgress, Container } from '@mui/material';
-import Grid2 from '@mui/material/Grid2';
+import { Box, CircularProgress, Container, Typography } from '@mui/material';
 import CategoryCard from '../base/categoryCard';
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { AppDispatch, RootState } from '../../app/store';
 import { getCategoryCards } from '../../features/category/categoryThunk';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import HeroTitle from './Titles/HeroTitle';
 
 const CategoryCardContainer = () => {
   const dispatch = useDispatch<AppDispatch>();
@@ -16,30 +19,72 @@ const CategoryCardContainer = () => {
     dispatch(getCategoryCards());
   }, [dispatch]);
 
+  const sliderSettings = {
+    dots: false,
+    arrows: false,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 8,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+  // Limit to maximum 8 items
+
   return (
     <Container style={{ maxWidth: '1350px', marginTop: '40px' }}>
       {loading ? (
         <Box className={'w-full flex justify-center items-center py-8'}>
-          <CircularProgress className='' />
+          <CircularProgress className="" />
         </Box>
       ) : (
-        <Grid2
-          container
-          spacing={4}
-          sx={{ alignItems: 'center' }}
-          component={'div'}
-          className="justify-evenly"
-        >
-          {data.map((category) => (
-            <CategoryCard
-              key={category.id}
-              linkTo={`/category/${category.id}`}
-              svgLink={category.svgLink || ''}
-              name={category.name}
-              productsCount={category.productsCount || 0}
-            />
-          ))}
-        </Grid2>
+        <>
+          <HeroTitle
+            title="Our Categories "
+            subtitle="New products with updated stocks."
+          />
+          <Slider {...sliderSettings}>
+            {data.map((category) => (
+              <div key={category.id} style={{ padding: '0 10px' }}>
+                <CategoryCard
+                  linkTo={`/category/${category.id}`}
+                  svgLink={category.svgLink || ''}
+                  name={category.name}
+                  productsCount={category.productsCount || 0}
+                />
+              </div>
+            ))}
+          </Slider>
+        </>
       )}
     </Container>
   );
