a606e6cd0238a22a47cf09ad21d745d672ff4b07
Osama Lmezouari <Oussamalm147@gmail.com>
2025-04-24 23:02:29 +0100

    feat: wrap RouterProvider with AuthProvider for authentication context

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
 import ThemeProvider from './theme';
 import { RouterProvider } from '@tanstack/react-router';
 import { router } from './routes/__root';
+import { AuthProvider } from './context/AuthContext';
 
 export default function App() {
-
   return (
     <ThemeProvider>
-      <RouterProvider router={router} />
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
     </ThemeProvider>
   );
 }
