aac3d8288fb09edf29ed399ee0cfa7ecb7c0b7bc
Osama Lmezouari <Oussamalm147@gmail.com>
2025-04-25 23:55:26 +0100

    fix: wrap NewArrivalsSection content in Box component for consistent layout

--- client/src/components/extra/newArrivalsSection.tsx
+++ client/src/components/extra/newArrivalsSection.tsx
@@ -1,15 +1,16 @@
+import { Box } from '@mui/material';
 import ProductnewArrivalsContainer from './productnewArrivalsContainer';
 import HeroTitle from './Titles/HeroTitle';
 
 const NewArrivalsSection = () => {
   return (
-    <>
+    <Box>
       <HeroTitle
         title={'New Arrivals'}
         subtitle={'Shop online for new arrivals and get free shipping!'}
       />
       <ProductnewArrivalsContainer />
-    </>
+    </Box>
   );
 };
 
