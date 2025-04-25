0221d21b8f3ad5851b23cbbed3e65723b9d5798e
Osama Lmezouari <Oussamalm147@gmail.com>
2025-04-25 23:55:08 +0100

    feat: update ServiceCard component to include title prop and use Typography for improved styling

--- client/src/components/base/service-card/ServiceCard.tsx
+++ client/src/components/base/service-card/ServiceCard.tsx
@@ -1,13 +1,21 @@
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import { ServiceCardProps } from './interface';
 
-const ServiceCard = ({ icon, subtitle }: ServiceCardProps) => {
+const ServiceCard = ({ icon, subtitle, title }: ServiceCardProps) => {
   return (
-    <Box className="w-full h-[140px] p-6 flex place-items-center border-gray-200 border-2 rounded text-center">
-      <>{icon}</>
-      <p className="text-start px-4 py-1 text-secondary-main font-normal text-xl font-primay">
-        {subtitle}
-      </p>
+    <Box className="w-full h-max flex gap-2 rounded items-center">
+      <Box>{icon}</Box>
+      <Box>
+        <Typography
+          variant="body1"
+          className="text-start py-1 text-secondary-darker font-bold font-main"
+        >
+          {title}
+        </Typography>
+        <Typography variant="body2" className="w-full text-start">
+          {subtitle}{' '}
+        </Typography>
+      </Box>
     </Box>
   );
 };
