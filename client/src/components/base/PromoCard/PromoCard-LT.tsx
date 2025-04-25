ffc2f63d4d6ae7567f396a77c5f2a87c2c535d90
Osama Lmezouari <Oussamalm147@gmail.com>
2025-04-25 23:54:42 +0100

    feat: add PromoCard component for promotional display

---
+++ client/src/components/base/PromoCard/PromoCard-LT.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Box, Button, Typography } from '@mui/material';
+import { PromoCardProps } from './interfaces';
+import { FaArrowRight } from 'react-icons/fa';
+
+const PromoCardlefttitle: React.FC<PromoCardProps> = ({
+  image,
+  title,
+  subtitle,
+  discountText,
+  buttonText,
+}) => {
+  return (
+    <div>
+      <Box
+        component="div"
+        style={{ backgroundImage: `url(${image})`  , backgroundColor :''}}
+
+        className="flex flex-col justify-center bg-cover bg-center  w-full h-full relative px-4 py-6  rounded"
+      >
+        <Box
+          component={'div'}
+          className=" text-start flex flex-col gap-2 justify-center"
+        >
+          <Typography
+            variant="body2"
+            color="primary"
+            fontWeight=""
+            className="font-Inria capitalize mt-2"
+          >
+            {discountText}
+          </Typography>
+          <Typography
+            variant="h5"
+            fontWeight="bold"
+            color="black"
+            className="w-1/2"
+          >
+            {title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {subtitle}
+          </Typography>
+          {buttonText !== 'none' && (
+            <Button
+              className="text-black bg-white h-8 text-[12px] p-4 w-max rounded-full"
+              endIcon={<FaArrowRight className="w-4 h-2" />}
+            >
+              {buttonText}
+            </Button>
+          )}
+        </Box>
+      </Box>
+    </div>
+  );
+};
+
+export default PromoCardlefttitle;
