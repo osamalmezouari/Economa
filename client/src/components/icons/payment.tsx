edd20d550b83229e0d02b6fc5ec30c37ad4cbd3e
Osama Lmezouari <Oussamalm147@gmail.com>
2025-04-27 20:53:06 +0100

    feat: add Payment icon component

---
+++ client/src/components/icons/payment.tsx
@@ -0,0 +1,104 @@
+const PaymentDocument = ({ className, ...props }: { className: string }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={56}
+    height={57}
+    viewBox="0 0 56 57"
+    fill="none"
+    className={className}
+    {...props}
+  >
+    <g clipPath="url(#clip0_157_32645)">
+      <mask
+        id="mask0_157_32645"
+        style={{
+          maskType: 'luminance',
+        }}
+        maskUnits="userSpaceOnUse"
+        x={0}
+        y={0}
+        width={56}
+        height={57}
+      >
+        <path d="M56 0.200012H0V56.2H56V0.200012Z" fill="white" />
+      </mask>
+      <g mask="url(#mask0_157_32645)">
+        <path
+          d="M16.625 42.725L11.375 46.05L16.8 54.45L22.05 51.125L16.625 42.725Z"
+          stroke="#1D1D1B"
+          strokeWidth={1.5}
+          strokeMiterlimit={10}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M1.75 51.125L7 54.45L12.425 46.05L7.175 42.725L1.75 51.125Z"
+          stroke="#1D1D1B"
+          strokeWidth={1.5}
+          strokeMiterlimit={10}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M50.3996 39.575C46.7246 47.1 33.4246 47.975 33.4246 47.975C33.4246 47.975 28.1746 37.825 32.7246 30.825C37.9746 22.6 54.7746 21.725 54.7746 21.725C54.7746 21.725 54.0746 32.225 50.3996 39.575Z"
+          className="fill-primary-main"
+        />
+        <path
+          d="M47.6 27.85C47.6 27.85 32.55 43.775 32.375 53.225"
+          stroke="#1D1D1B"
+          strokeWidth={1.5}
+          strokeMiterlimit={10}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M44.7998 1.95001H11.5498V41.85H44.7998V1.95001Z"
+          stroke="#1D1D1B"
+          strokeWidth={1.5}
+          strokeMiterlimit={10}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M12.4246 50.5999C17.6746 50.5999 21.8746 46.3999 21.8746 41.15C21.8746 35.9 17.6746 31.7 12.4246 31.7C7.17461 31.7 2.97461 35.9 2.97461 41.15C2.97461 46.3999 7.17461 50.5999 12.4246 50.5999Z"
+          fill="#FFEB3B"
+        />
+        <path
+          d="M12.425 46.575C15.4 46.575 17.85 44.125 17.85 41.15C17.85 38.175 15.4 35.725 12.425 35.725C9.45 35.725 7 38.175 7 41.15C7 44.125 9.45 46.575 12.425 46.575Z"
+          stroke="#1D1D1B"
+          strokeWidth={1.5}
+          strokeMiterlimit={10}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M18.375 12.275H38.15"
+          stroke="#1D1D1B"
+          strokeWidth={1.5}
+          strokeMiterlimit={10}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M18.375 20.675H38.15"
+          stroke="#1D1D1B"
+          strokeWidth={1.5}
+          strokeMiterlimit={10}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </g>
+    </g>
+    <defs>
+      <clipPath id="clip0_157_32645">
+        <rect
+          width={56}
+          height={56}
+          fill="white"
+          transform="translate(0 0.200012)"
+        />
+      </clipPath>
+    </defs>
+  </svg>
+);
+export default PaymentDocument;
