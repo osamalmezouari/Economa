e436841fa7665d3e62f716dcedd8be968a329041
Osama Lmezouari <Oussamalm147@gmail.com>
2025-04-27 21:00:07 +0100

    feat: update Landing component structure by reordering and adding HealthSafetyBanner

--- client/src/pages/landing.tsx
+++ client/src/pages/landing.tsx
@@ -1,19 +1,22 @@
-
+import HealthSafetyBanner from '../components/base/promotional banners/healthSafety';
 import Home from '../components/extra/Home';
 import NewArrivalsSection from '../components/extra/newArrivalsSection';
 import OfferBanner from '../components/extra/offerBanner';
+import OfferBanner2 from '../components/extra/offerBanner-2';
 import ProductsWithDiscount from '../components/extra/ProductsWithDiscount';
-import PromoCardWraper from '../components/extra/PromoCard';
 import ServicesContainer from '../components/extra/ServicesContainer';
+import TestimonialsContainer from '../components/extra/TestimonialsContainer';
 
 const Landing = () => {
   return (
     <>
       <Home />
+      <HealthSafetyBanner />
       <ProductsWithDiscount />
-      <PromoCardWraper />
-      <NewArrivalsSection />
       <OfferBanner />
+      <NewArrivalsSection />
+      <OfferBanner2 />
+      <TestimonialsContainer />
       <ServicesContainer />
     </>
   );
