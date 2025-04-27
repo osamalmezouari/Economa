5f699ed703352e102576a23ca08a131c96a82707
Osama Lmezouari <Oussamalm147@gmail.com>
2025-04-27 20:53:15 +0100

    feat: add Quality icon component

---
+++ client/src/components/icons/quality.tsx
@@ -0,0 +1,121 @@
+const Quality = ({ className, ...props }: { className?: string }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={56}
+    height={57}
+    viewBox="0 0 56 57"
+    fill="none"
+    className={className}
+    {...props}
+  >
+    <path
+      d="M34.3004 38.875C41.3004 38.875 47.0754 33.1 47.0754 26.1C47.0754 19.1 41.3004 13.325 34.3004 13.325C27.3004 13.325 21.5254 19.1 21.5254 26.1C21.5254 33.1 27.1254 38.875 34.3004 38.875Z"
+      fill="#FFEB3B"
+    />
+    <path
+      d="M25.7247 21.2C26.5997 20.15 26.9497 18.925 26.9497 17.525C26.9497 14.725 25.0247 12.275 22.3997 11.75C22.5747 11.05 22.7497 10.35 22.7497 9.65001C22.7497 5.45001 19.2497 1.95001 14.8747 1.95001C10.6747 1.95001 7.17471 5.45001 7.17471 9.65001C7.17471 10.35 7.34971 10.875 7.34971 11.575C4.89971 12.275 2.97471 14.725 2.97471 17.525C2.97471 18.925 3.49971 20.15 4.19971 21.2C2.79971 22.6 1.39971 24.525 1.74971 26.8C2.09971 28.9 11.0247 50.25 11.0247 50.25H18.7247C18.7247 50.25 27.6497 28.9 27.9997 26.8C28.5247 24.525 27.1247 22.6 25.7247 21.2Z"
+      className="fill-primary-main"
+    />
+    <path
+      d="M14.875 10.7V49.725"
+      stroke="#1D1D1B"
+      strokeWidth={1.5}
+      strokeMiterlimit={10}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M8.22461 27.675L14.8746 35.2"
+      stroke="#1D1D1B"
+      strokeWidth={1.5}
+      strokeMiterlimit={10}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M21.7 27.675L14.875 35.2"
+      stroke="#1D1D1B"
+      strokeWidth={1.5}
+      strokeMiterlimit={10}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M8.22461 16.65L15.0496 24.35"
+      stroke="#1D1D1B"
+      strokeWidth={1.5}
+      strokeMiterlimit={10}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M21.7 16.65L14.875 24.35"
+      stroke="#1D1D1B"
+      strokeWidth={1.5}
+      strokeMiterlimit={10}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M42.3496 38.175C42.6996 39.4 42.8746 40.625 43.0496 41.675H54.2496L49.6996 27.85L40.5996 34.675C41.2996 35.725 41.8246 36.95 42.3496 38.175Z"
+      fill="#FFEB3B"
+    />
+    <path
+      d="M2.45039 51.3C1.75039 49.375 2.45039 47.275 3.85039 46.4L29.9254 31C34.4754 28.725 40.2504 32.05 42.3504 38.175C44.2754 44.3 41.6504 50.425 36.5754 51.3L6.4754 54.45C4.9004 54.625 3.15039 53.225 2.45039 51.3Z"
+      stroke="#1D1D1B"
+      strokeWidth={1.5}
+      strokeMiterlimit={10}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M29.5752 31.35C29.7502 31.7 33.4252 43.775 33.4252 43.775"
+      stroke="#1D1D1B"
+      strokeWidth={1.5}
+      strokeMiterlimit={10}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M25.375 45.175C25.55 45.525 27.65 52.175 27.65 52.175"
+      stroke="#1D1D1B"
+      strokeWidth={1.5}
+      strokeMiterlimit={10}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M17.8496 38.525C18.0246 38.875 20.1246 45.525 20.1246 45.525"
+      stroke="#1D1D1B"
+      strokeWidth={1.5}
+      strokeMiterlimit={10}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M7.875 48.85L9.45 53.925"
+      stroke="#1D1D1B"
+      strokeWidth={1.5}
+      strokeMiterlimit={10}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M38.8502 8.25L36.5752 16.825"
+      stroke="#1D1D1B"
+      strokeWidth={1.5}
+      strokeMiterlimit={10}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M39.8998 16.65C39.3748 17 38.6748 17 37.9748 17C36.5748 17 35.1748 16.3 34.2998 15.25"
+      stroke="#1D1D1B"
+      strokeWidth={1.5}
+      strokeMiterlimit={10}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+export default Quality;
