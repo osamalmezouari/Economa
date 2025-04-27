b262db65035b0491f4f285ccdd23074b244dce3c
Osama Lmezouari <Oussamalm147@gmail.com>
2025-04-27 20:52:30 +0100

    feat: add Dilevry icon component

---
+++ client/src/components/icons/dilevery.tsx
@@ -0,0 +1,97 @@
+const Dilevry = ({ className, ...props }: { className?: string }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={57}
+    height={57}
+    viewBox="0 0 57 57"
+    fill="none"
+    className={className}
+    {...props}
+  >
+    <g clipPath="url(#clip0_157_32746)">
+      <mask
+        id="mask0_157_32746"
+        style={{
+          maskType: 'luminance',
+        }}
+        maskUnits="userSpaceOnUse"
+        x={0}
+        y={0}
+        width={57}
+        height={57}
+      >
+        <path d="M56.5 0.200012H0.5V56.2H56.5V0.200012Z" fill="white" />
+      </mask>
+      <g mask="url(#mask0_157_32746)">
+        <path
+          d="M38.9996 23.125H6.97461V48.85H38.9996V23.125Z"
+          className="fill-primary-main"
+        />
+        <path
+          d="M39.1748 30.475H46.1748L54.0498 45.525C54.9248 47.1 53.6998 48.85 52.1248 48.85H38.2998"
+          stroke="#1D1D1B"
+          strokeWidth={1.5}
+          strokeMiterlimit={10}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M14.8494 54.275C17.1244 54.275 19.0494 52.35 19.0494 49.9C19.0494 47.45 17.1244 45.525 14.8494 45.525C12.5744 45.525 10.6494 47.45 10.6494 49.9C10.6494 52.35 12.5744 54.275 14.8494 54.275Z"
+          stroke="#1D1D1B"
+          strokeWidth={1.5}
+          strokeMiterlimit={10}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          fill="#FFEB3B"
+        />
+        <path
+          d="M45.6492 54.275C47.9242 54.275 49.8492 52.35 49.8492 49.9C49.8492 47.45 47.9242 45.525 45.6492 45.525C43.3742 45.525 41.4492 47.45 41.4492 49.9C41.4492 52.35 43.3742 54.275 45.6492 54.275Z"
+          stroke="#1D1D1B"
+          strokeWidth={1.5}
+          strokeMiterlimit={10}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M5.22461 43.775H18.3496"
+          stroke="#1D1D1B"
+          strokeWidth={1.5}
+          strokeMiterlimit={10}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M2.59961 39.75H15.7246"
+          stroke="#1D1D1B"
+          strokeWidth={1.5}
+          strokeMiterlimit={10}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M41.2742 27.85C48.4492 27.85 54.2242 22.075 54.2242 14.9C54.2242 7.72501 48.4492 1.95001 41.2742 1.95001C34.0992 1.95001 28.3242 7.72501 28.3242 14.9C28.3242 21.9 34.0992 27.85 41.2742 27.85Z"
+          fill="#FFEB3B"
+        />
+        <path
+          d="M35.6748 12.625L40.7498 22.425L47.9248 8.95001"
+          stroke="#1D1D1B"
+          strokeWidth={1.5}
+          strokeMiterlimit={10}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </g>
+    </g>
+    <defs>
+      <clipPath id="clip0_157_32746">
+        <rect
+          width={56}
+          height={56}
+          fill="white"
+          transform="translate(0.5 0.200012)"
+        />
+      </clipPath>
+    </defs>
+  </svg>
+);
+export default Dilevry;
