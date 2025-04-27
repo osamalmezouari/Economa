aee1fc08387801a8cb169cdb7dc6d3202c675f12
Osama Lmezouari <Oussamalm147@gmail.com>
2025-04-27 21:00:30 +0100

    refactor: update Store component layout by wrapping content in Box and adjusting Grid properties

--- client/src/pages/store.tsx
+++ client/src/pages/store.tsx
@@ -1,4 +1,4 @@
-import { Grid } from '@mui/material';
+import { Box, Grid } from '@mui/material';
 import StoreTopSection from '../components/extra/StoreTopSection.tsx';
 import LeftStoreFilter from '../components/extra/leftStoreFilter.tsx';
 import TopStoreFilter from '../components/extra/topStoreFilter.tsx';
@@ -6,16 +6,16 @@ import StoreProducts from '../components/extra/storeProducts.tsx';
 
 const Store = () => {
   return (
-    <>
+    <Box maxWidth={1350} className={'m-auto'}>
       <StoreTopSection />
-      <Grid container gap={2} maxWidth={1200} sx={{ margin: 'auto' }}>
+      <Grid container gap={1} sx={{ margin: 'auto' }}>
         <LeftStoreFilter />
         <Grid item lg={8.5} sx={{ marginTop: '20px', paddingTop: '0px' }}>
           <TopStoreFilter />
           <StoreProducts />
         </Grid>
       </Grid>
-    </>
+    </Box>
   );
 };
 export default Store;
