5f15845eed956c87156a3866716ec8793f1df282
Osama Lmezouari <Oussamalm147@gmail.com>
2025-05-03 08:33:45 +0100

    refactor: enhance AuthenticationGuard to use async token validation and improve error handling

--- server/src/common/guards/authentication/authentication.guard.ts
+++ server/src/common/guards/authentication/authentication.guard.ts
@@ -1,34 +1,47 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { Observable } from 'rxjs';
 import { AccesTokenGuard } from './acces-token.guard';
 import { Reflector } from '@nestjs/core';
 import { AuthenticationType } from '../../enums/authentication';
 import { AUTHENTICATION_KEY } from '../../decorators/meta/authentication.decorator';
 import { INVALID_TOKEN_Exception } from '../../exceptions/INVALID_TOKEN.exception';
+import { AuthorizationGuard } from '../authorization/authorization.guard';
+import { PERMESSIONS_KEY } from 'src/common/decorators/meta/authorization.decorator';
+import { Permissions_TYPE } from 'src/common/enums/permissions';
 
 @Injectable()
 export class AuthenticationGuard implements CanActivate {
   constructor(
     private readonly accessTokenGuard: AccesTokenGuard,
-    private readonly reflactor: Reflector,
+    private readonly authorizationGuard: AuthorizationGuard,
+    private readonly reflector: Reflector,
   ) {}
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    const meta = this.reflactor.get<AuthenticationType[]>(
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const meta = this.reflector.get<AuthenticationType[]>(
       AUTHENTICATION_KEY,
       context.getHandler(),
     ) || [AuthenticationType.bearer];
-    console.log(meta);
+
     if (meta.includes(AuthenticationType.None)) {
       return true;
     }
+
     if (meta.includes(AuthenticationType.bearer)) {
-      const isTokenValide = this.accessTokenGuard.canActivate(context);
-      if (!isTokenValide) {
-        throw INVALID_TOKEN_Exception;
-      } else {
-        return true;
+      try {
+        const isTokenValid = await this.accessTokenGuard.canActivate(context);
+        if (!isTokenValid) {
+          throw INVALID_TOKEN_Exception;
+        }
+        const requiredPermissions = this.reflector.get<Permissions_TYPE[]>(
+          PERMESSIONS_KEY,
+          context.getHandler(),
+        );
+        if (!requiredPermissions) {
+          return true;
+        }
+        return await this.authorizationGuard.canActivate(context);
+      } catch (error) {
+        throw error;
       }
     }
   }
