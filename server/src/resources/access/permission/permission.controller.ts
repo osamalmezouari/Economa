340ab700db2880afadcda5c569bf9675cc03d7ee
Osama Lmezouari <Oussamalm147@gmail.com>
2025-05-03 08:34:58 +0100

    feat: simplify PermissionController by removing unused methods and imports

--- server/src/resources/access/permission/permission.controller.ts
+++ server/src/resources/access/permission/permission.controller.ts
@@ -1,53 +1,19 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { PermissionService } from './permission.service';
-import { CreatePermissionDto } from './dto/create-permission.dto';
-import { UpdatePermissionDto } from './dto/update-permission.dto';
+import { SET_PERMESSIONS } from 'src/common/decorators/meta/authorization.decorator';
+import { Permissions_TYPE } from 'src/common/enums/permissions';
+import { AuthenticationType } from 'src/common/enums/authentication';
+import { AUTH } from 'src/common/decorators/meta/authentication.decorator';
 
 @Controller('permissions')
 export class PermissionController {
   constructor(private readonly permissionService: PermissionService) {}
 
-  @Post()
-  async create(@Body() createPermissionDto: CreatePermissionDto) {
-    const permission = await this.permissionService.create(createPermissionDto);
-    return permission;
-  }
-
+  @AUTH(AuthenticationType.bearer)
+  @SET_PERMESSIONS(Permissions_TYPE.PERMISSIONS_READ)
   @Get()
   async findAll() {
     const permissions = await this.permissionService.findAll();
     return permissions;
-  }
-
-  @Get(':id')
-  async findOne(@Param('id') id: string) {
-    const permission = await this.permissionService.findOne(id);
-    return permission;
-  }
-
-  @Patch(':id')
-  async update(
-    @Param('id') id: string,
-    @Body() updatePermissionDto: UpdatePermissionDto,
-  ) {
-    const permission = await this.permissionService.update(
-      id,
-      updatePermissionDto,
-    );
-    return permission;
-  }
-
-  @Delete(':id')
-  async remove(@Param('id') id: string) {
-    const permission = await this.permissionService.remove(id);
-    return permission;
   }
 }
