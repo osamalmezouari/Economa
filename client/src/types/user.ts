6629408747f77af698667bc9ad0596feed6c94b3
Osama Lmezouari <Oussamalm147@gmail.com>
2025-05-03 08:32:46 +0100

    feat: refactor user interfaces and state types for improved structure and clarity

--- client/src/types/user.ts
+++ client/src/types/user.ts
@@ -1,4 +1,3 @@
-
 export interface User {
   id: string;
   name: string;
@@ -9,18 +8,103 @@ export interface User {
   createdAt: Date;
   updatedAt: Date;
   lastLogin?: Date;
+  avatar: string;
   roleId: String;
 }
 
-export interface ShoortedUserInfo {
+export interface UserWithRoleLvl {
+  id: string;
   name: string;
   email: string;
+  password: string;
+  phoneNumber?: string;
+  address?: string;
+  createdAt: Date;
+  updatedAt: Date;
+  lastLogin?: Date;
+  roleId: String;
+  avatar: string;
+  role: {
+    rolelvl: number;
+  };
+}
+
+export interface UserDetails {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+  phoneNumber: string;
+  address: string;
+  createdAt: string;
+  updatedAt: string;
+  lastLogin: string;
+  roleId: string;
+  avatar: string;
+  role: {
+    name?: string;
+  };
+  balance: {
+    Balance: number;
+  };
+}
+
+export interface createUser {
+  name: string;
+  email: string;
+  password: string;
+  phoneNumber: string;
+  address: string;
+  roleId: string;
+  file?: File | null;
+}
+
+export interface updateUser {
+  name?: string;
+  email?: string;
+  password?: string;
+  phoneNumber?: string;
+  address?: string;
+  roleId?: string;
+  file?: File | null;
 }
 
 export interface userStateType {
   ShoortedUserInfo: {
-    data: ShoortedUserInfo;
+    data: UserWithRoleLvl;
     loading: boolean;
-    error:string;
+    error: string;
   };
+
+  UserDetails: {
+    data: {
+      users: UserDetails[];
+      pageCount: number;
+    };
+    loading: boolean;
+    error: string;
+  };
+  getUserById: {
+    data: UserDetails;
+    loading: boolean;
+    error: string;
+  };
+  createUser: {
+    data: createUser;
+    loading: boolean;
+    error: string;
+  };
+  updateUser: {
+    data: updateUser;
+    loading: boolean;
+    error: string;
+  };
+  ProfileUpdate : {
+    data : User,
+    loading : boolean,
+    error : string
+  }
+  userToEditId: string;
+  isEditUserOpen: boolean;
+  isAddUserOpen: boolean;
 }
