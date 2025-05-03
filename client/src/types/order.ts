54ea3ac57b2064240ec7a5bcc7b7bca33feb241b
Osama Lmezouari <Oussamalm147@gmail.com>
2025-05-03 08:32:23 +0100

    feat: define Order interface and extend OrderStateType for improved order management

--- client/src/types/order.ts
+++ client/src/types/order.ts
@@ -1,12 +1,60 @@
-
 export interface place_and_pay_order {
   couponCode: string;
+}
+export interface Order {
+  id: string;
+  userId: string;
+  couponId: string | null;
+  status: string;
+  totalAmount: number;
+  createdAt: string;
+  updatedAt: string;
+  orderItems: {
+    quantity: number;
+    product: {
+      name: string;
+      gallery: {
+        imageUrl: string;
+      }[];
+      Units: {
+        name: string;
+      };
+    };
+  }[];
+  user: {
+    name: string;
+    avatar: string;
+    email: string;
+  };
 }
 
 export interface OrderStateType {
   placeandpay: {
     data: {
       orderId: string;
+    };
+    loading: boolean;
+    error: string;
+  };
+  OrdersHistory: {
+    data: {
+      orders: Order[];
+      pageCount: number;
+    };
+    loading: boolean;
+    error: string;
+  };
+
+  OrderById: {
+    data: Order;
+    loading: boolean;
+    error: string;
+  };
+
+  UserOrders: {
+    data: {
+      orders: Order[];
+      pageCount: number;
     };
     loading: boolean;
     error: string;
