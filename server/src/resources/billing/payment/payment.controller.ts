92942c2304e4e0d176a564cc650abc0361976c23
Osama Lmezouari <Oussamalm147@gmail.com>
2025-05-03 08:36:03 +0100

    feat: refactor PaymentController to streamline transaction retrieval and remove unused methods

--- server/src/resources/billing/payment/payment.controller.ts
+++ server/src/resources/billing/payment/payment.controller.ts
@@ -1,50 +1,25 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { PaymentService } from './payment.service';
-import { CreatePaymentDto } from './dto/create-payment.dto';
-import { UpdatePaymentDto } from './dto/update-payment.dto';
+import { SET_PERMESSIONS } from 'src/common/decorators/meta/authorization.decorator';
+import { Permissions_TYPE } from 'src/common/enums/permissions';
+import { AuthenticationType } from 'src/common/enums/authentication';
+import { AUTH } from 'src/common/decorators/meta/authentication.decorator';
 
 @Controller('payment')
 export class PaymentController {
   constructor(private readonly paymentService: PaymentService) {}
 
-  /*   @Post()
-  async create(@Body() createPaymentDto: CreatePaymentDto) {
-    const payment = await this.paymentService.create(createPaymentDto);
-    return payment;
-  } */
-
-  @Get()
-  async findAll() {
-    const payment = await this.paymentService.findAll();
-    return payment;
-  }
-
-  @Get(':id')
-  async findOne(@Param('id') id: string) {
-    const payment = await this.paymentService.findOne(id);
-    return payment;
-  }
-
-  @Patch(':id')
-  async update(
-    @Param('id') id: string,
-    @Body() updatePaymentDto: UpdatePaymentDto,
+  @AUTH(AuthenticationType.bearer)
+  @SET_PERMESSIONS(Permissions_TYPE.PAYMENT_READ)
+  @Get('transactions')
+  async getPaymentTransactions(
+    @Query('page') page: number,
+    @Query('search') search: string,
   ) {
-    const payment = await this.paymentService.update(id, updatePaymentDto);
-    return payment;
-  }
-
-  @Delete(':id')
-  async remove(@Param('id') id: string) {
-    const payment = await this.paymentService.remove(id);
+    const payment = await this.paymentService.getPaymentTransactions(
+      page,
+      search,
+    );
     return payment;
   }
 }
