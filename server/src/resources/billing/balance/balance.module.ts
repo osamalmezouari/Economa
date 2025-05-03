7752c355ff3e909b82b6014d357d788d5935fe07
Osama Lmezouari <Oussamalm147@gmail.com>
2025-05-03 08:35:36 +0100

    feat: update BalanceModule to include NotificationModule and WebsocketsModule

--- server/src/resources/billing/balance/balance.module.ts
+++ server/src/resources/billing/balance/balance.module.ts
@@ -4,11 +4,13 @@ import { BalanceController } from './balance.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { RefillBalanceService } from './services/refillbalance.service';
 import { UsersTransfersService } from './services/userTransfers.service';
+import { NotificationModule } from 'src/resources/notifications/notification/notification.module';
+import { WebsocketsModule } from 'src/common/websockets/websockets.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, NotificationModule, WebsocketsModule],
   controllers: [BalanceController],
-  providers: [BalanceService, RefillBalanceService , UsersTransfersService],
-  exports: [BalanceService , RefillBalanceService , UsersTransfersService ],
+  providers: [BalanceService, RefillBalanceService, UsersTransfersService],
+  exports: [BalanceService, RefillBalanceService, UsersTransfersService],
 })
 export class BalanceModule {}
