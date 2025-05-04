a8caf3256243c3c7de4a5e6ca34a7f5f1dd337bf
Osama Lmezouari <Oussamalm147@gmail.com>
2025-05-04 23:05:04 +0100

    feat: enhance UserController with user retrieval, creation, and update methods

--- server/src/resources/core/user/user.controller.ts
+++ server/src/resources/core/user/user.controller.ts
@@ -6,45 +6,96 @@ import {
   Patch,
   Param,
   Delete,
-  NotFoundException,
+  Query,
+  UseInterceptors,
+  UploadedFile,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { activeUser } from 'src/common/decorators/params/activeUser.decorator';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { SET_PERMESSIONS } from 'src/common/decorators/meta/authorization.decorator';
+import { Permissions_TYPE } from 'src/common/enums/permissions';
+import { AuthenticationType } from 'src/common/enums/authentication';
+import { AUTH } from 'src/common/decorators/meta/authentication.decorator';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Post()
-  async create(@Body() createUserDto: CreateUserDto) {
-    const user = await this.userService.create(createUserDto);
+  @AUTH(AuthenticationType.bearer)
+  @SET_PERMESSIONS(Permissions_TYPE.USER_READ)
+  @Get('StoredUserFromToken')
+  async findByEmail(@activeUser('sub') userId: string) {
+    const user = await this.userService.findStoredUserFromToken(userId);
     return user;
   }
-
-  @Get()
-  async findAll() {
-    const user = await this.userService.findAll();
-    return user;
+  @AUTH(AuthenticationType.bearer)
+  @SET_PERMESSIONS(Permissions_TYPE.USERS_READ)
+  @Get('list')
+  async getUsersList(
+    @Query('search') search: string,
+    @Query('page') page: number,
+  ) {
+    return this.userService.getUsersList(page, search);
   }
-
-  @Get('byEmail')
-  async findEmail(@activeUser('sub') userId: string) {
-    const user = await this.userService.findByEmail(userId);
-    return user;
-  }
+  
+  @AUTH(AuthenticationType.bearer)
+  @SET_PERMESSIONS(Permissions_TYPE.USER_READ)
   @Get(':id')
-  async findOne(@Param('id') id: string) {
-    const user = await this.userService.findOne(id);
+  async findOne(@Param('id') userId: string) {
+    const user = await this.userService.findOne(userId);
     return user;
   }
 
-  @Patch(':id')
-  async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    const user = await this.userService.update(id, updateUserDto);
+  @AUTH(AuthenticationType.bearer)
+  @SET_PERMESSIONS(Permissions_TYPE.USER_CREATE)
+  @Post('adminCreate')
+  @UseInterceptors(FileInterceptor('file'))
+  async adminCreate(
+    @Body() createUserDto: CreateUserDto,
+    @UploadedFile() file: Express.Multer.File,
+  ) {
+    createUserDto.file = file;
+    const user = await this.userService.adminCreate(createUserDto);
+    return user;
   }
 
+  @AUTH(AuthenticationType.bearer)
+  @SET_PERMESSIONS(Permissions_TYPE.USER_UPDATE)
+  @Patch('adminUpdate/:id')
+  @UseInterceptors(FileInterceptor('file'))
+  async adminUpdate(
+    @Param('id') id: string,
+    @Body() updateUserDto: UpdateUserDto,
+    @UploadedFile() file: Express.Multer.File,
+  ) {
+    if (file) {
+      updateUserDto.file = file;
+    }
+    const user = await this.userService.adminUpdate(id, updateUserDto);
+    return user;
+  }
+
+  @AUTH(AuthenticationType.bearer)
+  @SET_PERMESSIONS(Permissions_TYPE.USER_PROFILE_UPDATE)
+  @Patch('ProfileUpdate')
+  @UseInterceptors(FileInterceptor('file'))
+  async ProfileUpdate(
+    @Body() updateUserDto: UpdateUserDto,
+    @UploadedFile() file: Express.Multer.File,
+    @activeUser('sub') userId: string,
+  ) {
+    if (file) {
+      updateUserDto.file = file;
+    }
+    const user = await this.userService.ProfileUpdate(userId, updateUserDto);
+    return user;
+  }
+
+  @AUTH(AuthenticationType.bearer)
+  @SET_PERMESSIONS(Permissions_TYPE.USER_DELETE)
   @Delete(':id')
   async remove(@Param('id') id: string) {
     const user = await this.userService.remove(id);
