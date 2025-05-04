4de428f46b3b76cbd1adc2524379dd1f6651723d
Osama Lmezouari <Oussamalm147@gmail.com>
2025-05-04 23:04:26 +0100

    feat: add authorization and permissions to category endpoints

--- server/src/resources/core/category/category.controller.ts
+++ server/src/resources/core/category/category.controller.ts
@@ -16,11 +16,15 @@ import { UpdateCategoryDto } from './dto/update-category.dto';
 import { AUTH } from 'src/common/decorators/meta/authentication.decorator';
 import { AuthenticationType } from 'src/common/enums/authentication';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { SET_PERMESSIONS } from 'src/common/decorators/meta/authorization.decorator';
+import { Permissions_TYPE } from 'src/common/enums/permissions';
 
 @Controller('category')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
 
+  @AUTH(AuthenticationType.bearer)
+  @SET_PERMESSIONS(Permissions_TYPE.CATEGORY_CREATE)
   @Post()
   @UseInterceptors(FileInterceptor('file'))
   async create(
@@ -32,32 +36,42 @@ export class CategoryController {
     return category;
   }
 
+  @AUTH(AuthenticationType.bearer)
+  @SET_PERMESSIONS(Permissions_TYPE.CATEGORIES_READ)
   @Get()
   async findAll(@Query('page') page: number) {
     const categories = await this.categoryService.findAll(page);
     return categories;
   }
 
+  @AUTH(AuthenticationType.None)
+  @SET_PERMESSIONS(Permissions_TYPE.CATEGORY_NAMESANDIDS_READ)
   @Get('CategoriesNamesandIds')
   async findCategoriesNamesandIds() {
     const categories = await this.categoryService.CategoriesNamesandIds();
     return categories;
   }
 
+
   @AUTH(AuthenticationType.None)
+  @SET_PERMESSIONS(Permissions_TYPE.CATEGORY_CARDS_READ)
   @Get('cards')
   async findAllCards() {
     const categories = await this.categoryService.findAllCards();
     return categories;
   }
 
+  @AUTH(AuthenticationType.bearer)
+  @SET_PERMESSIONS(Permissions_TYPE.CATEGORY_READ)
   @Get(':id')
   async findOne(@Param('id') id: string) {
     const category = await this.categoryService.findOne(id);
     return category;
   }
 
+  @AUTH(AuthenticationType.bearer)
   @UseInterceptors(FileInterceptor('file'))
+  @SET_PERMESSIONS(Permissions_TYPE.CATEGORY_UPDATE)
   @Patch(':id')
   async update(
     @Param('id') id: string,
@@ -71,6 +85,8 @@ export class CategoryController {
     return category;
   }
 
+  @AUTH(AuthenticationType.bearer)
+  @SET_PERMESSIONS(Permissions_TYPE.CATEGORY_DELETE)
   @Delete(':id')
   async remove(@Param('id') id: string) {
     const category = await this.categoryService.remove(id);
