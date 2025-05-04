e95899ba30ae97d87fc05bd6264383ea98b2c060
Osama Lmezouari <Oussamalm147@gmail.com>
2025-05-04 23:05:07 +0100

    feat: add BalanceModule to UserModule imports

--- server/src/resources/core/user/user.module.ts
+++ server/src/resources/core/user/user.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { PrismaModule } from '../../../prisma/prisma.module';
+import { BalanceModule } from 'src/resources/billing/balance/balance.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, BalanceModule],
   controllers: [UserController],
   providers: [UserService],
 })
