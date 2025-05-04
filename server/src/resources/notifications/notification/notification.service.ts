7b3034646377b6c1f7d4dc13543783c3ad095fd2
Osama Lmezouari <Oussamalm147@gmail.com>
2025-05-04 23:05:54 +0100

    feat: refactor NotificationService to remove CreateNotificationDto and streamline notification retrieval

--- server/src/resources/notifications/notification/notification.service.ts
+++ server/src/resources/notifications/notification/notification.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { CreateNotificationDto } from './dto/create-notification.dto';
 import { UpdateNotificationDto } from './dto/update-notification.dto';
 import { v4 as uuid } from 'uuid';
 import { NOTIFICATION_NOT_FOUND_Exception } from 'src/common/exceptions/Notification_not_found.exception';
@@ -8,18 +7,6 @@ import { NOTIFICATION_NOT_FOUND_Exception } from 'src/common/exceptions/Notifica
 @Injectable()
 export class NotificationService {
   constructor(private readonly prisma: PrismaService) {}
-
-  async create(createNotificationDto: CreateNotificationDto) {
-    const notification = await this.prisma.notification.create({
-      data: { id: uuid(), ...createNotificationDto },
-    });
-    return notification;
-  }
-
-  async findAll() {
-    const notifications = await this.prisma.notification.findMany();
-    return notifications;
-  }
 
   async findOne(id: string) {
     const notification = await this.prisma.notification.findUnique({
@@ -29,19 +16,63 @@ export class NotificationService {
     throw new NOTIFICATION_NOT_FOUND_Exception(id);
   }
 
+  async findAll() {
+    const notifications = await this.prisma.notification.findMany();
+    return notifications;
+  }
+
+  async getNotificationList(userId: string) {
+    const userNotifications = await this.prisma.userNotification.findMany({
+      where: { userId },
+      include: { notification: true },
+      orderBy: {
+        notification: {
+          createdAt: 'desc',
+        },
+      },
+    });
+
+    return userNotifications.map((userNotif) => ({
+      id: userNotif.notification.id,
+      userId: userNotif.userId,
+      type: userNotif.notification.type,
+      message: userNotif.notification.message,
+      isRead: userNotif.notification.isRead, // from userNotification table
+      createdAt: userNotif.notification.createdAt.toISOString(),
+    }));
+  }
+
+  async createPermissionBasedNotification(
+    users: { id: string }[],
+    message: string,
+    type: string,
+  ) {
+    if (users.length === 0) return;
+    const notification = await this.prisma.notification.create({
+      data: {
+        id: uuid(),
+        message,
+        type,
+        isRead: false,
+      },
+    });
+    const notificationLinks = users.map((user) => ({
+      userId: user.id,
+      notificationId: notification.id,
+    }));
+
+    await this.prisma.userNotification.createMany({
+      data: notificationLinks,
+    });
+
+    return notification;
+  }
+
   async update(id: string, updateNotificationDto: UpdateNotificationDto) {
     await this.findOne(id);
     const Notification = await this.prisma.notification.update({
       where: { id },
       data: updateNotificationDto,
-    });
-    return Notification;
-  }
-
-  async remove(id: string) {
-    await this.findOne(id);
-    const Notification = await this.prisma.notification.delete({
-      where: { id },
     });
     return Notification;
   }
